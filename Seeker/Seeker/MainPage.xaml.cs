84303b3a688aa8712c1eef6eed0a4ac5c63a1d08
mig1023 <mig1023@yandex.ru>
2020-06-16 22:32:23 +0300

    temporary router

--- Seeker/Seeker/MainPage.xaml.cs
+++ Seeker/Seeker/MainPage.xaml.cs
@@ -11,6 +11,8 @@ namespace Seeker
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        private static Dictionary<string, int> tmpRouter = new Dictionary<string, int>();
+
         public MainPage()
         {
             InitializeComponent();
@@ -25,15 +27,38 @@ namespace Seeker
             this.Title.Text = paragraph.Title;
             this.Text.Text = paragraph.Text;
 
-            foreach(Gamebook.Option option in paragraph.Options)
+            tmpRouter.Clear();
+
+            Options.Children.Clear();
+
+            foreach (Gamebook.Option option in paragraph.Options)
             {
                 Button button = new Button()
                 {
                     Text = option.Text,
+                    TextColor = Xamarin.Forms.Color.White,
+                    BackgroundColor = Color.FromHex("#2196F3")
                 };
+
+                tmpRouter.Add(option.Text, option.Destination);
+
+                button.Clicked += Option_Click;
 
                 Options.Children.Add(button);
             }
+        }
+
+        private void Option_Click(object sender, EventArgs e)
+        {
+            Button b = sender as Button;
+            int? newParagraph = null;
+
+            foreach(KeyValuePair<string, int> k in tmpRouter)
+                if (k.Key == b.Text)
+                    newParagraph = k.Value;
+
+            if (newParagraph != null)
+                GoToParagraph(newParagraph ?? 0);
         }
     }
 }
