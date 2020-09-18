cd141d63c356e0127cd60bd5f76e873e74956679
mig1023 <mig1023@yandex.ru>
2020-09-18 22:27:27 +0300

    aldar param, actions, fixes

--- Seeker/Seeker/Gamebook/AdventuresOfABeardlessDeceiver/Actions.cs
+++ Seeker/Seeker/Gamebook/AdventuresOfABeardlessDeceiver/Actions.cs
@@ -12,20 +12,9 @@ namespace Seeker.Gamebook.AdventuresOfABeardlessDeceiver
         public string Aftertext { get; set; }
         public string OpenOption { get; set; }
 
-        // Fight
-        public List<Character> Allies { get; set; }
-        public List<Character> Enemies { get; set; }
-        public int RoundsToWin { get; set; }
-        public int WoundsToWin { get; set; }
-        public int DamageToWin { get; set; }
-        public int MasteryPenalty { get; set; }
-        public bool GroupFight { get; set; }
-
-        // Get
         public string Text { get; set; }
-        public int Price { get; set; }
-        public bool Used { get; set; }
-        public bool Multiple { get; set; }
+        public string Stat { get; set; }
+        public int TestLevel { get; set; }
 
         public List<string> Do(out bool reload, string action = "", bool openOption = false)
         {
@@ -44,9 +33,13 @@ namespace Seeker.Gamebook.AdventuresOfABeardlessDeceiver
         {
             List<string> statusLines = new List<string>
             {
-                String.Format("Мастерство: {0}", Character.Protagonist.Mastery),
-                String.Format("Выносливость: {0}", Character.Protagonist.Endurance),
-                String.Format("Золото: {0}", Character.Protagonist.Gold)
+                String.Format("Сила: {0}", Character.Protagonist.Strength),
+                String.Format("Ловкость: {0}", Character.Protagonist.Skill),
+                String.Format("Мудрость: {0}", Character.Protagonist.Wisdom),
+                String.Format("Хитрость: {0}", Character.Protagonist.Cunning),
+                String.Format("Красноречие: {0}", Character.Protagonist.Oratory),
+                String.Format("Популярность: {0}", Character.Protagonist.Popularity),
+                String.Format("Деньги: {0}", Character.Protagonist.Tanga)
             };
 
             return statusLines;
@@ -54,17 +47,15 @@ namespace Seeker.Gamebook.AdventuresOfABeardlessDeceiver
 
         public bool GameOver(out int toEndParagraph, out string toEndText)
         {
-            toEndParagraph = 0;
-            toEndText = "Начать сначала";
+            toEndParagraph = 150;
+            toEndText = "Задуматься о судьбе";
 
-            return (Character.Protagonist.Endurance <= 0 ? true : false);
+            return (Character.Protagonist.Popularity <= 0 ? true : false);
         }
 
         public bool IsButtonEnabled()
         {
-            bool disabledGetOptions = (Price > 0) && Used;
-
-            return !disabledGetOptions;
+            return (Character.Protagonist.StatBonuses > 0);
         }
 
         public static bool CheckOnlyIf(string option)
@@ -77,285 +68,53 @@ namespace Seeker.Gamebook.AdventuresOfABeardlessDeceiver
 
                 return true;
             }
-            else if (option.Contains("ЗОЛОТО >="))
-                return int.Parse(option.Split('=')[1]) <= Character.Protagonist.Gold;
+            else if (option.Contains("ТАНЬГА >="))
+                return int.Parse(option.Split('=')[1]) <= Character.Protagonist.Tanga;
             else
                 return Game.Data.OpenedOption.Contains(option);
         }
 
-        public List<string> Representer()
+        public List<string> Test()
         {
-            List<string> enemies = new List<string>();
+            int firstDice = Game.Dice.Roll();
+            int secondDice = Game.Dice.Roll();
 
-            if (ActionName == "Get")
+            int currentStat = (int)Character.Protagonist.GetType().GetProperty(Stat).GetValue(Character.Protagonist, null);
+            bool testIsOk = (firstDice + secondDice) + currentStat >= TestLevel;
+
+            Dictionary<string, string> statNames = new Dictionary<string, string>
             {
-                string countMarker = String.Empty;
-
-                return new List<string> { String.Format("{0}{1}", Text, countMarker) };
-            }
-
-            if (Enemies == null)
-                return enemies;
-
-            if ((Allies != null) && GroupFight)
-            {
-                foreach (Character ally in Allies)
-                    enemies.Add(String.Format("{0}\nловкость {1}  сила {2}", ally.Name, ally.Mastery, ally.GetEndurance()));
-
-                enemies.Add("SPLITTER|против");
-            }
-                
-
-            foreach (Character enemy in Enemies)
-                enemies.Add(String.Format("{0}\nловкость {1}  сила {2}", enemy.Name, enemy.Mastery, enemy.GetEndurance()));
-
-            return enemies;
-        }
-
-        public List<string> Break()
-        {
-            List<string> breakingDoor = new List<string> { "Ломаете дверь:" };
-
-            bool succesBreaked = false;
-
-            while (!succesBreaked && (Character.Protagonist.Endurance > 0))
-            {
-                int firstDice = Game.Dice.Roll();
-                int secondDice = Game.Dice.Roll();
-
-                if (((firstDice == 1) || (firstDice == 6)) && (firstDice == secondDice))
-                    succesBreaked = true;
-                else
-                    Character.Protagonist.Endurance -= 1;
-
-                string result = (succesBreaked ? "удачный, дверь поддалась!" : "неудачный, -1 сила" );
-                breakingDoor.Add(String.Format("Удар: {0} ⚄ + {1} ⚄ - {2}", firstDice, secondDice, result));
-            }
-
-            breakingDoor.Add(succesBreaked ? "BIG|GOOD|ДВЕРЬ ВЗЛОМАНА :)" : "BIG|BAD|ВЫ УБИЛИСЬ ОБ ДВЕРЬ :(");
-
-            return breakingDoor;
-        }
-
-        public List<string> Luck()
-        {
-            Dictionary<int, string> luckList = new Dictionary<int, string>
-            {
-                [1] = "①",
-                [2] = "②",
-                [3] = "③",
-                [4] = "④",
-                [5] = "⑤",
-                [6] = "⑥",
-
-                [11] = "❶",
-                [12] = "❷",
-                [13] = "❸",
-                [14] = "❹",
-                [15] = "❺",
-                [16] = "❻",
+                ["Strength"] = "силы",
+                ["Skill"] = "ловкости",
+                ["Wisdom"] = "мудрости",
+                ["Cunning"] = "хитрости",
+                ["Oratory"] = "красноречия",
+                ["Popularity"] = "популярности",
             };
 
-            List<string> luckCheck = new List<string> { "Квадраты удачи:" };
-
-            string luckListShow = String.Empty;
-
-            for (int i = 1; i < 7; i++)
-                luckListShow += (Character.Protagonist.Luck[i] ? luckList[i] : luckList[i + 10]) + " ";
-
-            luckCheck.Add("BIG|" + luckListShow);
-
-            int goodLuck = Game.Dice.Roll();
-
-            luckCheck.Add(String.Format("Проверка удачи: {0} ⚄ - {1}зачёркунтый", goodLuck, (Character.Protagonist.Luck[goodLuck] ? "не " : String.Empty)));
-
-            luckCheck.Add(Character.Protagonist.Luck[goodLuck] ? "BIG|GOOD|УСПЕХ :)" : "BIG|BAD|НЕУДАЧА :(");
-            
-            Character.Protagonist.Luck[goodLuck] = !Character.Protagonist.Luck[goodLuck];
-
-            return luckCheck;
-        }
-        
-        public List<string> RollDice()
-        {
-            return new List<string> { String.Format("BIG|Бросок: {0} ⚄", Game.Dice.Roll()) };
-        }
-
-        public List<string> RollDoubleDices()
-        {
-            int firstDice = Game.Dice.Roll();
-            int secondDice = Game.Dice.Roll();
-
-            return new List<string> { String.Format("BIG|Бросок: {0} ⚄ + {1} ⚄ = {2}", firstDice, secondDice, (firstDice + secondDice)) };
-        }
-
-        public List<string> Mastery()
-        {
-            int firstDice = Game.Dice.Roll();
-            int secondDice = Game.Dice.Roll();
-
-            bool goodMastery = (firstDice + secondDice) <= Character.Protagonist.Mastery;
-
-            List<string> masteryCheck = new List<string> { String.Format(
-                "Проверка мастерства: {0} ⚄ + {1} ⚄ {2} {3} мастерство", firstDice, secondDice, (goodMastery ? "<=" : ">"), Character.Protagonist.Mastery
+            List<string> testLines = new List<string> { String.Format(
+                "Проверка {0}: {1} ⚄ + {2} ⚄ + {3} {4} {5}", statNames[Stat], firstDice, secondDice, currentStat, (testIsOk ? ">=" : "<"), TestLevel
             ) };
 
-            masteryCheck.Add(goodMastery ? "BIG|GOOD|МАСТЕРСТВА ХВАТИЛО :)" : "BIG|BAD|МАСТЕРСТВА НЕ ХВАТИЛО :(");
+            testLines.Add(testIsOk ? "BIG|GOOD|АЛДАР СПРАВИЛСЯ :)" : "BIG|BAD|АЛДАР НЕ СПРАВИЛСЯ :(");
 
-            return masteryCheck;
+            return testLines;
         }
-
 
         public List<string> Get()
         {
-            if ((Price > 0) && (Character.Protagonist.Gold >= Price))
+            if (Character.Protagonist.StatBonuses >= 0)
             {
-                Character.Protagonist.Gold -= Price;
+                int currentStat = (int)Character.Protagonist.GetType().GetProperty(Stat).GetValue(Character.Protagonist, null);
 
-                if (!Multiple)
-                    Used = true;
+                currentStat += 1;
+
+                Character.Protagonist.GetType().GetProperty(Stat).SetValue(Character.Protagonist, currentStat);
+
+                Character.Protagonist.StatBonuses -= 1;
             }
 
             return new List<string> { "RELOAD" };
-        }
-
-        private bool IsHero(string name)
-        {
-            return (name == "ГЛАВГЕРОЙ" ? true : false);
-        }
-
-        public List<string> Fight()
-        {
-            List<string> fight = new List<string>();
-
-            int round = 1;
-            int enemyWounds = 0;
-
-            List<Character> FightAllies = new List<Character>();
-            List<Character> FightEnemies = new List<Character>();
-
-            foreach (Character enemy in Enemies)
-                FightEnemies.Add(enemy.Clone().SetEndurance());             
-
-            foreach (Character ally in Allies)
-                if (ally == Character.Protagonist)
-                    FightAllies.Add(ally);
-                else
-                    FightAllies.Add(ally.Clone().SetEndurance());                   
-
-            while (true)
-            {
-                fight.Add(String.Format("HEAD|Раунд: {0}", round));
-
-                foreach (Character ally in FightAllies)
-                {
-                    if (ally.Endurance <= 0)
-                        continue;
-
-                    if (GroupFight)
-                        fight.Add(String.Format("{0} (сила {1})", (IsHero(ally.Name) ? "Вы" : ally.Name), ally.Endurance));
-
-                    foreach (Character enemy in FightEnemies)
-                    {
-                        if (enemy.Endurance <= 0)
-                            continue;
-
-                        fight.Add(String.Format("{0} (сила {1})", enemy.Name, enemy.Endurance));
-
-                        int firstHeroRoll = Game.Dice.Roll();
-                        int secondHeroRoll = Game.Dice.Roll();
-                        int allyHitEndurance = firstHeroRoll + secondHeroRoll + (ally.Mastery - MasteryPenalty);
-
-                        fight.Add(
-                            String.Format(
-                                "{0} мощность удара : {1} ⚄ + {2} ⚄ + {3} = {4}",
-                                (IsHero(ally.Name) ? "Ваша" : String.Format("{0} -", ally.Name)), firstHeroRoll, secondHeroRoll, ally.Mastery, allyHitEndurance
-                            )
-                        );
-
-                        int firstEnemyRoll = Game.Dice.Roll();
-                        int secondEnemyRoll = Game.Dice.Roll();
-                        int enemyHitEndurance = firstEnemyRoll + secondEnemyRoll + enemy.Mastery;
-
-                        fight.Add(
-                            String.Format(
-                                "{0} мощность удара: {1} ⚄ + {2} ⚄ + {3} = {4}",
-                                (GroupFight ? String.Format("{0} -", enemy.Name) : "Его"), firstEnemyRoll, secondEnemyRoll, enemy.Mastery, enemyHitEndurance
-                            )
-                        );
-
-                        if (allyHitEndurance > enemyHitEndurance)
-                        {
-                            if (enemy.SeaArmour && (firstHeroRoll == secondHeroRoll))
-                                fight.Add(String.Format("BOLD|Чешуя отразила ваш удар"));
-                            else
-                            {
-                                fight.Add(String.Format("GOOD|{0} ранен", (GroupFight ? enemy.Name : "Он")));
-                                enemy.Endurance -= 2 + ally.ExtendedDamage;
-                                enemy.Mastery -= ally.MasteryDamage;
-
-                                if (enemy.Endurance <= 0)
-                                    enemy.Endurance = 0;
-
-                                enemy.SaveEndurance();
-
-                                enemyWounds += 1;
-
-                                bool enemyLost = true;
-
-                                foreach (Character e in FightEnemies)
-                                    if ((e.Endurance > 0) && (e.Endurance > DamageToWin))
-                                        enemyLost = false;
-
-                                if (enemyLost || ((WoundsToWin > 0) && (WoundsToWin <= enemyWounds)))
-                                {
-                                    fight.Add(String.Empty);
-                                    fight.Add(String.Format("BIG|GOOD|{0} :)", (GroupFight && !IsHero(ally.Name) ? ally.Name + " ПОБЕДИЛ" : "ВЫ ПОБЕДИЛИ")));
-                                    return fight;
-                                }
-                            }
-                        }
-                        else if (allyHitEndurance < enemyHitEndurance)
-                        {
-                            fight.Add(GroupFight && !IsHero(ally.Name) ? String.Format("BAD|{0} ранен",  ally.Name) : "BAD|Вы ранены");
-                            ally.Endurance -= 2 + enemy.ExtendedDamage;
-                            ally.Mastery -= enemy.MasteryDamage;
-
-                            if (ally.Endurance < 0)
-                                ally.Endurance = 0;
-
-                            ally.SaveEndurance();
-
-                            bool allyLost = true;
-
-                            foreach (Character a in FightAllies)
-                                if (a.Endurance > 0)
-                                    allyLost = false;
-
-                            if (allyLost)
-                            {
-                                fight.Add(String.Empty);
-                                fight.Add(String.Format("BIG|BAD|{0} :(", (IsHero(ally.Name) ? "ВЫ ПРОИГРАЛИ" : String.Format("{0} ПРОИГРАЛ", ally.Name))));
-                                return fight;
-                            }
-                        }
-                        else
-                            fight.Add(String.Format("BOLD|Ничья в раунде"));
-
-                        if ((RoundsToWin > 0) && (RoundsToWin <= round))
-                        {
-                            fight.Add(String.Empty);
-                            fight.Add(String.Format("BAD|Отведённые на победу раунды истекли.", RoundsToWin));
-                            fight.Add(String.Format("BIG|BAD|{0} :(", (IsHero(ally.Name) ? "ВЫ ПРОИГРАЛИ" : String.Format("{0} ПРОИГРАЛ", ally.Name))));
-                            return fight;
-                        }
-
-                        fight.Add(String.Empty);
-                    }
-                }
-
-                round += 1;
-            }
         }
     }
 }
--- Seeker/Seeker/Gamebook/AdventuresOfABeardlessDeceiver/Character.cs
+++ Seeker/Seeker/Gamebook/AdventuresOfABeardlessDeceiver/Character.cs
@@ -10,100 +10,42 @@ namespace Seeker.Gamebook.AdventuresOfABeardlessDeceiver
 
         public string Name { get; set; }
 
-        public int Mastery { get; set; }
-        public int Endurance { get; set; }
-        public int Gold { get; set; }
-        public int ExtendedDamage { get; set; }
-        public int MasteryDamage { get; set; }
-        public bool SeaArmour { get; set; }
-        public Dictionary<int, bool> Luck { get; set; }
+        public int Strength { get; set; }
+        public int Skill { get; set; }
+        public int Wisdom { get; set; }
+        public int Cunning { get; set; }
+        public int Oratory { get; set; }
+        public int Popularity { get; set; }
+        public int Tanga { get; set; }
 
-        private static Dictionary<string, int> EnduranceLoss = new Dictionary<string, int>();
+        public int StatBonuses { get; set; }
 
         public void Init()
         {
-            Dictionary<int, int> Masterys = new Dictionary<int, int>
-            {
-                [2] = 8,
-                [3] = 10,
-                [4] = 12,
-                [5] = 9,
-                [6] = 11,
-                [7] = 9,
-                [8] = 10,
-                [9] = 8,
-                [10] = 9,
-                [11] = 10,
-                [12] = 11
-            };
-
-            Dictionary<int, int> Endurances = new Dictionary<int, int>
-            {
-                [2] = 22,
-                [3] = 20,
-                [4] = 16,
-                [5] = 18,
-                [6] = 20,
-                [7] = 20,
-                [8] = 16,
-                [9] = 24,
-                [10] = 22,
-                [11] = 18,
-                [12] = 20
-            };
-
-            int dice = Game.Dice.Roll(dices: 2);
-
-            Name = "ГЛАВГЕРОЙ";
-            Mastery = Masterys[dice];
-            Endurance = Endurances[dice];
-            Gold = 15;
-
-            Luck = new Dictionary<int, bool>
-            {
-                [1] = true,
-                [2] = true,
-                [3] = true,
-                [4] = true,
-                [5] = true,
-                [6] = true,
-            };
-
-            for (int i = 0; i < 2; i++)
-                Luck[Game.Dice.Roll()] = false;
+            Strength = 1;
+            Skill = 1;
+            Wisdom = 1;
+            Cunning = 1;
+            Oratory = 1;
+            Popularity = 3;
+            Tanga = 15;
+            StatBonuses = 4;
         }
 
         public Character Clone()
         {
             Character newCharacter = new Character();
 
-            newCharacter.Name = this.Name;
-            newCharacter.Mastery = this.Mastery;
-            newCharacter.Endurance = this.Endurance;
-            newCharacter.Gold = this.Gold;
-            newCharacter.ExtendedDamage = this.ExtendedDamage;
-            newCharacter.MasteryDamage = this.MasteryDamage;
-            newCharacter.SeaArmour = this.SeaArmour;
+            newCharacter.Strength = this.Strength;
+            newCharacter.Skill = this.Skill;
+            newCharacter.Wisdom = this.Wisdom;
+            newCharacter.Cunning = this.Cunning;
+            newCharacter.Oratory = this.Oratory;
+            newCharacter.Popularity = this.Popularity;
+            newCharacter.Tanga = this.Tanga;
+            newCharacter.StatBonuses = this.StatBonuses;
 
             return newCharacter;
-        }
-
-        public Character SetEndurance()
-        {
-            if (EnduranceLoss.ContainsKey(this.Name))
-                this.Endurance = EnduranceLoss[this.Name];
-
-            return this;
-        }
-
-        public void SaveEndurance()
-        {
-            EnduranceLoss[this.Name] = this.Endurance;
-        }
-
-        public int GetEndurance()
-        {
-            return (EnduranceLoss.ContainsKey(this.Name) ? EnduranceLoss[this.Name] : this.Endurance);
         }
     }
 }
