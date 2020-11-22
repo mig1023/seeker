a80266fd1fb4732bbb1bb6fb731a3233ea1f2c53
mig1023 <mig1023@yandex.ru>
2020-11-22 20:36:12 +0300

    swampfever save/load

--- Seeker/Seeker/Gamebook/SwampFever/Character.cs
+++ Seeker/Seeker/Gamebook/SwampFever/Character.cs
@@ -65,5 +65,35 @@ namespace Seeker.Gamebook.SwampFever
                 LiveMucus = this.LiveMucus,
             };
         }
+
+        public string Save()
+        {
+            return String.Format(
+                "{0}|{1}|{2}|{3}|{4}|{5}|{6}|{7}|{8}|{9}|{10}|{11}|{12}|{13}",
+                Fury, Creds, Stigon, Rate, Hitpoints, SecondEngine, Stealth,
+                Radar, CircularSaw, Flamethrower, PlasmaCannon, Harmonizer,
+                AcousticMembrane, LiveMucus
+            );
+        }
+
+        public void Load(string saveLine)
+        {
+            string[] save = saveLine.Split('|');
+
+            Fury = int.Parse(save[0]);
+            Creds = int.Parse(save[1]);
+            Stigon = int.Parse(save[2]);
+            Rate = int.Parse(save[3]);
+            Hitpoints = int.Parse(save[4]);
+            SecondEngine = int.Parse(save[5]);
+            Stealth = int.Parse(save[6]);
+            Radar = int.Parse(save[7]);
+            CircularSaw = int.Parse(save[8]);
+            Flamethrower = int.Parse(save[9]);
+            PlasmaCannon = int.Parse(save[10]);
+            Harmonizer = int.Parse(save[11]);
+            AcousticMembrane = int.Parse(save[12]);
+            LiveMucus = int.Parse(save[13]);
+        }
     }
 }
