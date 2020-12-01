b650d3066f38dbd95caf425c73735ad3fe003bdb
mig1023 <mig1023@yandex.ru>
2020-12-01 19:01:35 +0300

    checks fixes

--- Seeker/Seeker/Game/Checks.cs
+++ Seeker/Seeker/Game/Checks.cs
@@ -8,19 +8,19 @@ namespace Seeker.Game
     {
         public static bool ExistsInParagraph(string actionName = "", string actionText = "", string optionText = "")
         {
-            if ((!String.IsNullOrEmpty(actionName) || !String.IsNullOrEmpty(actionText)) && (Game.Data.CurrentParagraph.Actions != null))
+            if (Game.Data.CurrentParagraph.Actions != null)
                 foreach (Abstract.IActions action in Game.Data.CurrentParagraph.Actions)
                 {
-                    if (action.ActionName.ToUpper().Contains(actionName.ToUpper()))
+                    if (!String.IsNullOrEmpty(actionName) && action.ActionName.ToUpper().Contains(actionName.ToUpper()))
                         return true;
 
-                    if (action.ButtonName.ToUpper().Contains(actionText.ToUpper()))
+                    if (!String.IsNullOrEmpty(actionText) && action.ButtonName.ToUpper().Contains(actionText.ToUpper()))
                         return true;
                 }
 
-            if (!String.IsNullOrEmpty(optionText) && (Game.Data.CurrentParagraph.Options != null))
+            if (Game.Data.CurrentParagraph.Options != null)
                 foreach (Option option in Game.Data.CurrentParagraph.Options)
-                    if (option.Text.ToUpper().Contains(optionText.ToUpper()))
+                    if (!String.IsNullOrEmpty(optionText) && option.Text.ToUpper().Contains(optionText.ToUpper()))
                         return true;
 
             return false;
