5d2c6d2a1d76047d97856ba49c3639e8938fcc24
mig1023 <mig1023@yandex.ru>
2020-12-07 17:59:51 +0300

    random button colors for string of worlds

--- Seeker/Seeker/Gamebook/StringOfWorlds/Constants.cs
+++ Seeker/Seeker/Gamebook/StringOfWorlds/Constants.cs
@@ -9,21 +9,39 @@ namespace Seeker.Gamebook.StringOfWorlds
 {
     class Constants : Abstract.IConstants
     {
-        static Dictionary<ButtonTypes, string> ButtonsColors = new Dictionary<ButtonTypes, string>
-        {
-            [ButtonTypes.Main] = "#151515",
-            [ButtonTypes.Action] = "#3f3f3f",
-            [ButtonTypes.Option] = "#696969",
-        };
+        private Random random = new Random();
+
+        private static string LastButtonColor = String.Empty; 
 
         static Dictionary<ColorTypes, string> Colors = new Dictionary<ColorTypes, string>
         {
-            [ColorTypes.StatusBar] = "#2a2a2a",
+            [ColorTypes.StatusBar] = "#990066",
         };
 
         public string GetButtonsColor(ButtonTypes type)
         {
-            return (ButtonsColors.ContainsKey(type) ? ButtonsColors[type] : String.Empty);
+            if (type == ButtonTypes.Border)
+                return String.Empty;
+            else if (type == ButtonTypes.Font)
+                return BlackOrWhite();
+            else
+                return RandomColor();
+        }
+
+        private string RandomColor()
+        {
+            LastButtonColor = String.Format("#{0:X6}", random.Next(0x1000000));
+            return LastButtonColor;
+        }
+
+        private string BlackOrWhite()
+        {
+            List<int> rgb = new List<int>();
+
+            for (int i = 1; i < 6; i += 2)
+                rgb.Add(Convert.ToInt32(new string(new char[] { LastButtonColor[i], LastButtonColor[i+1] }), 16));
+
+            return (((rgb[0] * 0.299) + (rgb[1] * 0.587) + (rgb[2] * 0.114)) > 186 ? "#000000" : "#FFFFFF" );
         }
 
         public string GetColor(Game.Data.ColorTypes type)
