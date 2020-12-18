d78510d212bfc7175abb0509b45666c18a9b1fc3
mig1023 <mig1023@yandex.ru>
2020-12-18 18:49:26 +0300

    three paths gamebook (start)

---
+++ Seeker/Seeker/Gamebook/ThreePaths/Paragraphs.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+using Seeker.Game;
+
+
+namespace Seeker.Gamebook.ThreePaths
+{
+    class Paragraphs : Abstract.IParagraphs
+    {
+        public Game.Paragraph Get(int id, XmlNode xmlParagraph)
+        {
+            Game.Paragraph paragraph = new Game.Paragraph();
+
+            paragraph.Options = new List<Option>();
+            paragraph.Actions = new List<Abstract.IActions>();
+            paragraph.Modification = new List<Abstract.IModification>();
+
+            foreach (XmlNode xmlOption in xmlParagraph.SelectNodes("Options/Option"))
+            {
+                Option option = new Option
+                {
+                    Destination = Game.Xml.IntParse(xmlOption.Attributes["Destination"]),
+                    Text = Game.Xml.StringParse(xmlOption.Attributes["Text"]),
+                    OnlyIf = Game.Xml.StringParse(xmlOption.Attributes["OnlyIf"]),
+                };
+
+                paragraph.Options.Add(option);
+            }
+
+            foreach (XmlNode xmlAction in xmlParagraph.SelectNodes("Actions/Action"))
+            {
+                Actions action = new Actions
+                {
+                    ActionName = Game.Xml.StringParse(xmlAction["ActionName"]),
+                    ButtonName = Game.Xml.StringParse(xmlAction["ButtonName"]),
+                    Aftertext = Game.Xml.StringParse(xmlAction["Aftertext"]),
+                    Trigger = Game.Xml.StringParse(xmlAction["Trigger"]),
+                    Text = Game.Xml.StringParse(xmlAction["Text"]),
+
+                    RoundsToWin = Game.Xml.IntParse(xmlAction["RoundsToWin"]),
+                    WoundsToWin = Game.Xml.IntParse(xmlAction["WoundsToWin"]),
+                    Price = Game.Xml.IntParse(xmlAction["Price"]),
+
+                    Multiple = Game.Xml.BoolParse(xmlAction["Multiple"]),
+                    ThisIsSpell = Game.Xml.BoolParse(xmlAction["ThisIsSpell"]),
+                };
+
+                if (xmlAction["Enemies"] != null)
+                {
+                    action.Enemies = new List<Character>();
+
+                    foreach (XmlNode xmlEnemy in xmlAction.SelectNodes("Enemies/Enemy"))
+                    {
+                        Character enemy = new Character
+                        {
+                            Name = Game.Xml.StringParse(xmlEnemy.Attributes["Name"]),
+                            Mastery = Game.Xml.IntParse(xmlEnemy.Attributes["Mastery"]),
+                            Endurance = Game.Xml.IntParse(xmlEnemy.Attributes["Endurance"]),
+                        };
+
+                        action.Enemies.Add(enemy);
+                    }
+                }
+
+                action.Benefit = ModificationParse(xmlAction["Benefit"]);
+
+                paragraph.Actions.Add(action);
+            }
+
+            foreach (XmlNode xmlModification in xmlParagraph.SelectNodes("Modifications/Modification"))
+                paragraph.Modification.Add(ModificationParse(xmlModification));
+
+            paragraph.Trigger = Game.Xml.StringParse(xmlParagraph["Triggers"]);
+
+            return paragraph;
+        }
+
+        private static Modification ModificationParse(XmlNode xmlNode)
+        {
+            if (xmlNode == null)
+                return null;
+
+            Modification modification = new Modification
+            {
+                Name = Game.Xml.StringParse(xmlNode.Attributes["Name"]),
+            };
+
+            if (xmlNode.Attributes["Value"] != null)
+                modification.Value = Game.Xml.IntParse(xmlNode.Attributes["Value"]);
+
+            if (xmlNode.Attributes["ValueString"] != null)
+                modification.Value = Game.Xml.IntParse(xmlNode.Attributes["ValueString"]);
+
+            return modification;
+        }
+    }
+}
