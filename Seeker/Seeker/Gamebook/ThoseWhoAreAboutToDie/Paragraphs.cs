9573dd4975d0bf591d71f533c3a824fe8f3fb14d
mig1023 <mig1023@yandex.ru>
2021-01-03 22:09:37 +0300

    texts/options fixes

--- Seeker/Seeker/Gamebook/ThoseWhoAreAboutToDie/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/ThoseWhoAreAboutToDie/Paragraphs.cs
@@ -11,6 +11,8 @@ namespace Seeker.Gamebook.ThoseWhoAreAboutToDie
 {
     class Paragraphs : Abstract.IParagraphs
     {
+        private Random random = new Random();
+
         public Game.Paragraph Get(int id, XmlNode xmlParagraph)
         {
             Game.Paragraph paragraph = new Game.Paragraph();
@@ -23,10 +25,17 @@ namespace Seeker.Gamebook.ThoseWhoAreAboutToDie
             {
                 Option option = new Option
                 {
-                    Destination = Game.Xml.IntParse(xmlOption.Attributes["Destination"]),
                     Text = Game.Xml.StringParse(xmlOption.Attributes["Text"]),
                     OnlyIf = Game.Xml.StringParse(xmlOption.Attributes["OnlyIf"]),
                 };
+
+                if (int.TryParse(xmlOption.Attributes["Destination"].Value, out int _))
+                    option.Destination = Game.Xml.IntParse(xmlOption.Attributes["Destination"]);
+                else
+                {
+                    List<string> destinations = xmlOption.Attributes["Destination"].Value.Split(',').ToList<string>();
+                    option.Destination = int.Parse(destinations[random.Next(destinations.Count())]);
+                }
 
                 paragraph.Options.Add(option);
             }
