dac37303a9737c87494245c2d6e4a69ae33edd8c
mig1023 <mig1023@yandex.ru>
2021-01-10 21:35:04 +0300

    vertical text

---
+++ Seeker/Seeker.Android/RotatedTextRenderer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Text;
+using Android.Views;
+using Android.Widget;
+using Seeker;
+using Seeker.Droid;
+using Seeker.Output;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(VerticalText), typeof(RotatedTextRenderer))]
+namespace Seeker.Droid
+{
+    public class RotatedTextRenderer : ViewRenderer
+    {
+        private Context _context;
+
+        public RotatedTextRenderer(Context c) : base(c)
+        {
+            _context = c;
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.View> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement is VerticalText)
+            {
+                string title = ((VerticalText)e.NewElement).Value;
+                SetNativeControl(new RotatedTextView(_context, title));
+            }
+        }
+    }
+
+    public class RotatedTextView : Android.Views.View
+    {
+        private int DEFAULT_TEXT_SIZE = 30;
+        private string _text;
+        private TextPaint _textPaint;
+
+        public RotatedTextView(Context c, string title) : base(c)
+        {
+            _text = title;
+            initLabelView();
+        }
+
+        private void initLabelView()
+        {
+            this._textPaint = new TextPaint();
+            this._textPaint.AntiAlias = true;
+            this._textPaint.TextAlign = Paint.Align.Center;
+            this._textPaint.TextSize = DEFAULT_TEXT_SIZE;
+            this._textPaint.Color = new Android.Graphics.Color(0, 0, 0);
+        }
+
+        public override void Draw(Canvas canvas)
+        {
+            base.Draw(canvas);
+
+            if (!string.IsNullOrEmpty(this._text))
+            {
+                float x = (Width / 2) - DEFAULT_TEXT_SIZE / 3;
+                float y = (Height / 2);
+
+                canvas.Rotate(90);
+                canvas.DrawText(this._text, y, -x, this._textPaint);
+            }
+        }
+    }
+}
---
+++ Seeker/Seeker/Output/VerticalText.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Seeker.Output
+{
+    public class VerticalText : View
+    {
+        public static BindableProperty ValueProperty = BindableProperty.Create(
+            nameof(Value), typeof(string), typeof(string), null, BindingMode.TwoWay, null, (bindable, oldValue, newValue) => { }
+        );
+
+        public string Value
+        {
+            get => (string)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+    }
+}
