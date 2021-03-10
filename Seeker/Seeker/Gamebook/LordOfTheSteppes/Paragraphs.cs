62ac36e5f1bd2f72e933e948c360b759fa23e5d1
mig1023 <mig1023@yandex.ru>
2021-03-10 20:13:23 +0300

    forgotten OnlyIf param

--- Seeker/Seeker/Gamebook/LordOfTheSteppes/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/LordOfTheSteppes/Paragraphs.cs
@@ -25,6 +25,7 @@ namespace Seeker.Gamebook.LordOfTheSteppes
                 {
                     Destination = Game.Xml.IntParse(xmlOption.Attributes["Destination"]),
                     Text = Game.Xml.StringParse(xmlOption.Attributes["Text"], defaultText: "Далее"),
+                    OnlyIf = Game.Xml.StringParse(xmlOption.Attributes["OnlyIf"]),
                     Aftertext = Game.Xml.StringParse(xmlOption.Attributes["Aftertext"]),
                 };
 
