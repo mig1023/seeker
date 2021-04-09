c806e4058df23d0abfa84902d675e025355d1576
mig1023 <mig1023@yandex.ru>
2021-04-09 22:37:13 +0300

    last hokku gamebook (start)

---
+++ Seeker/Seeker/Gamebook/LastHokku/Character.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Seeker.Gamebook.LastHokku
+{
+    class Character : Abstract.ICharacter
+    {
+        public static Character Protagonist = new Gamebook.LastHokku.Character();
+
+        public string Name { get; set; }
+
+        public List<string> Hokku { get; set; }
+
+        public void Init()
+        {
+            Name = String.Empty;
+            Hokku = new List<string>();
+        }
+
+        public Character Clone()
+        {
+            return new Character() {
+                Name = this.Name,
+            };
+        }
+
+        public string Save() => String.Empty;
+
+        public void Load(string saveLine) { }
+    }
+}
