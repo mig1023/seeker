a1be763663cbc956299d40ce3da26aec1290032a
mig1023 <mig1023@yandex.ru>
2021-04-09 22:45:28 +0300

    hokku reformat

--- Seeker/Seeker/Gamebook/LastHokku/Actions.cs
+++ Seeker/Seeker/Gamebook/LastHokku/Actions.cs
@@ -58,6 +58,22 @@ namespace Seeker.Gamebook.LastHokku
             if (!Constants.GetParagraphsWithoutHokkuCreation().Contains(Game.Data.CurrentParagraphID))
                 Character.Protagonist.Hokku.Add(option);
 
+            if (Character.Protagonist.Hokku.Count >= 7)
+            {
+                List<string> oldHokku = Character.Protagonist.Hokku;
+
+                oldHokku[2] = new System.Globalization.CultureInfo("ru-RU", false).TextInfo.ToTitleCase(oldHokku[2]);
+
+                List<string> newHokku = new List<string>
+                {
+                    String.Format("{0} {1}", oldHokku[0], oldHokku[1]),
+                    String.Format("{0} {1}", oldHokku[2], oldHokku[3]),
+                    String.Format("{0} {1} {2}", oldHokku[4], oldHokku[5], oldHokku[6]),
+                };
+
+                Character.Protagonist.Hokku = newHokku;
+            }
+
             return String.Join("\n", Character.Protagonist.Hokku);
         }
     }
