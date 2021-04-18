f273ea3a38073ca2ff67c08a12ccb36715e30e0a
mig1023 <mig1023@yandex.ru>
2021-04-18 19:06:19 +0300

    aftertext fix (those who)

--- Seeker/Seeker/Gamebook/ThoseWhoAreAboutToDie/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/ThoseWhoAreAboutToDie/Paragraphs.cs
@@ -27,6 +27,7 @@ namespace Seeker.Gamebook.ThoseWhoAreAboutToDie
                 {
                     Text = Game.Xml.StringParse(xmlOption.Attributes["Text"], defaultText: "Далее"),
                     OnlyIf = Game.Xml.StringParse(xmlOption.Attributes["OnlyIf"]),
+                    Aftertext = Game.Xml.StringParse(xmlOption.Attributes["Aftertext"]),
                 };
 
                 if (int.TryParse(xmlOption.Attributes["Destination"].Value, out int _))
