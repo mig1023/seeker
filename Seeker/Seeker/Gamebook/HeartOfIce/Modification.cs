c39d803d4e94358de7f5c86123fa3cb9b50d8f2d
mig1023 <mig1023@yandex.ru>
2021-04-23 22:47:00 +0300

    skills by modification

--- Seeker/Seeker/Gamebook/HeartOfIce/Modification.cs
+++ Seeker/Seeker/Gamebook/HeartOfIce/Modification.cs
@@ -8,7 +8,21 @@ namespace Seeker.Gamebook.HeartOfIce
     {
         public string Name { get; set; }
         public int Value { get; set; }
+        public string ValueString { get; set; }
 
-        public void Do() { }
+        public void Do()
+        {
+            if (Name == "Skill")
+                Character.Protagonist.Skills.Add(ValueString);
+
+            else
+            {
+                int currentValue = (int)Character.Protagonist.GetType().GetProperty(Name).GetValue(Character.Protagonist, null);
+
+                currentValue += Value;
+
+                Character.Protagonist.GetType().GetProperty(Name).SetValue(Character.Protagonist, currentValue);
+            }
+        }
     }
 }
