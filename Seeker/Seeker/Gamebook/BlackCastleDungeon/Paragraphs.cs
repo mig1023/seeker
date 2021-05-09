f390c1fe1455e64a1acbb43cad8b4efbdd588596
mig1023 <mig1023@yandex.ru>
2021-05-09 18:27:26 +0300

    aftertext fix

--- Seeker/Seeker/Gamebook/BlackCastleDungeon/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/BlackCastleDungeon/Paragraphs.cs
@@ -27,6 +27,7 @@ namespace Seeker.Gamebook.BlackCastleDungeon
                     Destination = Game.Xml.IntParse(xmlOption.Attributes["Destination"]),
                     Text = Game.Xml.StringParse(xmlOption.Attributes["Text"]),
                     OnlyIf = Game.Xml.StringParse(xmlOption.Attributes["OnlyIf"]),
+                    Aftertext = Game.Xml.StringParse(xmlOption.Attributes["Aftertext"]),
                 };
 
                 if (xmlOption.Attributes["Do"] != null)
