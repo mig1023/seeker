3a714a4194902ab64657315a45be49fbab0d5221
mig1023 <mig1023@yandex.ru>
2021-05-14 22:21:03 +0300

    parse paragraphs fix

--- Seeker/Seeker/Gamebook/StainlessSteelRat/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/StainlessSteelRat/Paragraphs.cs
@@ -9,5 +9,34 @@ using Seeker.Game;
 
 namespace Seeker.Gamebook.StainlessSteelRat
 {
-    class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs { }
+    class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs
+    {
+        public override Game.Paragraph Get(int id, XmlNode xmlParagraph)
+        {
+            Game.Paragraph paragraph = new Game.Paragraph();
+
+            paragraph.Options = new List<Option>();
+
+            foreach (XmlNode xmlOption in xmlParagraph.SelectNodes("Options/Option"))
+            {
+                Option option = new Option
+                {
+                    Text = Game.Xml.StringParse(xmlOption.Attributes["Text"]),
+                    Aftertext = Game.Xml.StringParse(xmlOption.Attributes["Aftertext"]),
+                };
+
+                if (int.TryParse(xmlOption.Attributes["Destination"].Value, out int _))
+                    option.Destination = Game.Xml.IntParse(xmlOption.Attributes["Destination"]);
+                else
+                {
+                    List<string> destinations = xmlOption.Attributes["Destination"].Value.Split(',').ToList<string>();
+                    option.Destination = int.Parse(destinations[random.Next(destinations.Count())]);
+                }
+
+                paragraph.Options.Add(option);
+            }
+
+            return paragraph;
+        }
+    }
 }
