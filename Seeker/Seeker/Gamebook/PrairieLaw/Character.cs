fc89f008461d03f5e8c0b44111cbb78ce3b6a54a
mig1023 <mig1023@yandex.ru>
2021-05-21 17:53:16 +0300

    character params

--- Seeker/Seeker/Gamebook/PrairieLaw/Character.cs
+++ Seeker/Seeker/Gamebook/PrairieLaw/Character.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Seeker.Gamebook.PrairieLaw
@@ -10,13 +11,144 @@ namespace Seeker.Gamebook.PrairieLaw
 
         public string Name { get; set; }
 
-        public void Init()
+        private int _skill;
+        public int MaxSkill { get; set; }
+        public int Skill
         {
-            Name = String.Empty;
+            get => _skill;
+            set
+            {
+                if (value > MaxSkill)
+                    _skill = MaxSkill;
+                else if (value < 0)
+                    _skill = 0;
+                else
+                    _skill = value;
+            }
         }
 
-        public string Save() => String.Empty;
+        private int _strength;
+        public int MaxStrength { get; set; }
+        public int Strength
+        {
+            get => _strength;
+            set
+            {
+                if (value > MaxStrength)
+                    _strength = MaxStrength;
+                else if (value < 0)
+                    _strength = 0;
+                else
+                    _strength = value;
+            }
+        }
 
-        public void Load(string saveLine) { }
+        private int _charm;
+        public int Charm
+        {
+            get => _charm;
+            set
+            {
+                if (value < 2)
+                    _charm = 2;
+                else
+                    _charm = value;
+            }
+        }
+
+        private int _dollars;
+        public int Dollars
+        {
+            get => _dollars;
+            set
+            {
+                if (value < 0)
+                    _dollars = 0;
+                else
+                    _dollars = value;
+            }
+        }
+        
+        private int _cartridges;
+        public int Сartridges
+        {
+            get => _cartridges;
+            set
+            {
+                if (value < 0)
+                    _cartridges = 0;
+                else
+                    _cartridges = value;
+            }
+        }
+
+        public Dictionary<int, bool> Luck { get; set; }
+
+        public void Init()
+        {
+            int dice = Game.Dice.Roll(dices: 2);
+
+            MaxSkill = Constants.Skills()[dice];
+            Skill = MaxSkill;
+            MaxStrength = Constants.Strengths()[dice];
+            Strength = MaxStrength;
+            Charm = Constants.Charms()[dice];
+
+            Dollars = 15;
+            Сartridges = 6;
+
+            Luck = new Dictionary<int, bool>
+            {
+                [1] = true,
+                [2] = true,
+                [3] = true,
+                [4] = true,
+                [5] = true,
+                [6] = true,
+            };
+
+            for (int i = 0; i < 2; i++)
+                Luck[Game.Dice.Roll()] = false;
+        }
+
+        public Character Clone() => new Character()
+        {
+            Name = this.Name,
+            MaxSkill = this.MaxSkill,
+            Skill = this.Skill,
+            MaxStrength = this.MaxStrength,
+            Strength = this.Strength,
+            Charm = this.Charm,
+            Dollars = this.Dollars,
+            Сartridges = this.Сartridges,
+        };
+
+        public string Save()
+        {
+            List<string> lucks = new List<string>();
+
+            foreach (bool luck in Luck.Values.ToList())
+                lucks.Add(luck ? "1" : "0");
+
+            return String.Join("|", MaxSkill, Skill, MaxStrength, Strength, Charm, Dollars, Сartridges, String.Join(",", lucks));
+        }
+
+        public void Load(string saveLine)
+        {
+            string[] save = saveLine.Split('|');
+
+            MaxSkill = int.Parse(save[0]);
+            Skill = int.Parse(save[1]);
+            MaxStrength = int.Parse(save[2]);
+            Strength = int.Parse(save[3]);
+            Charm = int.Parse(save[4]);
+            Dollars = int.Parse(save[5]);
+            Сartridges = int.Parse(save[6]);
+
+            string[] lucks = save[7].Split(',');
+
+            for (int i = 0; i < 6; i++)
+                Luck[i + 1] = (lucks[i] == "1");
+        }
     }
 }
--- Seeker/Seeker/Gamebook/PrairieLaw/Constants.cs
+++ Seeker/Seeker/Gamebook/PrairieLaw/Constants.cs
@@ -19,5 +19,69 @@ namespace Seeker.Gamebook.PrairieLaw
         public string GetButtonsColor(ButtonTypes type) => (ButtonsColors.ContainsKey(type) ? ButtonsColors[type] : String.Empty);
 
         public string GetColor(Game.Data.ColorTypes type) => (Colors.ContainsKey(type) ? Colors[type] : String.Empty);
+
+        public override List<int> GetParagraphsWithoutStatuses() => new List<int> { 0 };
+
+        public static Dictionary<int, int> Skills() => new Dictionary<int, int>
+        {
+            [2] = 8,
+            [3] = 10,
+            [4] = 12,
+            [5] = 9,
+            [6] = 11,
+            [7] = 9,
+            [8] = 10,
+            [9] = 8,
+            [10] = 9,
+            [11] = 10,
+            [12] = 11
+        };
+
+        public static Dictionary<int, int> Strengths() => new Dictionary<int, int>
+        {
+            [2] = 22,
+            [3] = 20,
+            [4] = 16,
+            [5] = 18,
+            [6] = 20,
+            [7] = 20,
+            [8] = 16,
+            [9] = 24,
+            [10] = 22,
+            [11] = 18,
+            [12] = 20
+        };
+
+        public static Dictionary<int, int> Charms() => new Dictionary<int, int>
+        {
+            [2] = 8,
+            [3] = 6,
+            [4] = 5,
+            [5] = 8,
+            [6] = 6,
+            [7] = 7,
+            [8] = 7,
+            [9] = 7,
+            [10] = 6,
+            [11] = 7,
+            [12] = 5
+        };
+
+        public static Dictionary<int, string> LuckList() => new Dictionary<int, string>
+        {
+            [1] = "①",
+            [2] = "②",
+            [3] = "③",
+            [4] = "④",
+            [5] = "⑤",
+            [6] = "⑥",
+
+            [11] = "❶",
+            [12] = "❷",
+            [13] = "❸",
+            [14] = "❹",
+            [15] = "❺",
+            [16] = "❻",
+        };
     }
 }
