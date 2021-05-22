3b7f5cb5965186740bbb9e028f3202748664bec3
mig1023 <mig1023@yandex.ru>
2021-05-22 16:03:43 +0300

    modification extended (octopus)

--- Seeker/Seeker/Gamebook/OctopusIsland/Modification.cs
+++ Seeker/Seeker/Gamebook/OctopusIsland/Modification.cs
@@ -4,15 +4,8 @@ using System.Text;
 
 namespace Seeker.Gamebook.OctopusIsland
 {
-    class Modification : Prototypes.Modification, Abstract.IModification
+    class Modification : Prototypes.ModificationExtended, Abstract.IModification
     {
-        public override void Do()
-        {
-            int currentValue = (int)Character.Protagonist.GetType().GetProperty(Name).GetValue(Character.Protagonist, null);
-
-            currentValue += Value;
-
-            Character.Protagonist.GetType().GetProperty(Name).SetValue(Character.Protagonist, currentValue);
-        }
+        public override void Do() => InnerDo(Character.Protagonist);
     }
 }
