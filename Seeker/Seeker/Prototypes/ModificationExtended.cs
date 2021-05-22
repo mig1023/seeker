861eaefd7d6989b5388328f0e58ed53d251c62e2
mig1023 <mig1023@yandex.ru>
2021-05-22 15:58:50 +0300

    ModificationExtended prototype

---
+++ Seeker/Seeker/Prototypes/ModificationExtended.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Seeker.Prototypes
+{
+    class ModificationExtended : Modification
+    {
+        public void InnerDo(Abstract.ICharacter Character)
+        {
+            if (Name == "Trigger")
+                Game.Option.Trigger(ValueString);
+
+            else if (Name == "Healing")
+                Game.Healing.Add(ValueString);
+
+            else
+            {
+                int currentValue = (int)Character.GetType().GetProperty(Name).GetValue(Character, null);
+
+                if (Name.StartsWith("Max"))
+                {
+                    string normalParam = Name.Remove(0, 3);
+
+                    int normalValue = (int)Character.GetType().GetProperty(normalParam).GetValue(Character, null);
+
+                    if ((normalValue + Value) > currentValue)
+                        Character.GetType().GetProperty(Name).SetValue(Character, currentValue + Value);
+
+                    Character.GetType().GetProperty(normalParam).SetValue(Character, currentValue + Value);
+                }
+                else
+                {
+                    currentValue += Value;
+
+                    Character.GetType().GetProperty(Name).SetValue(Character, currentValue);
+                }
+            }
+        }
+    }
+}
