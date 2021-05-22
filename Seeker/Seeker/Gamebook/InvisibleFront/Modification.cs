1eefc00331ab63b2f47ccdc3ad6f75fe12a112e8
mig1023 <mig1023@yandex.ru>
2021-05-22 16:03:23 +0300

    modification extended (invisible front)

--- Seeker/Seeker/Gamebook/InvisibleFront/Modification.cs
+++ Seeker/Seeker/Gamebook/InvisibleFront/Modification.cs
@@ -4,28 +4,18 @@ using System.Text;
 
 namespace Seeker.Gamebook.InvisibleFront
 {
-    class Modification : Prototypes.Modification, Abstract.IModification
+    class Modification : Prototypes.ModificationExtended, Abstract.IModification
     {
         public override void Do()
         {
-            
-            if (Name == "Trigger")
-                Game.Option.Trigger(ValueString);
-
-            else if (Name == "Meeting")
+            if (Name == "Meeting")
                 Game.Option.Trigger(Game.Dice.Roll() > 3 ? "предатель" : "не предатель");
 
             else if (Name == "Apartment")
                 Game.Option.Trigger(Constants.GetApartments()[Game.Dice.Roll() - 1]);
-   
+
             else
-            {
-                int currentValue = (int)Character.Protagonist.GetType().GetProperty(Name).GetValue(Character.Protagonist, null);
-
-                currentValue += Value;
-
-                Character.Protagonist.GetType().GetProperty(Name).SetValue(Character.Protagonist, currentValue);
-            }
+                InnerDo(Character.Protagonist);
         }
     }
 }
