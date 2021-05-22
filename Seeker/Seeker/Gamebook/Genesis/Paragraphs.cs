5f2b20cc9325eec6eb83a0bbe3f02ab0faa837b7
mig1023 <mig1023@yandex.ru>
2021-05-22 16:09:57 +0300

    paragraph template (genesis)

--- Seeker/Seeker/Gamebook/Genesis/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/Genesis/Paragraphs.cs
@@ -9,32 +9,21 @@ using Seeker.Game;
 
 namespace Seeker.Gamebook.Genesis
 {
-    class Paragraphs : Abstract.IParagraphs
+    class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs
     {
-        public Game.Paragraph Get(int id, XmlNode xmlParagraph)
+        public override Game.Paragraph Get(int id, XmlNode xmlParagraph)
         {
-            Game.Paragraph paragraph = new Game.Paragraph();
-
-            paragraph.Options = new List<Option>();
-            paragraph.Actions = new List<Abstract.IActions>();
-            paragraph.Modification = new List<Abstract.IModification>();
+            Game.Paragraph paragraph = ParagraphTemplate(xmlParagraph);
 
             foreach (XmlNode xmlOption in xmlParagraph.SelectNodes("Options/Option"))
             {
-                Option option = new Option
-                {
-                    Destination = Game.Xml.IntParse(xmlOption.Attributes["Destination"]),
-                    Text = Game.Xml.StringParse(xmlOption.Attributes["Text"]),
-                    OnlyIf = Game.Xml.StringParse(xmlOption.Attributes["OnlyIf"]),
-                    Aftertext = Game.Xml.StringParse(xmlOption.Attributes["Aftertext"]),
-                };
+                Option option = OptionsTemplate(xmlOption);
 
                 if (xmlOption.Attributes["Do"] != null)
                     option.Do = Game.Xml.ModificationParse(xmlOption, new Modification(), name: "Do");
 
                 paragraph.Options.Add(option);
             }
-
 
             foreach (XmlNode xmlAction in xmlParagraph.SelectNodes("Actions/Action"))
             {
@@ -53,10 +42,6 @@ namespace Seeker.Gamebook.Genesis
 
             foreach (XmlNode xmlModification in xmlParagraph.SelectNodes("Modifications/Modification"))
                 paragraph.Modification.Add(Game.Xml.ModificationParse(xmlModification, new Modification()));
-
-            paragraph.Trigger = Game.Xml.StringParse(xmlParagraph["Triggers"]);
-            paragraph.RemoveTrigger = Game.Xml.StringParse(xmlParagraph["RemoveTriggers"]);
-            paragraph.Image = Game.Xml.StringParse(xmlParagraph["Image"]);
 
             return paragraph;
         }
