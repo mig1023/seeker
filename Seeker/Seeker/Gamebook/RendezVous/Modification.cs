e0fa2dc1068adeaa92a7fc3f924655e22cbfb29e
mig1023 <mig1023@yandex.ru>
2021-05-22 16:04:28 +0300

    modification extended (rendezvous)

--- Seeker/Seeker/Gamebook/RendezVous/Modification.cs
+++ Seeker/Seeker/Gamebook/RendezVous/Modification.cs
@@ -4,15 +4,8 @@ using System.Text;
 
 namespace Seeker.Gamebook.RendezVous
 {
-    class Modification : Prototypes.Modification, Abstract.IModification
+    class Modification : Prototypes.ModificationExtended, Abstract.IModification
     {
-        public override void Do()
-        {
-            int currentValue = (int)Character.Protagonist.GetType().GetProperty(Name).GetValue(Character.Protagonist, null);
-
-            currentValue += Value;
-
-            Character.Protagonist.GetType().GetProperty(Name).SetValue(Character.Protagonist, currentValue);
-        }
+        public override void Do() => InnerDo(Character.Protagonist);
     }
 }
