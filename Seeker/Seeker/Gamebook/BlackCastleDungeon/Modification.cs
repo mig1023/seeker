37c926479310baa6a7643f82f49227b67294f1e0
mig1023 <mig1023@yandex.ru>
2021-05-22 16:00:30 +0300

    modification extended (black castle)

--- Seeker/Seeker/Gamebook/BlackCastleDungeon/Modification.cs
+++ Seeker/Seeker/Gamebook/BlackCastleDungeon/Modification.cs
@@ -4,38 +4,14 @@ using System.Text;
 
 namespace Seeker.Gamebook.BlackCastleDungeon
 {
-    class Modification : Prototypes.Modification, Abstract.IModification
+    class Modification : Prototypes.ModificationExtended, Abstract.IModification
     {
         public override void Do()
         {
-            if (Name == "Healing")
-                Game.Healing.Add(ValueString);
-
-            else if (Name == "RemoveSpell")
+            if (Name == "RemoveSpell")
                 Character.Protagonist.Spells.RemoveAt(Character.Protagonist.Spells.IndexOf(ValueString));
-
             else
-            {
-                int currentValue = (int)Character.Protagonist.GetType().GetProperty(Name).GetValue(Character.Protagonist, null);
-
-                if (Name.StartsWith("Max"))
-                {
-                    string normalParam = Name.Remove(0, 3);
-
-                    int normalValue = (int)Character.Protagonist.GetType().GetProperty(normalParam).GetValue(Character.Protagonist, null);
-
-                    if ((normalValue + Value) > currentValue)
-                        Character.Protagonist.GetType().GetProperty(Name).SetValue(Character.Protagonist, currentValue + Value);
-
-                    Character.Protagonist.GetType().GetProperty(normalParam).SetValue(Character.Protagonist, currentValue + Value);
-                }
-                else
-                {
-                    currentValue += Value;
-
-                    Character.Protagonist.GetType().GetProperty(Name).SetValue(Character.Protagonist, currentValue);
-                }
-            }
+                InnerDo(Character.Protagonist);
         }
     }
 }
