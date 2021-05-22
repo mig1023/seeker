31cef7be97d334534404f3dd760963472c766bf4
mig1023 <mig1023@yandex.ru>
2021-05-22 16:08:35 +0300

    paragraph template (captain)

--- Seeker/Seeker/Gamebook/CaptainSheltonsSecret/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/CaptainSheltonsSecret/Paragraphs.cs
@@ -10,27 +10,14 @@ using Seeker.Game;
 
 namespace Seeker.Gamebook.CaptainSheltonsSecret
 {
-    class Paragraphs : Abstract.IParagraphs
+    class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs
     {
-        public Game.Paragraph Get(int id, XmlNode xmlParagraph)
+        public override Game.Paragraph Get(int id, XmlNode xmlParagraph)
         {
-            Game.Paragraph paragraph = new Game.Paragraph();
-
-            paragraph.Options = new List<Option>();
-            paragraph.Actions = new List<Abstract.IActions>();
-            paragraph.Modification = new List<Abstract.IModification>();
+            Game.Paragraph paragraph = ParagraphTemplate(xmlParagraph);
 
             foreach (XmlNode xmlOption in xmlParagraph.SelectNodes("Options/Option"))
-            {
-                Option option = new Option
-                {
-                    Destination = Game.Xml.IntParse(xmlOption.Attributes["Destination"]),
-                    Text = Game.Xml.StringParse(xmlOption.Attributes["Text"]),
-                    OnlyIf = Game.Xml.StringParse(xmlOption.Attributes["OnlyIf"]),
-                };
-
-                paragraph.Options.Add(option);
-            }
+                paragraph.Options.Add(OptionsTemplate(xmlOption));
 
             foreach (XmlNode xmlAction in xmlParagraph.SelectNodes("Actions/Action"))
             {
@@ -97,8 +84,6 @@ namespace Seeker.Gamebook.CaptainSheltonsSecret
 
             foreach (XmlNode xmlModification in xmlParagraph.SelectNodes("Modifications/Modification"))
                 paragraph.Modification.Add(Game.Xml.ModificationParse(xmlModification, new Modification()));
-
-            paragraph.Trigger = Game.Xml.StringParse(xmlParagraph["Triggers"]);
 
             return paragraph;
         }
