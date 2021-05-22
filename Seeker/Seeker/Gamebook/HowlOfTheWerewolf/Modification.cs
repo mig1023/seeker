e8376a878934abda39efb7e899e1f6d8372b0404
mig1023 <mig1023@yandex.ru>
2021-05-22 16:02:54 +0300

    modification extended (werewolf)

--- Seeker/Seeker/Gamebook/HowlOfTheWerewolf/Modification.cs
+++ Seeker/Seeker/Gamebook/HowlOfTheWerewolf/Modification.cs
@@ -4,41 +4,18 @@ using System.Text;
 
 namespace Seeker.Gamebook.HowlOfTheWerewolf
 {
-    class Modification : Prototypes.Modification, Abstract.IModification
+    class Modification : Prototypes.ModificationExtended, Abstract.IModification
     {
         public override void Do()
         {
-            if (Name == "Healing")
-                Game.Healing.Add(ValueString);
-
-            else if (Name == "Back")
+            if (Name == "Back")
                 Character.Protagonist.WayBack = Value;
 
             else if (Name == "VanRichtenIsDead")
                 Character.Protagonist.VanRichten = 0;
 
             else
-            {
-                int currentValue = (int)Character.Protagonist.GetType().GetProperty(Name).GetValue(Character.Protagonist, null);
-
-                if (Name.StartsWith("Max"))
-                {
-                    string normalParam = Name.Remove(0, 3);
-
-                    int normalValue = (int)Character.Protagonist.GetType().GetProperty(normalParam).GetValue(Character.Protagonist, null);
-
-                    if ((normalValue + Value) > currentValue)
-                        Character.Protagonist.GetType().GetProperty(Name).SetValue(Character.Protagonist, currentValue + Value);
-
-                    Character.Protagonist.GetType().GetProperty(normalParam).SetValue(Character.Protagonist, currentValue + Value);
-                }
-                else
-                {
-                    currentValue += Value;
-
-                    Character.Protagonist.GetType().GetProperty(Name).SetValue(Character.Protagonist, currentValue);
-                }
-            }
+                InnerDo(Character.Protagonist);
         }
     }
 }
