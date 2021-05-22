93cd79a8519adfadb9eb4c7818a4ae5d65496381
mig1023 <mig1023@yandex.ru>
2021-05-22 16:01:11 +0300

    modification extended (creature)

--- Seeker/Seeker/Gamebook/CreatureOfHavoc/Modification.cs
+++ Seeker/Seeker/Gamebook/CreatureOfHavoc/Modification.cs
@@ -4,20 +4,8 @@ using System.Text;
 
 namespace Seeker.Gamebook.CreatureOfHavoc
 {
-    class Modification : Prototypes.Modification, Abstract.IModification
+    class Modification : Prototypes.ModificationExtended, Abstract.IModification
     {
-        public bool Restore { get; set; }
-
-        public override void Do()
-        {
-            int currentValue = (int)Character.Protagonist.GetType().GetProperty(Name).GetValue(Character.Protagonist, null);
-
-            if (Restore)
-                currentValue = (int)Character.Protagonist.GetType().GetProperty("Max" + Name).GetValue(Character.Protagonist, null);
-            else
-                currentValue += Value;
-
-            Character.Protagonist.GetType().GetProperty(Name).SetValue(Character.Protagonist, currentValue);
-        }
+        public override void Do() => InnerDo(Character.Protagonist);
     }
 }
