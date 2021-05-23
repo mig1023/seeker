bee98fffd12d4ffb44b6eac60be8aeec83965638
mig1023 <mig1023@yandex.ru>
2021-05-23 19:43:15 +0300

    constants fixes (black castle)

--- Seeker/Seeker/Gamebook/BlackCastleDungeon/Constants.cs
+++ Seeker/Seeker/Gamebook/BlackCastleDungeon/Constants.cs
@@ -9,25 +9,19 @@ namespace Seeker.Gamebook.BlackCastleDungeon
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
-        static Dictionary<ButtonTypes, string> ButtonsColors = new Dictionary<ButtonTypes, string>
+        public override Dictionary<ButtonTypes, string> ButtonsColors() => new Dictionary<ButtonTypes, string>
         {
             [ButtonTypes.Main] = "#151515",
             [ButtonTypes.Action] = "#3f3f3f",
             [ButtonTypes.Option] = "#696969",
         };
 
-        static Dictionary<ColorTypes, string> Colors = new Dictionary<ColorTypes, string>
+        public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
         {
             [ColorTypes.StatusBar] = "#2a2a2a",
         };
 
-        public string GetButtonsColor(ButtonTypes type) => (ButtonsColors.ContainsKey(type) ? ButtonsColors[type] : String.Empty);
-
-        public string GetColor(Game.Data.ColorTypes type) => (Colors.ContainsKey(type) ? Colors[type] : String.Empty);
-
         public static List<int> GetParagraphsWithoutStaticsButtons() => new List<int> { 0, 619 };
-
-        public override List<int> GetParagraphsWithoutStatuses() => new List<int> { 0 };
 
         public static List<string> StaticSpells() => new List<string> { "ЗАКЛЯТИЕ КОПИИ", "ЗАКЛЯТИЕ СИЛЫ", "ЗАКЛЯТИЕ СЛАБОСТИ" };
     }
