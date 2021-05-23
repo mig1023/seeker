7aaa5dad916f9887a855d768458a41ef5f9c4da8
mig1023 <mig1023@yandex.ru>
2021-05-23 19:55:22 +0300

    constants fixes (those who are about to die)

--- Seeker/Seeker/Gamebook/ThoseWhoAreAboutToDie/Constants.cs
+++ Seeker/Seeker/Gamebook/ThoseWhoAreAboutToDie/Constants.cs
@@ -9,7 +9,7 @@ namespace Seeker.Gamebook.ThoseWhoAreAboutToDie
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
-        static Dictionary<ButtonTypes, string> ButtonsColors = new Dictionary<ButtonTypes, string>
+        public override Dictionary<ButtonTypes, string> ButtonsColors() => new Dictionary<ButtonTypes, string>
         {
             [ButtonTypes.Main] = "#fcdd76",
             [ButtonTypes.Action] = "#fcdd76",
@@ -18,16 +18,10 @@ namespace Seeker.Gamebook.ThoseWhoAreAboutToDie
             [ButtonTypes.Continue] = "#fdeeba",
         };
 
-        static Dictionary<ColorTypes, string> Colors = new Dictionary<ColorTypes, string>
+        public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
         {
             [ColorTypes.StatusBar] = "#fce391",
             [ColorTypes.StatusFont] = "#000000",
         };
-
-        public string GetButtonsColor(ButtonTypes type) => (ButtonsColors.ContainsKey(type) ? ButtonsColors[type] : String.Empty);
-
-        public string GetColor(Game.Data.ColorTypes type) => (Colors.ContainsKey(type) ? Colors[type] : String.Empty);
-
-        public override List<int> GetParagraphsWithoutStatuses() => new List<int> { 0 };
     }
 }
