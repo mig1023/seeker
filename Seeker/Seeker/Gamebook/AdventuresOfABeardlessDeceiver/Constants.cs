0ca7ce2c78e285d3e727d99bc9e03022f2e36bcf
mig1023 <mig1023@yandex.ru>
2021-05-23 19:42:50 +0300

    constants fixes (aldar)

--- Seeker/Seeker/Gamebook/AdventuresOfABeardlessDeceiver/Constants.cs
+++ Seeker/Seeker/Gamebook/AdventuresOfABeardlessDeceiver/Constants.cs
@@ -9,7 +9,7 @@ namespace Seeker.Gamebook.AdventuresOfABeardlessDeceiver
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
-        static Dictionary<ButtonTypes, string> ButtonsColors = new Dictionary<ButtonTypes, string>
+        public override Dictionary<ButtonTypes, string> ButtonsColors() => new Dictionary<ButtonTypes, string>
         {
             [ButtonTypes.Main] = "#5da130",
             [ButtonTypes.Action] = "#339933",
@@ -17,7 +17,7 @@ namespace Seeker.Gamebook.AdventuresOfABeardlessDeceiver
             [ButtonTypes.Continue] = "#b9cdab",
         };
 
-        static Dictionary<ColorTypes, string> Colors = new Dictionary<ColorTypes, string>
+        public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
         {
             [ColorTypes.Background] = "#dbeadc",
             [ColorTypes.ActionBox] = "#7cb281",
@@ -25,13 +25,7 @@ namespace Seeker.Gamebook.AdventuresOfABeardlessDeceiver
             [ColorTypes.AdditionalStatus] = "#99b999",
         };
 
-        public string GetButtonsColor(ButtonTypes type) => (ButtonsColors.ContainsKey(type) ? ButtonsColors[type] : String.Empty);
-
-        public string GetColor(Game.Data.ColorTypes type) => (Colors.ContainsKey(type) ? Colors[type] : String.Empty);
-
         public static List<int> GetParagraphsWithoutStaticsButtons() => new List<int> { 0, 30, 60, 90 };
-
-        public override List<int> GetParagraphsWithoutStatuses() => new List<int> { 0 };
 
         public static Dictionary<string, string> StatNames = new Dictionary<string, string>
         {
