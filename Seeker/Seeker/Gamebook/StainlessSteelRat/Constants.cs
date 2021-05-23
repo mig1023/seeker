d643abc8b9a8ddf343604e0aa6304e88270e0245
mig1023 <mig1023@yandex.ru>
2021-05-23 19:54:19 +0300

    constants fixes (rat)

--- Seeker/Seeker/Gamebook/StainlessSteelRat/Constants.cs
+++ Seeker/Seeker/Gamebook/StainlessSteelRat/Constants.cs
@@ -9,21 +9,16 @@ namespace Seeker.Gamebook.StainlessSteelRat
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
-        static Dictionary<ButtonTypes, string> ButtonsColors = new Dictionary<ButtonTypes, string>
+        public override Dictionary<ButtonTypes, string> ButtonsColors() => new Dictionary<ButtonTypes, string>
         {
             [ButtonTypes.Main] = "#738595",
             [ButtonTypes.Continue] = "#8f9daa",
         };
 
-
-        static Dictionary<ColorTypes, string> Colors = new Dictionary<ColorTypes, string>
+        public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
         {
             [ColorTypes.Background] = "#b9c2ca",
         };
-
-        public string GetButtonsColor(ButtonTypes type) => (ButtonsColors.ContainsKey(type) ? ButtonsColors[type] : String.Empty);
-
-        public string GetColor(Game.Data.ColorTypes type) => (Colors.ContainsKey(type) ? Colors[type] : String.Empty);
 
         public override Output.Interface.TextFontSize GetFontSize() => Output.Interface.TextFontSize.big;
     }
