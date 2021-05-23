07915655bd1d1b9361247dbe786456a75166e2c6
mig1023 <mig1023@yandex.ru>
2021-05-23 19:46:01 +0300

    constants fixes (sword)

--- Seeker/Seeker/Gamebook/FaithfulSwordOfTheKing/Constants.cs
+++ Seeker/Seeker/Gamebook/FaithfulSwordOfTheKing/Constants.cs
@@ -9,21 +9,17 @@ namespace Seeker.Gamebook.FaithfulSwordOfTheKing
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
-        static Dictionary<ButtonTypes, string> ButtonsColors = new Dictionary<ButtonTypes, string>
+        public override Dictionary<ButtonTypes, string> ButtonsColors() => new Dictionary<ButtonTypes, string>
         {
             [ButtonTypes.Main] = "#911",
             [ButtonTypes.Action] = "#ba2020",
             [ButtonTypes.Option] = "#696969",
         };
 
-        static Dictionary<ColorTypes, string> Colors = new Dictionary<ColorTypes, string>
+        public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
         {
             [ColorTypes.StatusBar] = "#870808",
         };
-
-        public string GetButtonsColor(ButtonTypes type) => (ButtonsColors.ContainsKey(type) ? ButtonsColors[type] : String.Empty);
-
-        public string GetColor(Game.Data.ColorTypes type) => (Colors.ContainsKey(type) ? Colors[type] : String.Empty);
 
         public override List<int> GetParagraphsWithoutStatuses() => new List<int> { 0, 660 };
 
