216d815e055ff79183a95fde6b61b8d4985bfd89
mig1023 <mig1023@yandex.ru>
2021-05-23 19:45:38 +0300

    constants fixes (dzungar war)

--- Seeker/Seeker/Gamebook/DzungarWar/Constants.cs
+++ Seeker/Seeker/Gamebook/DzungarWar/Constants.cs
@@ -9,7 +9,7 @@ namespace Seeker.Gamebook.DzungarWar
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
-        static Dictionary<ButtonTypes, string> ButtonsColors = new Dictionary<ButtonTypes, string>
+        public override Dictionary<ButtonTypes, string> ButtonsColors() => new Dictionary<ButtonTypes, string>
         {
             [ButtonTypes.Main] = "#4a8026",
             [ButtonTypes.Action] = "#339933",
@@ -17,17 +17,13 @@ namespace Seeker.Gamebook.DzungarWar
             [ButtonTypes.Continue] = "#a8c196",
         };
 
-        static Dictionary<ColorTypes, string> Colors = new Dictionary<ColorTypes, string>
+        public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
         {
             [ColorTypes.Background] = "#c3dcc6",
             [ColorTypes.ActionBox] = "#7cb281",
             [ColorTypes.StatusBar] = "#005100",
             [ColorTypes.AdditionalStatus] = "#99b999",
         };
-
-        public string GetButtonsColor(ButtonTypes type) => (ButtonsColors.ContainsKey(type) ? ButtonsColors[type] : String.Empty);
-
-        public string GetColor(Game.Data.ColorTypes type) => (Colors.ContainsKey(type) ? Colors[type] : String.Empty);
 
         public override List<int> GetParagraphsWithoutStatuses() => new List<int> { 0, 660 };
 
