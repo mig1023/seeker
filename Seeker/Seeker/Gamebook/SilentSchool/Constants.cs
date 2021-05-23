0518ff9df89ceb5cb74e99a3cd3456e80bd5bf7a
mig1023 <mig1023@yandex.ru>
2021-05-23 19:53:57 +0300

    constants fixes (school)

--- Seeker/Seeker/Gamebook/SilentSchool/Constants.cs
+++ Seeker/Seeker/Gamebook/SilentSchool/Constants.cs
@@ -9,22 +9,16 @@ namespace Seeker.Gamebook.SilentSchool
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
-        static Dictionary<ButtonTypes, string> ButtonsColors = new Dictionary<ButtonTypes, string>
+        public override Dictionary<ButtonTypes, string> ButtonsColors() => new Dictionary<ButtonTypes, string>
         {
             [ButtonTypes.Main] = "#434343",
             [ButtonTypes.Action] = "#555555",
             [ButtonTypes.Option] = "#686868",
         };
 
-        static Dictionary<ColorTypes, string> Colors = new Dictionary<ColorTypes, string>
+        public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
         {
             [ColorTypes.StatusBar] = "#2a2a2a",
         };
-
-        public string GetButtonsColor(ButtonTypes type) => (ButtonsColors.ContainsKey(type) ? ButtonsColors[type] : String.Empty);
-
-        public string GetColor(Game.Data.ColorTypes type) => (Colors.ContainsKey(type) ? Colors[type] : String.Empty);
-
-        public override List<int> GetParagraphsWithoutStatuses() => new List<int> { 0 };
     }
 }
