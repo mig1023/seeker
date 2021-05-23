4c38eac85fdf941876486a6794141c243b3bb12b
mig1023 <mig1023@yandex.ru>
2021-05-23 19:52:17 +0300

    constants fixes (octopus)

--- Seeker/Seeker/Gamebook/OctopusIsland/Constants.cs
+++ Seeker/Seeker/Gamebook/OctopusIsland/Constants.cs
@@ -9,22 +9,16 @@ namespace Seeker.Gamebook.OctopusIsland
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
-        static Dictionary<ButtonTypes, string> ButtonsColors = new Dictionary<ButtonTypes, string>
+        public override Dictionary<ButtonTypes, string> ButtonsColors() => new Dictionary<ButtonTypes, string>
         {
             [ButtonTypes.Main] = "#c93c20",
             [ButtonTypes.Action] = "#c93c20",
             [ButtonTypes.Option] = "#c93c20",
         };
 
-        static Dictionary<ColorTypes, string> Colors = new Dictionary<ColorTypes, string>
+        public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
         {
             [ColorTypes.StatusBar] = "#ce4f36",
         };
-
-        public string GetButtonsColor(ButtonTypes type) => (ButtonsColors.ContainsKey(type) ? ButtonsColors[type] : String.Empty);
-
-        public string GetColor(Game.Data.ColorTypes type) => (Colors.ContainsKey(type) ? Colors[type] : String.Empty);
-
-        public override List<int> GetParagraphsWithoutStatuses() => new List<int> { 0 };
     }
 }
