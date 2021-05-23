8705cea2ef53c058f2068e02a241d971b6840027
mig1023 <mig1023@yandex.ru>
2021-05-23 19:56:27 +0300

    constants fixes (three paths)

--- Seeker/Seeker/Gamebook/ThreePaths/Constants.cs
+++ Seeker/Seeker/Gamebook/ThreePaths/Constants.cs
@@ -9,22 +9,16 @@ namespace Seeker.Gamebook.ThreePaths
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
-        static Dictionary<ButtonTypes, string> ButtonsColors = new Dictionary<ButtonTypes, string>
+        public override Dictionary<ButtonTypes, string> ButtonsColors() => new Dictionary<ButtonTypes, string>
         {
             [ButtonTypes.Main] = "#009999",
             [ButtonTypes.Option] = "#009999",
             [ButtonTypes.Action] = "#005b5b",
         };
 
-        static Dictionary<ColorTypes, string> Colors = new Dictionary<ColorTypes, string>
+        public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
         {
             [ColorTypes.StatusBar] = "#007a7a",
         };
-
-        public string GetButtonsColor(ButtonTypes type) => (ButtonsColors.ContainsKey(type) ? ButtonsColors[type] : String.Empty);
-
-        public string GetColor(Game.Data.ColorTypes type) => (Colors.ContainsKey(type) ? Colors[type] : String.Empty);
-
-        public override List<int> GetParagraphsWithoutStatuses() => new List<int> { 0 };
     }
 }
