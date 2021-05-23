f0b2362d1ba193593ceee47517045810d07c6c64
mig1023 <mig1023@yandex.ru>
2021-05-23 19:52:41 +0300

    constants fixes (pensioner)

--- Seeker/Seeker/Gamebook/PensionerSimulator/Constants.cs
+++ Seeker/Seeker/Gamebook/PensionerSimulator/Constants.cs
@@ -9,16 +9,10 @@ namespace Seeker.Gamebook.PensionerSimulator
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
-        static Dictionary<ButtonTypes, string> ButtonsColors = new Dictionary<ButtonTypes, string>
+        public override Dictionary<ButtonTypes, string> ButtonsColors() => new Dictionary<ButtonTypes, string>
         {
             [ButtonTypes.Main] = "#030436",
         };
-
-        static Dictionary<ColorTypes, string> Colors = new Dictionary<ColorTypes, string>();
-
-        public string GetButtonsColor(ButtonTypes type) => (ButtonsColors.ContainsKey(type) ? ButtonsColors[type] : String.Empty);
-
-        public string GetColor(Game.Data.ColorTypes type) => (Colors.ContainsKey(type) ? Colors[type] : String.Empty);
 
         public override Output.Interface.TextFontSize GetFontSize() => Output.Interface.TextFontSize.big;
     }
