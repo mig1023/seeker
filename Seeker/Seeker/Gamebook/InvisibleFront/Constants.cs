005c9f9de3e712cfc829fde706ee3e9bdf7ba754
mig1023 <mig1023@yandex.ru>
2021-05-23 19:48:44 +0300

    constants fixes (invisible front)

--- Seeker/Seeker/Gamebook/InvisibleFront/Constants.cs
+++ Seeker/Seeker/Gamebook/InvisibleFront/Constants.cs
@@ -9,7 +9,7 @@ namespace Seeker.Gamebook.InvisibleFront
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
-        static Dictionary<ButtonTypes, string> ButtonsColors = new Dictionary<ButtonTypes, string>
+        public override Dictionary<ButtonTypes, string> ButtonsColors() => new Dictionary<ButtonTypes, string>
         {
             [ButtonTypes.Main] = "#d52b1e",
             [ButtonTypes.Option] = "#d52b1e",
@@ -17,18 +17,12 @@ namespace Seeker.Gamebook.InvisibleFront
             [ButtonTypes.Font] = "#eede49",
         };
 
-        static Dictionary<ColorTypes, string> Colors = new Dictionary<ColorTypes, string>
+        public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
         {
             [ColorTypes.Background] = "#ffdadb",
             [ColorTypes.StatusBar] = "#aa2218",
             [ColorTypes.StatusFont] = "#eede49",
         };
-
-        public string GetButtonsColor(ButtonTypes type) => (ButtonsColors.ContainsKey(type) ? ButtonsColors[type] : String.Empty);
-
-        public string GetColor(Game.Data.ColorTypes type) => (Colors.ContainsKey(type) ? Colors[type] : String.Empty);
-
-        public override List<int> GetParagraphsWithoutStatuses() => new List<int> { 0 };
 
         public static List<string> GetApartments() => new List<string> { "один", "два", "три", "один", "два", "три" };
     }
