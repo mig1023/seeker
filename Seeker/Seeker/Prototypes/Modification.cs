5eca2eb8e62b7f1798a8960ca921c04fff00187e
mig1023 <mig1023@yandex.ru>
2021-05-25 17:53:44 +0300

    single modification prototype

--- Seeker/Seeker/Prototypes/Modification.cs
+++ Seeker/Seeker/Prototypes/Modification.cs
@@ -10,6 +10,47 @@ namespace Seeker.Prototypes
         public int Value { get; set; }
         public string ValueString { get; set; }
 
+        public bool Empty { get; set; }
+        public bool Restore { get; set; }
+
         public virtual void Do() { }
+
+        public void InnerDo(Abstract.ICharacter Character)
+        {
+            if (Name == "Trigger")
+                Game.Option.Trigger(ValueString);
+
+            else if (Name == "Healing")
+                Game.Healing.Add(ValueString);
+
+            else
+            {
+                int currentValue = (int)Character.GetType().GetProperty(Name).GetValue(Character, null);
+
+                if (Restore)
+                    currentValue = (int)Character.GetType().GetProperty("Max" + Name).GetValue(Character, null);
+
+                else if (Empty)
+                    currentValue = 0;
+
+                if (Name.StartsWith("Max"))
+                {
+                    string normalParam = Name.Remove(0, 3);
+
+                    int normalValue = (int)Character.GetType().GetProperty(normalParam).GetValue(Character, null);
+
+                    if ((normalValue + Value) > currentValue)
+                        Character.GetType().GetProperty(Name).SetValue(Character, currentValue + Value);
+
+                    Character.GetType().GetProperty(normalParam).SetValue(Character, currentValue + Value);
+                }
+                else
+                {
+                    currentValue += Value;
+
+                    Character.GetType().GetProperty(Name).SetValue(Character, currentValue);
+                }
+            }
+        }
     }
 }
