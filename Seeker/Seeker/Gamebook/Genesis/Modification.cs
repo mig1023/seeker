8c9b94308700894c6e1a3f590773e15c541b8f84
mig1023 <mig1023@yandex.ru>
2021-05-25 17:56:46 +0300

    modification fix (genesis)

--- Seeker/Seeker/Gamebook/Genesis/Modification.cs
+++ Seeker/Seeker/Gamebook/Genesis/Modification.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Seeker.Gamebook.Genesis
 {
-    class Modification : Prototypes.ModificationExtended, Abstract.IModification
+    class Modification : Prototypes.Modification, Abstract.IModification
     {
         public override void Do() => InnerDo(Character.Protagonist);
     }
