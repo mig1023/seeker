166c8647bae051861bc8520d36a0eab52a311605
mig1023 <mig1023@yandex.ru>
2021-05-26 18:07:56 +0300

    character prototype

---
+++ Seeker/Seeker/Prototypes/Character.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Seeker.Prototypes
+{
+    class Character
+    {
+        public string Name { get; set; }
+
+        public virtual void Init() => Name = String.Empty;
+
+        public virtual string Save() => String.Empty;
+
+        public virtual void Load(string saveLine) => Game.Other.DoNothing();
+    }
+}
