432f4e6e5d1993217f5edc51736f891a6aa5c2c2
mig1023 <mig1023@yandex.ru>
2021-05-26 18:12:38 +0300

    character fix (pensioner)

--- Seeker/Seeker/Gamebook/PensionerSimulator/Character.cs
+++ Seeker/Seeker/Gamebook/PensionerSimulator/Character.cs
@@ -4,19 +4,8 @@ using System.Text;
 
 namespace Seeker.Gamebook.PensionerSimulator
 {
-    class Character : Abstract.ICharacter
+    class Character : Prototypes.Character, Abstract.ICharacter
     {
         public static Character Protagonist = new Gamebook.PensionerSimulator.Character();
-
-        public string Name { get; set; }
-
-        public void Init()
-        {
-            Name = String.Empty;
-        }
-
-        public string Save() => String.Empty;
-
-        public void Load(string saveLine) { }
     }
 }
