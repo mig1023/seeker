337e514720afbc9dccd543d090ecf8a7a4a27587
mig1023 <mig1023@yandex.ru>
2021-05-27 17:40:20 +0300

    action fix (creature)

--- Seeker/Seeker/Gamebook/CreatureOfHavoc/Actions.cs
+++ Seeker/Seeker/Gamebook/CreatureOfHavoc/Actions.cs
@@ -39,13 +39,8 @@ namespace Seeker.Gamebook.CreatureOfHavoc
             String.Format("Удачливость: {0}", Character.Protagonist.Luck),
         };
 
-        public override bool GameOver(out int toEndParagraph, out string toEndText)
-        {
-            toEndParagraph = 0;
-            toEndText = "Начать сначала";
-
-            return Character.Protagonist.Endurance <= 0;
-        }
+        public override bool GameOver(out int toEndParagraph, out string toEndText) =>
+            GameOverBy(Character.Protagonist.Endurance, out toEndParagraph, out toEndText);
 
         public static bool CheckOnlyIf(string option)
         {
