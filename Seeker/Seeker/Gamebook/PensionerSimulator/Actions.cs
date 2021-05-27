deff9a7761fccd0af4f5eb5511d14a15a1bb3d49
mig1023 <mig1023@yandex.ru>
2021-05-27 17:57:46 +0300

    instance + checkonlyif fix (pensioner)

--- Seeker/Seeker/Gamebook/PensionerSimulator/Actions.cs
+++ Seeker/Seeker/Gamebook/PensionerSimulator/Actions.cs
@@ -8,6 +8,6 @@ namespace Seeker.Gamebook.PensionerSimulator
 {
     class Actions : Prototypes.Actions, Abstract.IActions
     {
-        public static bool CheckOnlyIf(string option) => true;
+        public static Actions StaticInstance = new Actions();
     }
 }
