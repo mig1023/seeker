71e9b3eef14a74c94eaeb94c897c6640b9f50693
mig1023 <mig1023@yandex.ru>
2021-05-28 17:31:44 +0300

    static instances (pensioner)

--- Seeker/Seeker/Gamebook/PensionerSimulator/Actions.cs
+++ Seeker/Seeker/Gamebook/PensionerSimulator/Actions.cs
@@ -12,16 +12,16 @@ namespace Seeker.Gamebook.PensionerSimulator
 
         public override bool CheckOnlyIf(string option)
         {
-            foreach (string oneOption in option.Split(','))
-            {
-                if (oneOption.Contains("!") && (Game.Data.Triggers.Contains(oneOption.Replace("!", String.Empty).Trim())))
+                foreach (string oneOption in option.Split(','))
+                {
+                    if (oneOption.Contains("!") && (Game.Data.Triggers.Contains(oneOption.Replace("!", String.Empty).Trim())))
+                            return false;
+
+                    else if (!oneOption.Contains("!") && (!Game.Data.Triggers.Contains(oneOption.Trim())))
                         return false;
+                }
 
-                else if (!oneOption.Contains("!") && (!Game.Data.Triggers.Contains(oneOption.Trim())))
-                    return false;
-            }
-
-            return true;
+                return true;
         }
     }
 }
