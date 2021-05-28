910ebcd2cd210291c748bc323fff4b2bf638a1de
mig1023 <mig1023@yandex.ru>
2021-05-28 17:13:49 +0300

    static instances (black castle)

--- Seeker/Seeker/Gamebook/BlackCastleDungeon/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/BlackCastleDungeon/Paragraphs.cs
@@ -12,6 +12,8 @@ namespace Seeker.Gamebook.BlackCastleDungeon
 {
     class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs
     {
+        public static Paragraphs StaticInstance = new Paragraphs();
+
         public override Game.Paragraph Get(int id, XmlNode xmlParagraph)
         {
             Game.Paragraph paragraph = ParagraphTemplate(xmlParagraph);
