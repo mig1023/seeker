152b0cd5adc5f324d9dd8deacaf9406909d8fd82
mig1023 <mig1023@yandex.ru>
2021-05-28 17:29:56 +0300

    static instances (legends)

--- Seeker/Seeker/Gamebook/LegendsAlwaysLie/Constants.cs
+++ Seeker/Seeker/Gamebook/LegendsAlwaysLie/Constants.cs
@@ -9,6 +9,8 @@ namespace Seeker.Gamebook.LegendsAlwaysLie
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
+        public static Constants StaticInstance = new Constants();
+
         public override Dictionary<ButtonTypes, string> ButtonsColors() => new Dictionary<ButtonTypes, string>
         {
             [ButtonTypes.Main] = "#911",
