b776df8b0d55a01dfb2427919a0666d456ff25c3
mig1023 <mig1023@yandex.ru>
2021-05-28 17:33:23 +0300

    static instances (school)

--- Seeker/Seeker/Gamebook/SilentSchool/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/SilentSchool/Paragraphs.cs
@@ -12,6 +12,8 @@ namespace Seeker.Gamebook.SilentSchool
 {
     class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs
     {
+        public static Paragraphs StaticInstance = new Paragraphs();
+
         public override Game.Paragraph Get(int id, XmlNode xmlParagraph)
         {
             Game.Paragraph paragraph = ParagraphTemplate(xmlParagraph);
