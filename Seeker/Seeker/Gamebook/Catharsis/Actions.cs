40505633c1c57cc2f673395699f2895303f62217
mig1023 <mig1023@yandex.ru>
2021-06-01 20:19:30 +0300

    levelparse (catharsis)

--- Seeker/Seeker/Gamebook/Catharsis/Actions.cs
+++ Seeker/Seeker/Gamebook/Catharsis/Actions.cs
@@ -81,7 +81,7 @@ namespace Seeker.Gamebook.Catharsis
                     }
                     else if (oneOption.Contains("="))
                     {
-                        int level = int.Parse(oneOption.Split('=', '<')[1]);
+                        int level = Game.Other.LevelParse(oneOption);
 
                         if (oneOption.Contains("СТЕЛС") && (level > Character.Protagonist.Stealth))
                             return false;
