5d81477869658bbb1f41c6cf9cd2168367604d1a
mig1023 <mig1023@yandex.ru>
2021-06-11 20:23:32 +0300

    StrengthBy modifications

--- Seeker/Seeker/Gamebook/PrairieLaw/Modification.cs
+++ Seeker/Seeker/Gamebook/PrairieLaw/Modification.cs
@@ -11,6 +11,15 @@ namespace Seeker.Gamebook.PrairieLaw
             if (Name == "StrengthByPoison")
                 ModificationByTrigger("Противоядие", () => Character.Protagonist.Strength -= 10, not: true);
 
+            else if (Name == "StrengthByBlanket")
+                Character.Protagonist.Strength += (Game.Data.Triggers.Contains("Одеяло") ? 1 : -1);
+
+            else if (Name == "StrengthByBlanketOrMatches")
+            {
+                bool blanketOrMatches = Game.Data.Triggers.Contains("Одеяло") || Game.Data.Triggers.Contains("Спички");
+                Character.Protagonist.Strength += (blanketOrMatches ? 1 : -1);
+            }
+
             else if (Name == "Skin")
                 ModificationByTrigger("Нож", () => Character.Protagonist.AnimalSkins.Add(ValueString));
 
