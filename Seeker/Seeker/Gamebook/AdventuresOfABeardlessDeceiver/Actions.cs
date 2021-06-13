1d97f574d2bdc585f225d28eac5f509425d3da39
mig1023 <mig1023@yandex.ru>
2021-06-13 18:21:47 +0300

    price representer fix (aldar)

--- Seeker/Seeker/Gamebook/AdventuresOfABeardlessDeceiver/Actions.cs
+++ Seeker/Seeker/Gamebook/AdventuresOfABeardlessDeceiver/Actions.cs
@@ -30,6 +30,8 @@ namespace Seeker.Gamebook.AdventuresOfABeardlessDeceiver
 
                 return new List<string> { String.Format("{0}{1}", Text, diffLine) };
             }
+            else if (Price > 0)
+                return new List<string> { String.Format("{0}, {1} таньга", Text, Price) };
 
             else if (!String.IsNullOrEmpty(Text))
                 return new List<string> { Text };
