1e86d68c595dcafdeeb377b0353f85e3be1185a1
mig1023 <mig1023@yandex.ru>
2021-06-16 17:34:42 +0300

    paragraphs refactoring (captain)

--- Seeker/Seeker/Gamebook/CaptainSheltonsSecret/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/CaptainSheltonsSecret/Paragraphs.cs
@@ -22,72 +22,66 @@ namespace Seeker.Gamebook.CaptainSheltonsSecret
                 paragraph.Options.Add(OptionsTemplate(xmlOption));
 
             foreach (XmlNode xmlAction in xmlParagraph.SelectNodes("Actions/Action"))
-            {
-                Actions action = new Actions
-                {
-                    ActionName = Game.Xml.StringParse(xmlAction["ActionName"]),
-                    ButtonName = Game.Xml.StringParse(xmlAction["ButtonName"]),
-                    Aftertext = Game.Xml.StringParse(xmlAction["Aftertext"]),
-                    Trigger = Game.Xml.StringParse(xmlAction["Trigger"]),
-                    Text = Game.Xml.StringParse(xmlAction["Text"]),
-
-                    RoundsToWin = Game.Xml.IntParse(xmlAction["RoundsToWin"]),
-                    WoundsToWin = Game.Xml.IntParse(xmlAction["WoundsToWin"]),
-                    DamageToWin = Game.Xml.IntParse(xmlAction["DamageToWin"]),
-                    MasteryPenalty = Game.Xml.IntParse(xmlAction["MasteryPenalty"]),
-                    Price = Game.Xml.IntParse(xmlAction["Price"]),
-
-                    Multiple = Game.Xml.BoolParse(xmlAction["Multiple"]),
-                    GroupFight = Game.Xml.BoolParse(xmlAction["GroupFight"]),
-                };
-
-                if (xmlAction["Allies"] != null)
-                {
-                    action.Allies = new List<Character>();
-
-                    foreach (XmlNode xmlAlly in xmlAction.SelectNodes("Allies/Ally"))
-                    {
-                        Character ally = new Character
-                        {
-                            Name = Game.Xml.StringParse(xmlAlly.Attributes["Name"]),
-                            MaxMastery = Game.Xml.IntParse(xmlAlly.Attributes["Mastery"]),
-                            MaxEndurance = Game.Xml.IntParse(xmlAlly.Attributes["Endurance"]),
-                        };
-
-                        ally.Mastery = ally.MaxMastery;
-                        ally.Endurance = ally.MaxEndurance;
-
-                        action.Allies.Add(ally);
-                    }
-                }
-
-                if (xmlAction["Enemies"] != null)
-                {
-                    action.Enemies = new List<Character>();
-
-                    foreach (XmlNode xmlEnemy in xmlAction.SelectNodes("Enemies/Enemy"))
-                    {
-                        Character enemy = new Character
-                        {
-                            Name = Game.Xml.StringParse(xmlEnemy.Attributes["Name"]),
-                            MaxMastery = Game.Xml.IntParse(xmlEnemy.Attributes["Mastery"]),
-                            MaxEndurance = Game.Xml.IntParse(xmlEnemy.Attributes["Endurance"]),
-                        };
-
-                        enemy.Mastery = enemy.MaxMastery;
-                        enemy.Endurance = enemy.MaxEndurance;
-
-                        action.Enemies.Add(enemy);
-                    }
-                }
-
-                paragraph.Actions.Add(action);
-            }
+                paragraph.Actions.Add(ActionParse(xmlAction));
 
             foreach (XmlNode xmlModification in xmlParagraph.SelectNodes("Modifications/Modification"))
                 paragraph.Modification.Add(Game.Xml.ModificationParse(xmlModification, new Modification()));
 
             return paragraph;
+        }
+
+        private Actions ActionParse(XmlNode xmlAction)
+        {
+            Actions action = new Actions
+            {
+                ActionName = Game.Xml.StringParse(xmlAction["ActionName"]),
+                ButtonName = Game.Xml.StringParse(xmlAction["ButtonName"]),
+                Aftertext = Game.Xml.StringParse(xmlAction["Aftertext"]),
+                Trigger = Game.Xml.StringParse(xmlAction["Trigger"]),
+                Text = Game.Xml.StringParse(xmlAction["Text"]),
+
+                RoundsToWin = Game.Xml.IntParse(xmlAction["RoundsToWin"]),
+                WoundsToWin = Game.Xml.IntParse(xmlAction["WoundsToWin"]),
+                DamageToWin = Game.Xml.IntParse(xmlAction["DamageToWin"]),
+                MasteryPenalty = Game.Xml.IntParse(xmlAction["MasteryPenalty"]),
+                Price = Game.Xml.IntParse(xmlAction["Price"]),
+
+                Multiple = Game.Xml.BoolParse(xmlAction["Multiple"]),
+                GroupFight = Game.Xml.BoolParse(xmlAction["GroupFight"]),
+            };
+
+            if (xmlAction["Allies"] != null)
+            {
+                action.Allies = new List<Character>();
+
+                foreach (XmlNode xmlAlly in xmlAction.SelectNodes("Allies/Ally"))
+                    action.Allies.Add(EnemyParse(xmlAlly));
+            }
+
+            if (xmlAction["Enemies"] != null)
+            {
+                action.Enemies = new List<Character>();
+
+                foreach (XmlNode xmlEnemy in xmlAction.SelectNodes("Enemies/Enemy"))
+                    action.Enemies.Add(EnemyParse(xmlEnemy));
+            }
+
+            return action;
+        }
+
+        private static Character EnemyParse(XmlNode xmlEnemy)
+        {
+            Character enemy = new Character
+            {
+                Name = Game.Xml.StringParse(xmlEnemy.Attributes["Name"]),
+                MaxMastery = Game.Xml.IntParse(xmlEnemy.Attributes["Mastery"]),
+                MaxEndurance = Game.Xml.IntParse(xmlEnemy.Attributes["Endurance"]),
+            };
+
+            enemy.Mastery = enemy.MaxMastery;
+            enemy.Endurance = enemy.MaxEndurance;
+
+            return enemy;
         }
     }
 }
