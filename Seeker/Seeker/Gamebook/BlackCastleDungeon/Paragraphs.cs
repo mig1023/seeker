4b3c7bddfb0c8a16bc10d45d28b2f58c1b410041
mig1023 <mig1023@yandex.ru>
2021-06-16 17:33:38 +0300

    paragraphs refactoring (black castle)

--- Seeker/Seeker/Gamebook/BlackCastleDungeon/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/BlackCastleDungeon/Paragraphs.cs
@@ -19,71 +19,80 @@ namespace Seeker.Gamebook.BlackCastleDungeon
             Game.Paragraph paragraph = ParagraphTemplate(xmlParagraph);
 
             foreach (XmlNode xmlOption in xmlParagraph.SelectNodes("Options/Option"))
-            {
-                Option option = OptionsTemplate(xmlOption);
-
-                if (xmlOption.Attributes["Do"] != null)
-                {
-                    Modification modification = new Modification
-                    {
-                        Name = Game.Xml.StringParse(xmlOption.Attributes["Do"]),
-                        Value = Game.Xml.IntParse(xmlOption.Attributes["Value"]),
-                        ValueString = Game.Xml.StringParse(xmlOption.Attributes["ValueString"]),
-                    };
-
-                    option.Do = modification;
-                }
-
-                paragraph.Options.Add(option);
-            }
+                paragraph.Options.Add(OptionParse(xmlOption));
 
             foreach (XmlNode xmlAction in xmlParagraph.SelectNodes("Actions/Action"))
-            {
-                Actions action = new Actions
-                {
-                    ActionName = Game.Xml.StringParse(xmlAction["ActionName"]),
-                    ButtonName = Game.Xml.StringParse(xmlAction["ButtonName"]),
-                    Aftertext = Game.Xml.StringParse(xmlAction["Aftertext"]),
-                    Trigger = Game.Xml.StringParse(xmlAction["Trigger"]),
-                    Text = Game.Xml.StringParse(xmlAction["Text"]),
-
-                    RoundsToWin = Game.Xml.IntParse(xmlAction["RoundsToWin"]),
-                    WoundsToWin = Game.Xml.IntParse(xmlAction["WoundsToWin"]),
-                    Price = Game.Xml.IntParse(xmlAction["Price"]),
-
-                    Multiple = Game.Xml.BoolParse(xmlAction["Multiple"]),
-                    ThisIsSpell = Game.Xml.BoolParse(xmlAction["ThisIsSpell"]),
-                };
-
-                if (xmlAction["Enemies"] != null)
-                {
-                    action.Enemies = new List<Character>();
-
-                    foreach (XmlNode xmlEnemy in xmlAction.SelectNodes("Enemies/Enemy"))
-                    {
-                        Character enemy = new Character
-                        {
-                            Name = Game.Xml.StringParse(xmlEnemy.Attributes["Name"]),
-                            MaxMastery = Game.Xml.IntParse(xmlEnemy.Attributes["Mastery"]),
-                            MaxEndurance = Game.Xml.IntParse(xmlEnemy.Attributes["Endurance"]),
-                        };
-
-                        enemy.Mastery = enemy.MaxMastery;
-                        enemy.Endurance = enemy.MaxEndurance;
-
-                        action.Enemies.Add(enemy);
-                    }
-                }
-
-                action.Benefit = Game.Xml.ModificationParse(xmlAction["Benefit"], new Modification());
-
-                paragraph.Actions.Add(action);
-            }
+                paragraph.Actions.Add(ActionParse(xmlAction));
 
             foreach (XmlNode xmlModification in xmlParagraph.SelectNodes("Modifications/Modification"))
                 paragraph.Modification.Add(Game.Xml.ModificationParse(xmlModification, new Modification()));
 
             return paragraph;
+        }
+
+        private Actions ActionParse(XmlNode xmlAction)
+        {
+            Actions action = new Actions
+            {
+                ActionName = Game.Xml.StringParse(xmlAction["ActionName"]),
+                ButtonName = Game.Xml.StringParse(xmlAction["ButtonName"]),
+                Aftertext = Game.Xml.StringParse(xmlAction["Aftertext"]),
+                Trigger = Game.Xml.StringParse(xmlAction["Trigger"]),
+                Text = Game.Xml.StringParse(xmlAction["Text"]),
+
+                RoundsToWin = Game.Xml.IntParse(xmlAction["RoundsToWin"]),
+                WoundsToWin = Game.Xml.IntParse(xmlAction["WoundsToWin"]),
+                Price = Game.Xml.IntParse(xmlAction["Price"]),
+
+                Multiple = Game.Xml.BoolParse(xmlAction["Multiple"]),
+                ThisIsSpell = Game.Xml.BoolParse(xmlAction["ThisIsSpell"]),
+            };
+
+            if (xmlAction["Enemies"] != null)
+            {
+                action.Enemies = new List<Character>();
+
+                foreach (XmlNode xmlEnemy in xmlAction.SelectNodes("Enemies/Enemy"))
+                    action.Enemies.Add(EnemyParse(xmlEnemy));
+            }
+
+            action.Benefit = Game.Xml.ModificationParse(xmlAction["Benefit"], new Modification());
+
+            return action;
+        }
+
+        private Option OptionParse(XmlNode xmlOption)
+        {
+            Option option = OptionsTemplate(xmlOption);
+
+            if (xmlOption.Attributes["Do"] != null)
+            {
+                Modification modification = new Modification
+                {
+                    Name = Game.Xml.StringParse(xmlOption.Attributes["Do"]),
+                    Value = Game.Xml.IntParse(xmlOption.Attributes["Value"]),
+                    ValueString = Game.Xml.StringParse(xmlOption.Attributes["ValueString"]),
+                };
+
+                option.Do = modification;
+            }
+
+            return option;
+        }
+
+        private static Character EnemyParse(XmlNode xmlEnemy)
+        {
+            Character enemy = new Character
+            {
+                Name = Game.Xml.StringParse(xmlEnemy.Attributes["Name"]),
+                MaxMastery = Game.Xml.IntParse(xmlEnemy.Attributes["Mastery"]),
+                MaxEndurance = Game.Xml.IntParse(xmlEnemy.Attributes["Endurance"]),
+            };
+
+            enemy.Mastery = enemy.MaxMastery;
+            enemy.Endurance = enemy.MaxEndurance;
+
+            return enemy;
         }
     }
 }
