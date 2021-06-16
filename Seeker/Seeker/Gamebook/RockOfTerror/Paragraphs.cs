1e8c246aaa31104a03f8a92fb295e29b32766241
mig1023 <mig1023@yandex.ru>
2021-06-16 17:38:36 +0300

    paragraphs refactoring (rock)

--- Seeker/Seeker/Gamebook/RockOfTerror/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/RockOfTerror/Paragraphs.cs
@@ -21,31 +21,27 @@ namespace Seeker.Gamebook.RockOfTerror
                 paragraph.Options.Add(OptionsTemplate(xmlOption));
 
             foreach (XmlNode xmlAction in xmlParagraph.SelectNodes("Actions/Action"))
-            {
-                Actions action = new Actions
-                {
-                    ActionName = Game.Xml.StringParse(xmlAction["ActionName"]),
-                    ButtonName = Game.Xml.StringParse(xmlAction["ButtonName"]),
-                    Aftertext = Game.Xml.StringParse(xmlAction["Aftertext"]),
-                    Trigger = Game.Xml.StringParse(xmlAction["Trigger"]),
-                };
-
-                paragraph.Actions.Add(action);
-            }
+                paragraph.Actions.Add(ActionParse(xmlAction));
 
             foreach (XmlNode xmlModification in xmlParagraph.SelectNodes("Modifications/Modification"))
-            {
-                Modification modification = new Modification
-                {
-                    Name = Game.Xml.StringParse(xmlModification.Attributes["Name"]),
-                    Value = Game.Xml.IntParse(xmlModification.Attributes["Value"]),
-                    Init = Game.Xml.BoolParse(xmlModification.Attributes["Init"]),
-                };
-
-                paragraph.Modification.Add(modification);
-            }
+                paragraph.Modification.Add(ModificationParse(xmlModification));
 
             return paragraph;
         }
+
+        private Actions ActionParse(XmlNode xmlAction) => new Actions
+        {
+            ActionName = Game.Xml.StringParse(xmlAction["ActionName"]),
+            ButtonName = Game.Xml.StringParse(xmlAction["ButtonName"]),
+            Aftertext = Game.Xml.StringParse(xmlAction["Aftertext"]),
+            Trigger = Game.Xml.StringParse(xmlAction["Trigger"]),
+        };
+
+        private static Modification ModificationParse(XmlNode xmlModification) => new Modification
+        {
+            Name = Game.Xml.StringParse(xmlModification.Attributes["Name"]),
+            Value = Game.Xml.IntParse(xmlModification.Attributes["Value"]),
+            Init = Game.Xml.BoolParse(xmlModification.Attributes["Init"]),
+        };
     }
 }
