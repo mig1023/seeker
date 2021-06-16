1748460df5df5d82d3661e90b62c3bab9061f590
mig1023 <mig1023@yandex.ru>
2021-06-16 17:40:31 +0300

    paragraphs refactoring (those who are about to die)

--- Seeker/Seeker/Gamebook/ThoseWhoAreAboutToDie/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/ThoseWhoAreAboutToDie/Paragraphs.cs
@@ -18,37 +18,38 @@ namespace Seeker.Gamebook.ThoseWhoAreAboutToDie
             Game.Paragraph paragraph = ParagraphTemplate(xmlParagraph);
 
             foreach (XmlNode xmlOption in xmlParagraph.SelectNodes("Options/Option"))
-            {
-                Option option = OptionsTemplateWithoutDestination(xmlOption);
-
-                if (int.TryParse(xmlOption.Attributes["Destination"].Value, out int _))
-                    option.Destination = Game.Xml.IntParse(xmlOption.Attributes["Destination"]);
-                else
-                {
-                    List<string> destinations = xmlOption.Attributes["Destination"].Value.Split(',').ToList<string>();
-                    option.Destination = int.Parse(destinations[random.Next(destinations.Count())]);
-                }
-
-                paragraph.Options.Add(option);
-            }
+                paragraph.Options.Add(OptionParse(xmlOption));
 
             foreach (XmlNode xmlAction in xmlParagraph.SelectNodes("Actions/Action"))
-            {
-                Actions action = new Actions
-                {
-                    ActionName = Game.Xml.StringParse(xmlAction["ActionName"]),
-                    ButtonName = Game.Xml.StringParse(xmlAction["ButtonName"]),
-                    Aftertext = Game.Xml.StringParse(xmlAction["Aftertext"]),
-                    Trigger = Game.Xml.StringParse(xmlAction["Trigger"]),
-                };
-
-                paragraph.Actions.Add(action);
-            }
+                paragraph.Actions.Add(ActionParse(xmlAction));
 
             foreach (XmlNode xmlModification in xmlParagraph.SelectNodes("Modifications/Modification"))
                 paragraph.Modification.Add(Game.Xml.ModificationParse(xmlModification, new Modification()));
 
             return paragraph;
+        }
+
+        private Actions ActionParse(XmlNode xmlAction) => new Actions
+        {
+            ActionName = Game.Xml.StringParse(xmlAction["ActionName"]),
+            ButtonName = Game.Xml.StringParse(xmlAction["ButtonName"]),
+            Aftertext = Game.Xml.StringParse(xmlAction["Aftertext"]),
+            Trigger = Game.Xml.StringParse(xmlAction["Trigger"]),
+        };
+
+        private Option OptionParse(XmlNode xmlOption)
+        {
+            Option option = OptionsTemplateWithoutDestination(xmlOption);
+
+            if (int.TryParse(xmlOption.Attributes["Destination"].Value, out int _))
+                option.Destination = Game.Xml.IntParse(xmlOption.Attributes["Destination"]);
+            else
+            {
+                List<string> destinations = xmlOption.Attributes["Destination"].Value.Split(',').ToList<string>();
+                option.Destination = int.Parse(destinations[random.Next(destinations.Count())]);
+            }
+
+            return option;
         }
     }
 }
