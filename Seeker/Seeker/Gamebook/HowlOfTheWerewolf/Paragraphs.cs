9b3043831f96d6f909adb443742b8adf77178299
mig1023 <mig1023@yandex.ru>
2021-06-16 17:36:21 +0300

    paragraphs refactoring (werewolf)

--- Seeker/Seeker/Gamebook/HowlOfTheWerewolf/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/HowlOfTheWerewolf/Paragraphs.cs
@@ -33,71 +33,81 @@ namespace Seeker.Gamebook.HowlOfTheWerewolf
             }
 
             foreach (XmlNode xmlAction in xmlParagraph.SelectNodes("Actions/Action"))
-            {
-                Actions action = new Actions
-                {
-                    ActionName = Game.Xml.StringParse(xmlAction["ActionName"]),
-                    ButtonName = Game.Xml.StringParse(xmlAction["ButtonName"]),
-                    Aftertext = Game.Xml.StringParse(xmlAction["Aftertext"]),
-                    Text = Game.Xml.StringParse(xmlAction["Text"]),
-                    Trigger = Game.Xml.StringParse(xmlAction["Trigger"]),
-
-                    Price = Game.Xml.IntParse(xmlAction["Price"]),
-                    Value = Game.Xml.IntParse(xmlAction["Value"]),
-                    RoundsToWin = Game.Xml.IntParse(xmlAction["RoundsToWin"]),
-                    RoundsWinToWin = Game.Xml.IntParse(xmlAction["RoundsWinToWin"]),
-                    RoundsFailToFail = Game.Xml.IntParse(xmlAction["RoundsFailToFail"]),
-                    RoundsToFight = Game.Xml.IntParse(xmlAction["RoundsToFight"]),
-                    WoundsToWin = Game.Xml.IntParse(xmlAction["WoundsToWin"]),
-                    WoundsToFail = Game.Xml.IntParse(xmlAction["WoundsToFail"]),
-                    WoundsForTransformation = Game.Xml.IntParse(xmlAction["WoundsForTransformation"]),
-                    WoundsLimit = Game.Xml.IntParse(xmlAction["WoundsLimit"]),
-                    HitStrengthBonus = Game.Xml.IntParse(xmlAction["HitStrengthBonus"]),
-                    ExtendedDamage = Game.Xml.IntParse(xmlAction["ExtendedDamage"]),
-
-                    Multiple = Game.Xml.BoolParse(xmlAction["Multiple"]),
-
-                    Specificity = SpecificsParse(xmlAction["Specificity"]),
-                };
-
-                if (xmlAction["Benefit"] != null)
-                {
-                    action.BenefitList = new List<Abstract.IModification>();
-
-                    foreach (XmlNode bonefit in xmlAction.SelectNodes("Benefit"))
-                        action.BenefitList.Add(Game.Xml.ModificationParse(bonefit, new Modification()));
-                }
-
-                if (xmlAction["Enemies"] != null)
-                {
-                    action.Enemies = new List<Character>();
-
-                    foreach (XmlNode xmlEnemy in xmlAction.SelectNodes("Enemies/Enemy"))
-                    {
-                        Character enemy = new Character
-                        {
-                            Name = Game.Xml.StringParse(xmlEnemy.Attributes["Name"]),
-                            MaxMastery = Game.Xml.IntParse(xmlEnemy.Attributes["Mastery"]),
-                            MaxEndurance = Game.Xml.IntParse(xmlEnemy.Attributes["Endurance"]),
-                        };
-
-                        enemy.Mastery = enemy.MaxMastery;
-                        enemy.Endurance = enemy.MaxEndurance;
-
-                        if (Game.Xml.BoolParse(xmlAction["RandomEnemyCount"]))
-                            EnemyMultiplier(Game.Dice.Roll(), ref action, enemy);
-                        else
-                            action.Enemies.Add(enemy);
-                    }
-                }
-
-                paragraph.Actions.Add(action);
-            }
+                paragraph.Actions.Add(ActionParse(xmlAction));
 
             foreach (XmlNode xmlModification in xmlParagraph.SelectNodes("Modifications/Modification"))
                 paragraph.Modification.Add(Game.Xml.ModificationParse(xmlModification, new Modification()));
 
             return paragraph;
+        }
+
+        private Actions ActionParse(XmlNode xmlAction)
+        {
+            Actions action = new Actions
+            {
+                ActionName = Game.Xml.StringParse(xmlAction["ActionName"]),
+                ButtonName = Game.Xml.StringParse(xmlAction["ButtonName"]),
+                Aftertext = Game.Xml.StringParse(xmlAction["Aftertext"]),
+                Text = Game.Xml.StringParse(xmlAction["Text"]),
+                Trigger = Game.Xml.StringParse(xmlAction["Trigger"]),
+
+                Price = Game.Xml.IntParse(xmlAction["Price"]),
+                Value = Game.Xml.IntParse(xmlAction["Value"]),
+                RoundsToWin = Game.Xml.IntParse(xmlAction["RoundsToWin"]),
+                RoundsWinToWin = Game.Xml.IntParse(xmlAction["RoundsWinToWin"]),
+                RoundsFailToFail = Game.Xml.IntParse(xmlAction["RoundsFailToFail"]),
+                RoundsToFight = Game.Xml.IntParse(xmlAction["RoundsToFight"]),
+                WoundsToWin = Game.Xml.IntParse(xmlAction["WoundsToWin"]),
+                WoundsToFail = Game.Xml.IntParse(xmlAction["WoundsToFail"]),
+                WoundsForTransformation = Game.Xml.IntParse(xmlAction["WoundsForTransformation"]),
+                WoundsLimit = Game.Xml.IntParse(xmlAction["WoundsLimit"]),
+                HitStrengthBonus = Game.Xml.IntParse(xmlAction["HitStrengthBonus"]),
+                ExtendedDamage = Game.Xml.IntParse(xmlAction["ExtendedDamage"]),
+
+                Multiple = Game.Xml.BoolParse(xmlAction["Multiple"]),
+
+                Specificity = SpecificsParse(xmlAction["Specificity"]),
+            };
+
+            if (xmlAction["Benefit"] != null)
+            {
+                action.BenefitList = new List<Abstract.IModification>();
+
+                foreach (XmlNode bonefit in xmlAction.SelectNodes("Benefit"))
+                    action.BenefitList.Add(Game.Xml.ModificationParse(bonefit, new Modification()));
+            }
+
+            if (xmlAction["Enemies"] != null)
+            {
+                action.Enemies = new List<Character>();
+
+                foreach (XmlNode xmlEnemy in xmlAction.SelectNodes("Enemies/Enemy"))
+                {
+                    Character enemy = EnemyParse(xmlEnemy);
+
+                    if (Game.Xml.BoolParse(xmlAction["RandomEnemyCount"]))
+                        EnemyMultiplier(Game.Dice.Roll(), ref action, enemy);
+                    else
+                        action.Enemies.Add(enemy);
+                }
+            }
+
+            return action;
+        }
+
+        private static Character EnemyParse(XmlNode xmlEnemy)
+        {
+            Character enemy = new Character
+            {
+                Name = Game.Xml.StringParse(xmlEnemy.Attributes["Name"]),
+                MaxMastery = Game.Xml.IntParse(xmlEnemy.Attributes["Mastery"]),
+                MaxEndurance = Game.Xml.IntParse(xmlEnemy.Attributes["Endurance"]),
+            };
+
+            enemy.Mastery = enemy.MaxMastery;
+            enemy.Endurance = enemy.MaxEndurance;
+
+            return enemy;
         }
 
         public static void EnemyMultiplier(int count, ref Actions action, Character enemy)
