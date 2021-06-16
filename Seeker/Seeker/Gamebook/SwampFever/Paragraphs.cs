7db0dc3cbd86ee6fc0fbd20a8bc3865c0d40c444
mig1023 <mig1023@yandex.ru>
2021-06-16 17:39:42 +0300

    paragraphs refactoring (swamp fever)

--- Seeker/Seeker/Gamebook/SwampFever/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/SwampFever/Paragraphs.cs
@@ -21,33 +21,28 @@ namespace Seeker.Gamebook.SwampFever
                 paragraph.Options.Add(OptionsTemplate(xmlOption));
 
             foreach (XmlNode xmlAction in xmlParagraph.SelectNodes("Actions/Action"))
-            {
-                Actions action = new Actions
-                {
-                    ActionName = Game.Xml.StringParse(xmlAction["ActionName"]),
-                    ButtonName = Game.Xml.StringParse(xmlAction["ButtonName"]),
-                    Aftertext = Game.Xml.StringParse(xmlAction["Aftertext"]),
-                    Trigger = Game.Xml.StringParse(xmlAction["Trigger"]),
-                    EnemyName = Game.Xml.StringParse(xmlAction["EnemyName"]),
-                    EnemyCombination = Game.Xml.StringParse(xmlAction["EnemyCombination"]),
-                    Text = Game.Xml.StringParse(xmlAction["Text"]),
-
-                    Price = Game.Xml.IntParse(xmlAction["Price"]),
-                    Level = Game.Xml.IntParse(xmlAction["Level"]),
-
-                    Birds = Game.Xml.BoolParse(xmlAction["Birds"]),
-
-                    Benefit = ModificationParse(xmlAction["Benefit"]),
-                };
-
-                paragraph.Actions.Add(action);
-            }
+                paragraph.Actions.Add(ActionParse(xmlAction));
 
             foreach (XmlNode xmlModification in xmlParagraph.SelectNodes("Modifications/Modification"))
                 paragraph.Modification.Add(ModificationParse(xmlModification));
 
             return paragraph;
         }
+
+        private Actions ActionParse(XmlNode xmlAction) => new Actions
+        {
+            ActionName = Game.Xml.StringParse(xmlAction["ActionName"]),
+            ButtonName = Game.Xml.StringParse(xmlAction["ButtonName"]),
+            Aftertext = Game.Xml.StringParse(xmlAction["Aftertext"]),
+            Trigger = Game.Xml.StringParse(xmlAction["Trigger"]),
+            EnemyName = Game.Xml.StringParse(xmlAction["EnemyName"]),
+            EnemyCombination = Game.Xml.StringParse(xmlAction["EnemyCombination"]),
+            Text = Game.Xml.StringParse(xmlAction["Text"]),
+            Price = Game.Xml.IntParse(xmlAction["Price"]),
+            Level = Game.Xml.IntParse(xmlAction["Level"]),
+            Birds = Game.Xml.BoolParse(xmlAction["Birds"]),
+            Benefit = ModificationParse(xmlAction["Benefit"]),
+        };
 
         private static Modification ModificationParse(XmlNode xmlNode)
         {
