edb9c93c2f93dffff03515706dac62b272aff5d3
mig1023 <mig1023@yandex.ru>
2021-06-16 17:50:43 +0300

    gettemplate (rendezvous)

--- Seeker/Seeker/Gamebook/RendezVous/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/RendezVous/Paragraphs.cs
@@ -13,23 +13,9 @@ namespace Seeker.Gamebook.RendezVous
     {
         public static Paragraphs StaticInstance = new Paragraphs();
 
-        public override Game.Paragraph Get(int id, XmlNode xmlParagraph)
-        {
-            Game.Paragraph paragraph = ParagraphTemplate(xmlParagraph);
+        public override Game.Paragraph Get(int id, XmlNode xmlParagraph) => GetTemplateModXml(xmlParagraph, new Modification());
 
-            foreach (XmlNode xmlOption in xmlParagraph.SelectNodes("Options/Option"))
-                paragraph.Options.Add(OptionsTemplate(xmlOption));
-
-            foreach (XmlNode xmlAction in xmlParagraph.SelectNodes("Actions/Action"))
-                paragraph.Actions.Add(ActionParse(xmlAction));
-
-            foreach (XmlNode xmlModification in xmlParagraph.SelectNodes("Modifications/Modification"))
-                paragraph.Modification.Add(Game.Xml.ModificationParse(xmlModification, new Modification()));
-
-            return paragraph;
-        }
-
-        private Actions ActionParse(XmlNode xmlAction) => new Actions
+        public override Abstract.IActions ActionParse(XmlNode xmlAction) => new Actions
         {
             ActionName = Game.Xml.StringParse(xmlAction["ActionName"]),
             ButtonName = Game.Xml.StringParse(xmlAction["ButtonName"]),
@@ -38,5 +24,7 @@ namespace Seeker.Gamebook.RendezVous
 
             Dices = Game.Xml.IntParse(xmlAction["Dices"]),
         };
+
+        public override Option OptionParse(XmlNode xmlOption) => OptionsTemplate(xmlOption);
     }
 }
