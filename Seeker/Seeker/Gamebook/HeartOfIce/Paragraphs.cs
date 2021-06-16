c2c0c7d44b3b24d3c84666b7e0f9dd1ed80df98f
mig1023 <mig1023@yandex.ru>
2021-06-16 17:36:05 +0300

    paragraphs refactoring (heart)

--- Seeker/Seeker/Gamebook/HeartOfIce/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/HeartOfIce/Paragraphs.cs
@@ -18,52 +18,58 @@ namespace Seeker.Gamebook.HeartOfIce
             Game.Paragraph paragraph = ParagraphTemplate(xmlParagraph);
 
             foreach (XmlNode xmlOption in xmlParagraph.SelectNodes("Options/Option"))
-            {
-                Option option = OptionsTemplate(xmlOption);
-
-                if (xmlOption.Attributes["Do"] != null)
-                    option.Do = Game.Xml.ModificationParse(xmlOption, new Modification(), name: "Do");
-
-                paragraph.Options.Add(option);
-            }
+                paragraph.Options.Add(OptionParse(xmlOption));
 
             foreach (XmlNode xmlAction in xmlParagraph.SelectNodes("Actions/Action"))
-            {
-                Actions action = new Actions
-                {
-                    ActionName = Game.Xml.StringParse(xmlAction["ActionName"]),
-                    ButtonName = Game.Xml.StringParse(xmlAction["ButtonName"]),
-                    Aftertext = Game.Xml.StringParse(xmlAction["Aftertext"]),
-                    Trigger = Game.Xml.StringParse(xmlAction["Trigger"]),
-                    Text = Game.Xml.StringParse(xmlAction["Text"]),
-                    Skill = Game.Xml.StringParse(xmlAction["Skill"]),
-                    RemoveTrigger = Game.Xml.StringParse(xmlAction["RemoveTrigger"]),
-                    SellType = Game.Xml.StringParse(xmlAction["SellType"]),
-
-                    Price = Game.Xml.IntParse(xmlAction["Price"]),
-
-                    Choice = Game.Xml.BoolParse(xmlAction["Choice"]),
-                    Multiple = Game.Xml.BoolParse(xmlAction["Multiple"]),
-                    Sell = Game.Xml.BoolParse(xmlAction["Sell"]),
-                    SellIfAvailable = Game.Xml.BoolParse(xmlAction["SellIfAvailable"]),
-                    Split = Game.Xml.BoolParse(xmlAction["Split"]),
-                };
-
-                if (xmlAction["Benefit"] != null)
-                {
-                    action.BenefitList = new List<Abstract.IModification>();
-
-                    foreach (XmlNode bonefit in xmlAction.SelectNodes("Benefit"))
-                        action.BenefitList.Add(Game.Xml.ModificationParse(bonefit, new Modification()));
-                }
-
-                paragraph.Actions.Add(action);
-            }
+                paragraph.Actions.Add(ActionParse(xmlAction));
 
             foreach (XmlNode xmlModification in xmlParagraph.SelectNodes("Modifications/Modification"))
                 paragraph.Modification.Add(Game.Xml.ModificationParse(xmlModification, new Modification()));
 
             return paragraph;
+        }
+
+        private Actions ActionParse(XmlNode xmlAction)
+        {
+            Actions action = new Actions
+            {
+                ActionName = Game.Xml.StringParse(xmlAction["ActionName"]),
+                ButtonName = Game.Xml.StringParse(xmlAction["ButtonName"]),
+                Aftertext = Game.Xml.StringParse(xmlAction["Aftertext"]),
+                Trigger = Game.Xml.StringParse(xmlAction["Trigger"]),
+                Text = Game.Xml.StringParse(xmlAction["Text"]),
+                Skill = Game.Xml.StringParse(xmlAction["Skill"]),
+                RemoveTrigger = Game.Xml.StringParse(xmlAction["RemoveTrigger"]),
+                SellType = Game.Xml.StringParse(xmlAction["SellType"]),
+
+                Price = Game.Xml.IntParse(xmlAction["Price"]),
+
+                Choice = Game.Xml.BoolParse(xmlAction["Choice"]),
+                Multiple = Game.Xml.BoolParse(xmlAction["Multiple"]),
+                Sell = Game.Xml.BoolParse(xmlAction["Sell"]),
+                SellIfAvailable = Game.Xml.BoolParse(xmlAction["SellIfAvailable"]),
+                Split = Game.Xml.BoolParse(xmlAction["Split"]),
+            };
+
+            if (xmlAction["Benefit"] != null)
+            {
+                action.BenefitList = new List<Abstract.IModification>();
+
+                foreach (XmlNode bonefit in xmlAction.SelectNodes("Benefit"))
+                    action.BenefitList.Add(Game.Xml.ModificationParse(bonefit, new Modification()));
+            }
+
+            return action;
+        }
+
+        private Option OptionParse(XmlNode xmlOption)
+        {
+            Option option = OptionsTemplate(xmlOption);
+
+            if (xmlOption.Attributes["Do"] != null)
+                option.Do = Game.Xml.ModificationParse(xmlOption, new Modification(), name: "Do");
+
+            return option;
         }
     }
 }
