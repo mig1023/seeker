5da5e3fde4ff8fde528610955d072538f3e67c4a
mig1023 <mig1023@yandex.ru>
2021-06-16 17:35:33 +0300

    paragraphs refactoring (sword)

--- Seeker/Seeker/Gamebook/FaithfulSwordOfTheKing/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/FaithfulSwordOfTheKing/Paragraphs.cs
@@ -18,71 +18,65 @@ namespace Seeker.Gamebook.FaithfulSwordOfTheKing
             Game.Paragraph paragraph = ParagraphTemplate(xmlParagraph);
 
             foreach (XmlNode xmlOption in xmlParagraph.SelectNodes("Options/Option"))
-            {
-                Option option = OptionsTemplate(xmlOption);
-
-                if (xmlOption.Attributes["Do"] != null)
-                    option.Do = Game.Xml.ModificationParse(xmlOption, new Modification(), name: "Do");
-
-                paragraph.Options.Add(option);
-            }
+                paragraph.Options.Add(OptionParse(xmlOption));
 
             foreach (XmlNode xmlAction in xmlParagraph.SelectNodes("Actions/Action"))
-            {
-                Actions action = new Actions
-                {
-                    ActionName = Game.Xml.StringParse(xmlAction["ActionName"]),
-                    ButtonName = Game.Xml.StringParse(xmlAction["ButtonName"]),
-                    Aftertext = Game.Xml.StringParse(xmlAction["Aftertext"]),
-                    Trigger = Game.Xml.StringParse(xmlAction["Trigger"]),
-                    Text = Game.Xml.StringParse(xmlAction["Text"]),
-
-                    RoundsToWin = Game.Xml.IntParse(xmlAction["RoundsToWin"]),
-                    WoundsToWin = Game.Xml.IntParse(xmlAction["WoundsToWin"]),
-                    SkillPenalty = Game.Xml.IntParse(xmlAction["SkillPenalty"]),
-                    Price = Game.Xml.IntParse(xmlAction["Price"]),
-
-                    MeritalArt = MeritalArtsParse(xmlAction["MeritalArt"]),
-
-                    Multiple = Game.Xml.BoolParse(xmlAction["Multiple"]),
-                    WithoutShooting = Game.Xml.BoolParse(xmlAction["WithoutShooting"]),
-                };
-
-                if (xmlAction["Benefit"] != null)
-                {
-                    action.BenefitList = new List<Abstract.IModification>();
-
-                    foreach (XmlNode bonefit in xmlAction.SelectNodes("Benefit"))
-                        action.BenefitList.Add(ModificationParse(bonefit));
-                }
-
-                if (xmlAction["Enemies"] != null)
-                {
-                    action.Enemies = new List<Character>();
-
-                    foreach (XmlNode xmlEnemy in xmlAction.SelectNodes("Enemies/Enemy"))
-                    {
-                        Character enemy = new Character
-                        {
-                            Name = Game.Xml.StringParse(xmlEnemy.Attributes["Name"]),
-                            MaxSkill = Game.Xml.IntParse(xmlEnemy.Attributes["Skill"]),
-                            MaxStrength = Game.Xml.IntParse(xmlEnemy.Attributes["Strength"]),
-                        };
-
-                        enemy.Skill = enemy.MaxSkill;
-                        enemy.Strength = enemy.MaxStrength;
-
-                        action.Enemies.Add(enemy);
-                    }
-                }
-
-                paragraph.Actions.Add(action);
-            }
+                paragraph.Actions.Add(ActionParse(xmlAction));
 
             foreach (XmlNode xmlModification in xmlParagraph.SelectNodes("Modifications/Modification"))
                 paragraph.Modification.Add(ModificationParse(xmlModification));
 
             return paragraph;
+        }
+
+        private Actions ActionParse(XmlNode xmlAction)
+        {
+            Actions action = new Actions
+            {
+                ActionName = Game.Xml.StringParse(xmlAction["ActionName"]),
+                ButtonName = Game.Xml.StringParse(xmlAction["ButtonName"]),
+                Aftertext = Game.Xml.StringParse(xmlAction["Aftertext"]),
+                Trigger = Game.Xml.StringParse(xmlAction["Trigger"]),
+                Text = Game.Xml.StringParse(xmlAction["Text"]),
+
+                RoundsToWin = Game.Xml.IntParse(xmlAction["RoundsToWin"]),
+                WoundsToWin = Game.Xml.IntParse(xmlAction["WoundsToWin"]),
+                SkillPenalty = Game.Xml.IntParse(xmlAction["SkillPenalty"]),
+                Price = Game.Xml.IntParse(xmlAction["Price"]),
+
+                MeritalArt = MeritalArtsParse(xmlAction["MeritalArt"]),
+
+                Multiple = Game.Xml.BoolParse(xmlAction["Multiple"]),
+                WithoutShooting = Game.Xml.BoolParse(xmlAction["WithoutShooting"]),
+            };
+
+            if (xmlAction["Benefit"] != null)
+            {
+                action.BenefitList = new List<Abstract.IModification>();
+
+                foreach (XmlNode bonefit in xmlAction.SelectNodes("Benefit"))
+                    action.BenefitList.Add(ModificationParse(bonefit));
+            }
+
+            if (xmlAction["Enemies"] != null)
+            {
+                action.Enemies = new List<Character>();
+
+                foreach (XmlNode xmlEnemy in xmlAction.SelectNodes("Enemies/Enemy"))
+                    action.Enemies.Add(EnemyParse(xmlEnemy));
+            }
+
+            return action;
+        }
+
+        private Option OptionParse(XmlNode xmlOption)
+        {
+            Option option = OptionsTemplate(xmlOption);
+
+            if (xmlOption.Attributes["Do"] != null)
+                option.Do = Game.Xml.ModificationParse(xmlOption, new Modification(), name: "Do");
+
+            return option;
         }
 
         private static Character.MeritalArts MeritalArtsParse(XmlNode xmlNode)
@@ -93,6 +87,21 @@ namespace Seeker.Gamebook.FaithfulSwordOfTheKing
             bool success = Enum.TryParse(xmlNode.InnerText, out Character.MeritalArts value);
 
             return (success ? value : Character.MeritalArts.Nope);
+        }
+
+        private static Character EnemyParse(XmlNode xmlEnemy)
+        {
+            Character enemy = new Character
+            {
+                Name = Game.Xml.StringParse(xmlEnemy.Attributes["Name"]),
+                MaxSkill = Game.Xml.IntParse(xmlEnemy.Attributes["Skill"]),
+                MaxStrength = Game.Xml.IntParse(xmlEnemy.Attributes["Strength"]),
+            };
+
+            enemy.Skill = enemy.MaxSkill;
+            enemy.Strength = enemy.MaxStrength;
+
+            return enemy;
         }
 
         private static Modification ModificationParse(XmlNode xmlNode)
