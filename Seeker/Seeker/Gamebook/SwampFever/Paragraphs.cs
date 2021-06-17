048f58717150120cde8ec6c992d261e2b5d7aa4b
mig1023 <mig1023@yandex.ru>
2021-06-17 17:26:30 +0300

    OptionParse fix (swamp fever)

--- Seeker/Seeker/Gamebook/SwampFever/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/SwampFever/Paragraphs.cs
@@ -30,6 +30,8 @@ namespace Seeker.Gamebook.SwampFever
             Benefit = ModificationParse(xmlAction["Benefit"]),
         };
 
+        public override Option OptionParse(XmlNode xmlOption) => OptionsTemplate(xmlOption);
+
         public override Abstract.IModification ModificationParse(XmlNode xmlNode)
         {
             if (xmlNode == null)
