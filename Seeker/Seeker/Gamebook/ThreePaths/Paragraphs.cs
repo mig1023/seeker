2b9bb8ff51881e1eff3df10b6525b79fa553a300
mig1023 <mig1023@yandex.ru>
2021-06-17 17:47:14 +0300

    OptionParseWithDo (three paths)

--- Seeker/Seeker/Gamebook/ThreePaths/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/ThreePaths/Paragraphs.cs
@@ -26,15 +26,7 @@ namespace Seeker.Gamebook.ThreePaths
             ThisIsSpell = Game.Xml.BoolParse(xmlAction["ThisIsSpell"]),
         };
 
-        public override Option OptionParse(XmlNode xmlOption)
-        {
-            Option option = OptionsTemplate(xmlOption);
-
-            if (xmlOption.Attributes["Do"] != null)
-                option.Do = Game.Xml.ModificationParse(xmlOption, new Modification(), name: "Do");
-
-            return option;
-        }
+        public override Option OptionParse(XmlNode xmlOption) => OptionParseWithDo(xmlOption, new Modification());
 
         public override Abstract.IModification ModificationParse(XmlNode xmlNode)
         {
