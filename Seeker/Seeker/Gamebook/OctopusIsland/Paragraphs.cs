56249c7696ef4883645db9f5849f933faf3d8fb0
mig1023 <mig1023@yandex.ru>
2021-06-17 17:22:23 +0300

    GetTemplateModDefault (octopus)

--- Seeker/Seeker/Gamebook/OctopusIsland/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/OctopusIsland/Paragraphs.cs
@@ -13,7 +13,7 @@ namespace Seeker.Gamebook.OctopusIsland
     {
         public static Paragraphs StaticInstance = new Paragraphs();
 
-        public override Game.Paragraph Get(int id, XmlNode xmlParagraph) => GetTemplateModXml(xmlParagraph, new Modification());
+        public override Game.Paragraph Get(int id, XmlNode xmlParagraph) => GetTemplateModDefault(xmlParagraph, new Modification());
 
         public override Abstract.IActions ActionParse(XmlNode xmlAction)
         {
