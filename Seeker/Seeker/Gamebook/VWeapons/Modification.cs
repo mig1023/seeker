7bc55df9036f34052f37a92e1087560e158c778d
mig1023 <mig1023@yandex.ru>
2021-07-02 17:07:07 +0300

    Davis/RemoveTrigger in Modifications

--- Seeker/Seeker/Gamebook/VWeapons/Modification.cs
+++ Seeker/Seeker/Gamebook/VWeapons/Modification.cs
@@ -13,6 +13,15 @@ namespace Seeker.Gamebook.VWeapons
 
             else if (Name == "Shadow")
                 ModificationByTrigger("V", () => Character.Protagonist.Suspicions += 1);
+            
+            else if (Name == "Davis")
+            {
+                ModificationByTrigger("D", () => Character.Protagonist.Suspicions += 2);
+                ModificationByTrigger("D", () => Character.Protagonist.Suspicions += 1, not: true);
+            }
+
+            else if (Name == "RemoveTrigger")
+                Game.Option.Trigger(ValueString, remove: true);
 
             else
                 InnerDo(Character.Protagonist);
