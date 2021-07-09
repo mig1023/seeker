79eb1c30a5c43996c42da0bbf798f0017dd6e3f4
mig1023 <mig1023@yandex.ru>
2021-07-09 17:55:41 +0300

    pargraph parse fix

--- Seeker/Seeker/Gamebook/MentorsAlwaysRight/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/MentorsAlwaysRight/Paragraphs.cs
@@ -12,5 +12,7 @@ namespace Seeker.Gamebook.MentorsAlwaysRight
     class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs
     {
         public static Paragraphs StaticInstance = new Paragraphs();
+
+        public override Game.Paragraph Get(int id, XmlNode xmlParagraph) => GetTemplate(xmlParagraph);
     }
 }
