1ee0e1a328a7c953c48af36d59cc0bdfcc32e2b5
mig1023 <mig1023@yandex.ru>
2021-07-19 18:17:36 +0300

    GetParagraphsWithoutStaticsButtons

--- Seeker/Seeker/Gamebook/MentorsAlwaysRight/Constants.cs
+++ Seeker/Seeker/Gamebook/MentorsAlwaysRight/Constants.cs
@@ -28,6 +28,8 @@ namespace Seeker.Gamebook.MentorsAlwaysRight
             [ColorTypes.AdditionalFont] = "#ced4de",
         };
 
+        public static List<int> GetParagraphsWithoutStaticsButtons() => new List<int> { 0, 556, 557, 558, 559 };
+
         public override List<int> GetParagraphsWithoutStatuses() => new List<int> { 0, 556, 557, 558, 559 };
     }
 }
