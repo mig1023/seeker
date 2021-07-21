0ebe3a2e4512a9541fc0071439727f35a3db9ab7
mig1023 <mig1023@yandex.ru>
2021-07-21 21:15:17 +0300

    color revert (those who are about)

--- Seeker/Seeker/Gamebook/ThoseWhoAreAboutToDie/Constants.cs
+++ Seeker/Seeker/Gamebook/ThoseWhoAreAboutToDie/Constants.cs
@@ -22,7 +22,6 @@ namespace Seeker.Gamebook.ThoseWhoAreAboutToDie
 
         public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
         {
-            [ColorTypes.Background] = "#fef4d5",
             [ColorTypes.StatusBar] = "#fce391",
             [ColorTypes.StatusFont] = "#000000",
         };
