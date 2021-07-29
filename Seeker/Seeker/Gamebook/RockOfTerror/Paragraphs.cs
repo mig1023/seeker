e8bb5a93c26c3274b37e8cd6b21e245ad97fef3a
mig1023 <mig1023@yandex.ru>
2021-07-29 17:33:42 +0300

    game.xml in paragraph ltl refactoring (rock)

--- Seeker/Seeker/Gamebook/RockOfTerror/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/RockOfTerror/Paragraphs.cs
@@ -13,15 +13,15 @@ namespace Seeker.Gamebook.RockOfTerror
     {
         public static Paragraphs StaticInstance = new Paragraphs();
 
-        public override Game.Paragraph Get(int id, XmlNode xmlParagraph) => GetTemplate(xmlParagraph);
+        public override Paragraph Get(int id, XmlNode xmlParagraph) => GetTemplate(xmlParagraph);
 
         public override Abstract.IActions ActionParse(XmlNode xmlAction) => (Actions)ActionTemplate(xmlAction, new Actions());
 
         public override Abstract.IModification ModificationParse(XmlNode xmlModification) => new Modification
         {
-            Name = Game.Xml.StringParse(xmlModification.Attributes["Name"]),
-            Value = Game.Xml.IntParse(xmlModification.Attributes["Value"]),
-            Init = Game.Xml.BoolParse(xmlModification.Attributes["Init"]),
+            Name = Xml.StringParse(xmlModification.Attributes["Name"]),
+            Value = Xml.IntParse(xmlModification.Attributes["Value"]),
+            Init = Xml.BoolParse(xmlModification.Attributes["Init"]),
         };
     }
 }
