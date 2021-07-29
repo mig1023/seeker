277e2d5d00073c53daa98e3004f46c1c7aaa2d1c
mig1023 <mig1023@yandex.ru>
2021-07-29 17:30:37 +0300

    game.xml in paragraph ltl refactoring (heart)

--- Seeker/Seeker/Gamebook/HeartOfIce/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/HeartOfIce/Paragraphs.cs
@@ -13,26 +13,26 @@ namespace Seeker.Gamebook.HeartOfIce
     {
         public static Paragraphs StaticInstance = new Paragraphs();
 
-        public override Game.Paragraph Get(int id, XmlNode xmlParagraph) => GetTemplate(xmlParagraph);
+        public override Paragraph Get(int id, XmlNode xmlParagraph) => GetTemplate(xmlParagraph);
 
         public override Abstract.IActions ActionParse(XmlNode xmlAction)
         {
             Actions action = (Actions)ActionTemplate(xmlAction, new Actions());
 
-            action.Skill = Game.Xml.StringParse(xmlAction["Skill"]);
-            action.RemoveTrigger = Game.Xml.StringParse(xmlAction["RemoveTrigger"]);
-            action.SellType = Game.Xml.StringParse(xmlAction["SellType"]);
-            action.Choice = Game.Xml.BoolParse(xmlAction["Choice"]);
-            action.Sell = Game.Xml.BoolParse(xmlAction["Sell"]);
-            action.SellIfAvailable = Game.Xml.BoolParse(xmlAction["SellIfAvailable"]);
-            action.Split = Game.Xml.BoolParse(xmlAction["Split"]);
+            action.Skill = Xml.StringParse(xmlAction["Skill"]);
+            action.RemoveTrigger = Xml.StringParse(xmlAction["RemoveTrigger"]);
+            action.SellType = Xml.StringParse(xmlAction["SellType"]);
+            action.Choice = Xml.BoolParse(xmlAction["Choice"]);
+            action.Sell = Xml.BoolParse(xmlAction["Sell"]);
+            action.SellIfAvailable = Xml.BoolParse(xmlAction["SellIfAvailable"]);
+            action.Split = Xml.BoolParse(xmlAction["Split"]);
 
             if (xmlAction["Benefit"] != null)
             {
                 action.BenefitList = new List<Abstract.IModification>();
 
                 foreach (XmlNode bonefit in xmlAction.SelectNodes("Benefit"))
-                    action.BenefitList.Add(Game.Xml.ModificationParse(bonefit, new Modification()));
+                    action.BenefitList.Add(Xml.ModificationParse(bonefit, new Modification()));
             }
 
             return action;
@@ -41,6 +41,6 @@ namespace Seeker.Gamebook.HeartOfIce
         public override Option OptionParse(XmlNode xmlOption) => OptionParseWithDo(xmlOption, new Modification());
 
         public override Abstract.IModification ModificationParse(XmlNode xmlModification) =>
-            Game.Xml.ModificationParse(xmlModification, new Modification());
+            Xml.ModificationParse(xmlModification, new Modification());
     }
 }
