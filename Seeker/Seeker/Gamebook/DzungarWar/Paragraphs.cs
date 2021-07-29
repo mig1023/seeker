64d2da1ba23fa9278e0a53ca4a4412ec06d19d6b
mig1023 <mig1023@yandex.ru>
2021-07-29 17:29:46 +0300

    game.xml in paragraph ltl refactoring (dzungar)

--- Seeker/Seeker/Gamebook/DzungarWar/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/DzungarWar/Paragraphs.cs
@@ -13,18 +13,18 @@ namespace Seeker.Gamebook.DzungarWar
     {
         public static Paragraphs StaticInstance = new Paragraphs();
 
-        public override Game.Paragraph Get(int id, XmlNode xmlParagraph) => GetTemplate(xmlParagraph);
+        public override Paragraph Get(int id, XmlNode xmlParagraph) => GetTemplate(xmlParagraph);
 
         public override Abstract.IActions ActionParse(XmlNode xmlAction)
         {
             Actions action = (Actions)ActionTemplate(xmlAction, new Actions());
 
-            action.RemoveTrigger = Game.Xml.StringParse(xmlAction["RemoveTrigger"]);
-            action.Stat = Game.Xml.StringParse(xmlAction["Stat"]);
-            action.TriggerTestPenalty = Game.Xml.StringParse(xmlAction["TriggerTestPenalty"]);
-            action.Level = Game.Xml.IntParse(xmlAction["Level"]);
-            action.StatStep = Game.Xml.IntParse(xmlAction["StatStep"]);
-            action.StatToMax = Game.Xml.BoolParse(xmlAction["StatToMax"]);
+            action.RemoveTrigger = Xml.StringParse(xmlAction["RemoveTrigger"]);
+            action.Stat = Xml.StringParse(xmlAction["Stat"]);
+            action.TriggerTestPenalty = Xml.StringParse(xmlAction["TriggerTestPenalty"]);
+            action.Level = Xml.IntParse(xmlAction["Level"]);
+            action.StatStep = Xml.IntParse(xmlAction["StatStep"]);
+            action.StatToMax = Xml.BoolParse(xmlAction["StatToMax"]);
             action.Benefit = ModificationParse(xmlAction["Benefit"]);
 
             if (action.Name == "Option")
@@ -42,10 +42,10 @@ namespace Seeker.Gamebook.DzungarWar
 
             Modification modification = new Modification
             {
-                Name = Game.Xml.StringParse(xmlNode.Attributes["Name"]),
-                Value = Game.Xml.IntParse(xmlNode.Attributes["Value"]),
-                Empty = Game.Xml.BoolParse(xmlNode.Attributes["Empty"]),
-                Init = Game.Xml.BoolParse(xmlNode.Attributes["Init"]),
+                Name = Xml.StringParse(xmlNode.Attributes["Name"]),
+                Value = Xml.IntParse(xmlNode.Attributes["Value"]),
+                Empty = Xml.BoolParse(xmlNode.Attributes["Empty"]),
+                Init = Xml.BoolParse(xmlNode.Attributes["Init"]),
             };
 
             return modification;
