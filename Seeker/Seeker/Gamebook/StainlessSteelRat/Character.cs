f13291bc3a2e18cefbdfb6813f01e253be1c0ef4
mig1023 <mig1023@yandex.ru>
2021-07-30 21:43:15 +0300

    ltl refactoring v.2 (rat)

--- Seeker/Seeker/Gamebook/StainlessSteelRat/Character.cs
+++ Seeker/Seeker/Gamebook/StainlessSteelRat/Character.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Seeker.Gamebook.StainlessSteelRat
 {
     class Character : Prototypes.Character, Abstract.ICharacter
     {
-        public static Character Protagonist = new Gamebook.StainlessSteelRat.Character();
+        public static Character Protagonist = new Character();
     }
 }
