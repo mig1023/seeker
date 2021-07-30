f87f77183d7e5a0a4104b722d9f7de0099d346aa
mig1023 <mig1023@yandex.ru>
2021-07-30 21:49:55 +0300

    ltl refactoring v2.1 (last hokku)

--- Seeker/Seeker/Gamebook/LastHokku/Actions.cs
+++ Seeker/Seeker/Gamebook/LastHokku/Actions.cs
@@ -6,15 +6,16 @@ namespace Seeker.Gamebook.LastHokku
     class Actions : Prototypes.Actions, Abstract.IActions
     {
         public static Actions StaticInstance = new Actions();
+        private static Character protogonist = Character.Protagonist;
 
         public override string TextByOptions(string option)
         {
             if (!Constants.GetParagraphsWithoutHokkuCreation().Contains(Game.Data.CurrentParagraphID) && !String.IsNullOrEmpty(option))
-                Character.Protagonist.Hokku.Add(option);
+                protogonist.Hokku.Add(option);
 
-            if (Character.Protagonist.Hokku.Count >= 7)
+            if (protogonist.Hokku.Count >= 7)
             {
-                List<string> oldHokku = Character.Protagonist.Hokku;
+                List<string> oldHokku = protogonist.Hokku;
 
                 oldHokku[2] = new System.Globalization.CultureInfo("ru-RU", false).TextInfo.ToTitleCase(oldHokku[2]);
 
@@ -25,10 +26,10 @@ namespace Seeker.Gamebook.LastHokku
                     String.Format("{0} - {1} {2}...", oldHokku[4], oldHokku[5], oldHokku[6]),
                 };
 
-                Character.Protagonist.Hokku = newHokku;
+                protogonist.Hokku = newHokku;
             }
 
-            return String.Join("\n", Character.Protagonist.Hokku);
+            return String.Join("\n", protogonist.Hokku);
         }
     }
 }
