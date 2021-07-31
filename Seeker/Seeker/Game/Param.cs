8ccbc621c1573df34ceefef4f6da7c1ca40dca18
mig1023 <mig1023@yandex.ru>
2021-07-31 18:48:18 +0300

    param setter

---
+++ Seeker/Seeker/Game/Param.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Seeker.Game
+{
+    class Param
+    {
+        public static int Setter(int value) => (value < 0 ? 0 : value);
+
+        public static int Setter(int value, int max) => (value > max ? max : Setter(value));
+
+        public static int? Setter(int? value, int max) => (value.HasValue ? Setter((int)value, max) : (int?)null);
+    }
+}
