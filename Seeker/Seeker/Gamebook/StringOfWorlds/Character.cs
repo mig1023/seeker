5576f7044894be0fd1f703eb994f87cf2631f7d2
mig1023 <mig1023@yandex.ru>
2021-07-31 19:01:37 +0300

    param setter (string)

--- Seeker/Seeker/Gamebook/StringOfWorlds/Character.cs
+++ Seeker/Seeker/Gamebook/StringOfWorlds/Character.cs
@@ -13,15 +13,7 @@ namespace Seeker.Gamebook.StringOfWorlds
         public int Skill
         {
             get => _skill;
-            set
-            {
-                if (value > MaxSkill)
-                    _skill = MaxSkill;
-                else if (value < 0)
-                    _skill = 0;
-                else
-                    _skill = value;
-            }
+            set => _skill = Game.Param.Setter(value, max: MaxSkill);
         }
 
         private int _strength;
@@ -29,15 +21,7 @@ namespace Seeker.Gamebook.StringOfWorlds
         public int Strength
         {
             get => _strength;
-            set
-            {
-                if (value > MaxStrength)
-                    _strength = MaxStrength;
-                else if (value < 0)
-                    _strength = 0;
-                else
-                    _strength = value;
-            }
+            set => _strength = Game.Param.Setter(value, max: MaxStrength);
         }
 
         private int _charm;
