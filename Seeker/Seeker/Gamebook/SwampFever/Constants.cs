9163ecdec58324ce2c76e44261fc248babd9785a
mig1023 <mig1023@yandex.ru>
2021-08-02 18:30:37 +0300

    getlinks (swamp fever)

--- Seeker/Seeker/Gamebook/SwampFever/Constants.cs
+++ Seeker/Seeker/Gamebook/SwampFever/Constants.cs
@@ -70,5 +70,16 @@ namespace Seeker.Gamebook.SwampFever
                 },
             };
         }
+
+        public static Links GetLinks() => new Links
+        {
+            Protagonist = Character.Protagonist.Init,
+            CheckOnlyIf = Actions.StaticInstance.CheckOnlyIf,
+            Paragraphs = Paragraphs.StaticInstance,
+            Actions = Actions.StaticInstance,
+            Constants = StaticInstance,
+            Save = Character.Protagonist.Save,
+            Load = Character.Protagonist.Load,
+        };
     }
 }
