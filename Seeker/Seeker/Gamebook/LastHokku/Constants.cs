3c96a41c9d904c5cc35ddb86dfa1ee1a9558a74b
mig1023 <mig1023@yandex.ru>
2021-08-02 18:27:44 +0300

    getlinks (hokku)

--- Seeker/Seeker/Gamebook/LastHokku/Constants.cs
+++ Seeker/Seeker/Gamebook/LastHokku/Constants.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using static Seeker.Output.Buttons;
-using static Seeker.Game.Data;
 
 namespace Seeker.Gamebook.LastHokku
 {
@@ -17,5 +16,16 @@ namespace Seeker.Gamebook.LastHokku
         public override Output.Interface.TextFontSize GetFontSize() => Output.Interface.TextFontSize.big;
 
         public static List<int> GetParagraphsWithoutHokkuCreation() => new List<int> { 0, 1, 9, 10, 11, 12 };
+
+        public static Links GetLinks() => new Links
+        {
+            Protagonist = Character.Protagonist.Init,
+            CheckOnlyIf = Actions.StaticInstance.CheckOnlyIf,
+            Paragraphs = Paragraphs.StaticInstance,
+            Actions = Actions.StaticInstance,
+            Constants = StaticInstance,
+            Save = Character.Protagonist.Save,
+            Load = Character.Protagonist.Load,
+        };
     }
 }
