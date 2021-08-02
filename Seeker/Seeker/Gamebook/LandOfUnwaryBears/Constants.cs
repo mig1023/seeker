55f12f4717093a2df89ed232d9a6a89d02a23165
mig1023 <mig1023@yandex.ru>
2021-08-02 18:27:30 +0300

    getlinks (bears)

--- Seeker/Seeker/Gamebook/LandOfUnwaryBears/Constants.cs
+++ Seeker/Seeker/Gamebook/LandOfUnwaryBears/Constants.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using static Seeker.Output.Buttons;
-using static Seeker.Game.Data;
 
 namespace Seeker.Gamebook.LandOfUnwaryBears
 {
@@ -16,5 +15,16 @@ namespace Seeker.Gamebook.LandOfUnwaryBears
         public override string GetFont() => "RobotoFont";
 
         public override Output.Interface.TextFontSize GetFontSize() => Output.Interface.TextFontSize.small;
+
+        public static Links GetLinks() => new Links
+        {
+            Protagonist = Character.Protagonist.Init,
+            CheckOnlyIf = Actions.StaticInstance.CheckOnlyIf,
+            Paragraphs = Paragraphs.StaticInstance,
+            Actions = Actions.StaticInstance,
+            Constants = StaticInstance,
+            Save = Character.Protagonist.Save,
+            Load = Character.Protagonist.Load,
+        };
     }
 }
