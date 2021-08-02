a1ed425025730a95ca3332fadf90d8b6eeab4931
mig1023 <mig1023@yandex.ru>
2021-08-02 18:26:03 +0300

    getlinks (dzungar war)

--- Seeker/Seeker/Gamebook/DzungarWar/Constants.cs
+++ Seeker/Seeker/Gamebook/DzungarWar/Constants.cs
@@ -38,5 +38,16 @@ namespace Seeker.Gamebook.DzungarWar
         };
 
         public override bool ShowDisabledOption() => true;
+
+        public static Links GetLinks() => new Links
+        {
+            Protagonist = Character.Protagonist.Init,
+            CheckOnlyIf = Actions.StaticInstance.CheckOnlyIf,
+            Paragraphs = Paragraphs.StaticInstance,
+            Actions = Actions.StaticInstance,
+            Constants = StaticInstance,
+            Save = Character.Protagonist.Save,
+            Load = Character.Protagonist.Load,
+        };
     }
 }
