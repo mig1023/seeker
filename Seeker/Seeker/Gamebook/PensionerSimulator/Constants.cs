7976020533a3d944c210d167964749c516bb9ffd
mig1023 <mig1023@yandex.ru>
2021-08-02 18:29:03 +0300

    getlinks (pensioner)

--- Seeker/Seeker/Gamebook/PensionerSimulator/Constants.cs
+++ Seeker/Seeker/Gamebook/PensionerSimulator/Constants.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using static Seeker.Output.Buttons;
-using static Seeker.Game.Data;
 
 namespace Seeker.Gamebook.PensionerSimulator
 {
@@ -15,5 +14,16 @@ namespace Seeker.Gamebook.PensionerSimulator
         };
 
         public override Output.Interface.TextFontSize GetFontSize() => Output.Interface.TextFontSize.big;
+
+        public static Links GetLinks() => new Links
+        {
+            Protagonist = Character.Protagonist.Init,
+            CheckOnlyIf = Actions.StaticInstance.CheckOnlyIf,
+            Paragraphs = Paragraphs.StaticInstance,
+            Actions = Actions.StaticInstance,
+            Constants = StaticInstance,
+            Save = Character.Protagonist.Save,
+            Load = Character.Protagonist.Load,
+        };
     }
 }
