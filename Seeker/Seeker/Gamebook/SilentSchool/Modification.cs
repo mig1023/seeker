5610b9c8a93fd34b86233d8c9569b751264c4589
mig1023 <mig1023@yandex.ru>
2021-08-07 21:35:45 +0300

    modification refactoring (school)

--- Seeker/Seeker/Gamebook/SilentSchool/Modification.cs
+++ Seeker/Seeker/Gamebook/SilentSchool/Modification.cs
@@ -10,18 +10,18 @@ namespace Seeker.Gamebook.SilentSchool
         {
             int woundsBonus = (Game.Data.Triggers.Contains("Кайф") ? 2 : 1);
 
-            bool trigger = ModificationByName("Trigger", () => Game.Option.Trigger(Value.ToString()));
-            bool change = ModificationByName("Change", () => protogonist.ChangeDecision = Value);
-            bool weapon = ModificationByName("Weapon", () => protogonist.Weapon = ValueString);
-            bool removeWeapon = ModificationByName("RemoveWeapon", () => protogonist.Weapon = String.Empty);
+            bool trigger = DoByName("Trigger", () => Game.Option.Trigger(Value.ToString()));
+            bool change = DoByName("Change", () => protogonist.ChangeDecision = Value);
+            bool weapon = DoByName("Weapon", () => protogonist.Weapon = ValueString);
+            bool removeWeapon = DoByName("RemoveWeapon", () => protogonist.Weapon = String.Empty);
 
-            bool woundsByWeapon2 = ModificationByName("WoundsByWeapon2",
+            bool woundsByWeapon2 = DoByName("WoundsByWeapon2",
                 () => protogonist.Life -= (String.IsNullOrEmpty(protogonist.Weapon) ? 4 : 2) * woundsBonus);
-            bool woundsByWeapon3 = ModificationByName("WoundsByWeapon3",
+            bool woundsByWeapon3 = DoByName("WoundsByWeapon3",
                 () => protogonist.Life -= (protogonist.Weapon == "Гантеля" ? 3 : 6) * woundsBonus);
-            bool woundsByWeapon4 = ModificationByName("WoundsByWeapon4",
+            bool woundsByWeapon4 = DoByName("WoundsByWeapon4",
                 () => protogonist.Life -= (protogonist.Weapon == "Флейта" ? 1 : 4) * woundsBonus);
-            bool woundsByBody = ModificationByName("WoundsByBody",
+            bool woundsByBody = DoByName("WoundsByBody",
                 () => protogonist.Life -= (Game.Data.Triggers.Contains("Толстяк") ? 4 : 6) * woundsBonus);
 
             if (trigger || change || weapon || removeWeapon || woundsByWeapon2 || woundsByWeapon3 || woundsByWeapon4 || woundsByBody)
@@ -37,11 +37,11 @@ namespace Seeker.Gamebook.SilentSchool
 
             else
             {
-                int currentValue = (int)protogonist.GetType().GetProperty(Name).GetValue(protogonist, null);
+                int currentValue = GetProperty(protogonist, Name);
 
                 currentValue += Value * ((woundsBonus > 1) && (Value < 1) ? woundsBonus : 1);
 
-                protogonist.GetType().GetProperty(Name).SetValue(protogonist, currentValue);
+                SetProperty(protogonist, Name, currentValue);
             }
         }
     }
