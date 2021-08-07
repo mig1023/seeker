d6d9bea387e7936759bb5ef63869fb4755cd1e79
mig1023 <mig1023@yandex.ru>
2021-08-07 21:11:16 +0300

    system menu (genesis)

--- Seeker/Seeker/Gamebook/Genesis/Constants.cs
+++ Seeker/Seeker/Gamebook/Genesis/Constants.cs
@@ -12,8 +12,9 @@ namespace Seeker.Gamebook.Genesis
         {
             [ButtonTypes.Main] = "#4c687c",
             [ButtonTypes.Option] = "#6f8696",
-            [ButtonTypes.Continue] = "#849fb3",
             [ButtonTypes.Action] = "#445d6f",
+            [ButtonTypes.Continue] = "#849fb3",
+            [ButtonTypes.System] = "#3c5363",
         };
 
         public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
@@ -22,6 +23,7 @@ namespace Seeker.Gamebook.Genesis
             [ColorTypes.Font] = "#b6cbd8",
             [ColorTypes.StatusBar] = "#3c5363",
             [ColorTypes.ActionBox] = "#69707a",
+            [ColorTypes.SystemFont] = "#b1b5ba",
         };
 
         public static Dictionary<string, int> GetStartValues() => new Dictionary<string, int>
