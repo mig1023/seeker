051583f1f856d972851490bd3fd0b025ab600a88
mig1023 <mig1023@yandex.ru>
2021-08-07 21:13:21 +0300

    system menu (lord)

--- Seeker/Seeker/Gamebook/LordOfTheSteppes/Constants.cs
+++ Seeker/Seeker/Gamebook/LordOfTheSteppes/Constants.cs
@@ -13,6 +13,8 @@ namespace Seeker.Gamebook.LordOfTheSteppes
             [ButtonTypes.Main] = "#b80f0a",
             [ButtonTypes.Action] = "#a92605",
             [ButtonTypes.Option] = "#878787",
+            [ButtonTypes.Continue] = "#db8784",
+            [ButtonTypes.System] = "#f2f2f2",
         };
 
         public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
