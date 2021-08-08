7648132f9af90741d169db909902d5436df7b157
mig1023 <mig1023@yandex.ru>
2021-08-08 14:09:22 +0300

    tournament misprints fixes

--- Seeker/Seeker/Gamebook/YounglingTournament/Actions.cs
+++ Seeker/Seeker/Gamebook/YounglingTournament/Actions.cs
@@ -6,19 +6,19 @@ namespace Seeker.Gamebook.YounglingTournament
     class Actions : Prototypes.Actions, Abstract.IActions
     {
         public static Actions StaticInstance = new Actions();
-        private static Character protogonist = Character.Protagonist;
+        private static Character protagonist = Character.Protagonist;
 
         public override List<string> Status() => new List<string>
         {
-            String.Format("Cветлая сторона: {0}", protogonist.LightSide),
-            String.Format("Тёмная сторона: {0}", protogonist.DarkSide),
+            String.Format("Cветлая сторона: {0}", protagonist.LightSide),
+            String.Format("Тёмная сторона: {0}", protagonist.DarkSide),
         };
 
         public override List<string> AdditionalStatus() => new List<string>
         {
-            String.Format("Взлом: {0}", protogonist.Hacking),
-            String.Format("Скрытность: {0}", protogonist.Stealth),
-            String.Format("Пилот: {0}", protogonist.Pilot),
+            String.Format("Взлом: {0}", protagonist.Hacking),
+            String.Format("Скрытность: {0}", protagonist.Stealth),
+            String.Format("Пилот: {0}", protagonist.Pilot),
         };
     }
 }
