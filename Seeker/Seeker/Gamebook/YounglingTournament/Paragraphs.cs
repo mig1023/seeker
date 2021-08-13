df9be14b62ffc62978b3653deb7712bdbef556e7
mig1023 <mig1023@yandex.ru>
2021-08-13 20:14:46 +0300

    action parse refactoring (tournament)

--- Seeker/Seeker/Gamebook/YounglingTournament/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/YounglingTournament/Paragraphs.cs
@@ -9,20 +9,8 @@ namespace Seeker.Gamebook.YounglingTournament
 
         public override Paragraph Get(int id, XmlNode xmlParagraph) => base.Get(xmlParagraph);
 
-        public override Abstract.IActions ActionParse(XmlNode xmlAction)
-        {
-            Actions action = (Actions)ActionTemplate(xmlAction, new Actions());
-
-            foreach (string param in Constants.GetActionParams())
-                SetProperty(action, param, xmlAction);
-
-            action.Benefit = ModificationParse(xmlAction["Benefit"]);
-
-            if (action.Name == "Option")
-                action.Option = OptionParse(xmlAction["Option"]);
-
-            return action;
-        }
+        public override Abstract.IActions ActionParse(XmlNode xmlAction) =>
+            base.ActionParse(xmlAction, new Actions(), Constants.GetActionParams(), new Modification());
 
         public override Option OptionParse(XmlNode xmlOption) => OptionParseWithDo(xmlOption, new Modification());
 
