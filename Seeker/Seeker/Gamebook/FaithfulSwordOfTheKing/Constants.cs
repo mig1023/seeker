38750448193ede1e796fe1cbdb9654a3c86be407
mig1023 <mig1023@yandex.ru>
2021-09-03 21:09:11 +0300

    constants fix (sword)

--- Seeker/Seeker/Gamebook/FaithfulSwordOfTheKing/Constants.cs
+++ Seeker/Seeker/Gamebook/FaithfulSwordOfTheKing/Constants.cs
@@ -19,6 +19,7 @@ namespace Seeker.Gamebook.FaithfulSwordOfTheKing
         public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
         {
             [ColorTypes.StatusBar] = "#870808",
+            [ColorTypes.BookColor] = "#911",
         };
 
         public override List<int> GetParagraphsWithoutStatuses() => new List<int> { 0, 660 };
