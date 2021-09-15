e11b5aeab076aaf2931b3e19e37419a53cbfe92b
mig1023 <mig1023@yandex.ru>
2021-09-15 21:36:20 +0300

    round output fix (werewolf)

--- Seeker/Seeker/Gamebook/HowlOfTheWerewolf/Actions.cs
+++ Seeker/Seeker/Gamebook/HowlOfTheWerewolf/Actions.cs
@@ -821,7 +821,7 @@ namespace Seeker.Gamebook.HowlOfTheWerewolf
 
             while (true)
             {
-                fight.Add(String.Format("HEAD|Раунд: {0}", round));
+                fight.Add(String.Format("HEAD|BOLD|Раунд: {0}", round));
 
                 bool attackAlready = false;
                 int protagonistHitStrength = 0;
