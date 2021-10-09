43171ffd01b8cf3742a569f4346bf3a2457316e8
mig1023 <mig1023@yandex.ru>
2021-10-09 17:20:16 +0300

    debug link (altheus)

--- Seeker/Seeker/Gamebook/BloodfeudOfAltheus/Constants.cs
+++ Seeker/Seeker/Gamebook/BloodfeudOfAltheus/Constants.cs
@@ -48,6 +48,7 @@ namespace Seeker.Gamebook.BloodfeudOfAltheus
             Constants = StaticInstance,
             Save = Character.Protagonist.Save,
             Load = Character.Protagonist.Load,
+            Debug = Character.Protagonist.Debug,
         };
     }
 }
