5b2b4fc0f232a97691939216164926c435af00db
mig1023 <mig1023@yandex.ru>
2021-10-09 17:27:10 +0300

    debug link (tournament)

--- Seeker/Seeker/Gamebook/YounglingTournament/Constants.cs
+++ Seeker/Seeker/Gamebook/YounglingTournament/Constants.cs
@@ -49,6 +49,7 @@ namespace Seeker.Gamebook.YounglingTournament
             Constants = StaticInstance,
             Save = Character.Protagonist.Save,
             Load = Character.Protagonist.Load,
+            Debug = Character.Protagonist.Debug,
         };
     }
 }
