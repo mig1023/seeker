b511187e9851592c2781b9775376a5074fd874b3
mig1023 <mig1023@yandex.ru>
2021-10-25 17:17:50 +0300

    ModBySpecificName ltl refactoring

--- Seeker/Seeker/Gamebook/GoingToLaughter/Modification.cs
+++ Seeker/Seeker/Gamebook/GoingToLaughter/Modification.cs
@@ -14,15 +14,66 @@ namespace Seeker.Gamebook.GoingToLaughter
         {
             Character protagonist = Character.Protagonist;
 
-            if (name == "CrazyDance")
+            if ((name == "CrazyDance") && Advantage("Пение, Танец"))
             {
                 protagonist.Heroism += 6;
                 protagonist.Inspiration += 6;
+            }
+            else if (name == "RevoltInspiration")
+            {
+                string[] advantagesList = { "Красноречие", "Артистизм", "Верховая езда", "Предусмотрительность", "Сражение" };
+
+                foreach(string advantage in advantagesList)
+                    if (Advantage(advantage))
+                        protagonist.Heroism += 1;
+
+                if (Advantage("Популярность"))
+                    protagonist.Heroism += 10;
+
+                for(int i = 0; i < protagonist.Buffoonery; i++)
+                    protagonist.Heroism += 10;
+
+                for (int i = 0; i < protagonist.Inspiration; i++)
+                    protagonist.Heroism += 5;
+
+                for (int i = 0; i < (protagonist.Villainy / 6); i++)
+                    protagonist.Heroism -= 10;
+
+                if (Advantage("Жестокосердие"))
+                    protagonist.Heroism -= 10;
             }
             else
                 return false;
 
             return true;
         }
+
+        private bool Advantage(string advantages)
+        {
+            Character protagonist = Character.Protagonist;
+
+            if (advantages.Contains(","))
+            {
+                string[] advantagesList = advantages.Split(',');
+
+                foreach (string advantage in advantagesList)
+                    if (!protagonist.Advantages.Contains(advantage.Trim()))
+                        return false;
+
+                return true;
+            }
+            else if (advantages.Contains("|"))
+            {
+                string[] advantagesList = advantages.Split('|');
+
+                foreach (string advantage in advantagesList)
+                    if (protagonist.Advantages.Contains(advantage.Trim()))
+                        return true;
+
+                return false;
+            }
+            else
+                return protagonist.Advantages.Contains(advantages);
+        } 
     }
 }
