7173c907af6778eab165f9e7b4360465c0746026
mig1023 <mig1023@yandex.ru>
2021-10-25 17:16:08 +0300

    ModBySpecificName

--- Seeker/Seeker/Gamebook/GoingToLaughter/Modification.cs
+++ Seeker/Seeker/Gamebook/GoingToLaughter/Modification.cs
@@ -4,6 +4,25 @@ namespace Seeker.Gamebook.GoingToLaughter
 {
     class Modification : Prototypes.Modification, Abstract.IModification
     {
-        public override void Do() => base.Do(Character.Protagonist);
+        public override void Do()
+        {
+            if (!ModBySpecificName(Name))
+                base.Do(Character.Protagonist);
+        }
+
+        private bool ModBySpecificName(string name)
+        {
+            Character protagonist = Character.Protagonist;
+
+            if (name == "CrazyDance")
+            {
+                protagonist.Heroism += 6;
+                protagonist.Inspiration += 6;
+            }
+            else
+                return false;
+
+            return true;
+        }
     }
 }
