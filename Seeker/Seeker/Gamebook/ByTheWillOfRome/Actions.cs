2701e65e14ae306081f136470f306daf08ebf61b
mig1023 <mig1023@yandex.ru>
2021-11-04 21:10:08 +0300

    gameover + modifications

--- Seeker/Seeker/Gamebook/ByTheWillOfRome/Actions.cs
+++ Seeker/Seeker/Gamebook/ByTheWillOfRome/Actions.cs
@@ -13,5 +13,13 @@ namespace Seeker.Gamebook.ByTheWillOfRome
             String.Format("Сестерциев: {0}", protagonist.Sestertius),
             String.Format("Честь: {0}", protagonist.Honor),
         };
+
+        public override bool GameOver(out int toEndParagraph, out string toEndText)
+        {
+            toEndParagraph = 0;
+            toEndText = "Ущерб чести слишком велик, лучше броситься на меч, а игру начать сначала";
+
+            return protagonist.Honor <= 0;
+        }
     }
 }
