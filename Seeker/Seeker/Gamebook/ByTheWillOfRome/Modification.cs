bd986602f581b20c50490980355109a2bb12e422
mig1023 <mig1023@yandex.ru>
2021-11-15 17:56:42 +0300

    FishFood modification

--- Seeker/Seeker/Gamebook/ByTheWillOfRome/Modification.cs
+++ Seeker/Seeker/Gamebook/ByTheWillOfRome/Modification.cs
@@ -7,9 +7,22 @@ namespace Seeker.Gamebook.ByTheWillOfRome
         public override void Do()
         {
             if (Name == "RemoveTrigger")
+            {
                 Game.Option.Trigger(ValueString, remove: true);
+            }
+            else if (Name == "FishFood")
+            {
+                if (Character.Protagonist.Sestertius >= 100)
+                    Character.Protagonist.Sestertius -= 100;
+                else
+                    Game.Option.Trigger("Рабы", remove: true);
+
+                Game.Option.Trigger("Еда №2", remove: true);
+            }
             else
+            {
                 base.Do(Character.Protagonist);
+            }
         }
     }
 }
