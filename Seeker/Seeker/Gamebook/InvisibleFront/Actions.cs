6b28c9a433045b171a3f220f25789d7e1b2df4c4
mig1023 <mig1023@yandex.ru>
2021-11-23 22:08:47 +0300

    empty option fuse (front)

--- Seeker/Seeker/Gamebook/InvisibleFront/Actions.cs
+++ Seeker/Seeker/Gamebook/InvisibleFront/Actions.cs
@@ -15,33 +15,48 @@ namespace Seeker.Gamebook.InvisibleFront
 
         public override bool CheckOnlyIf(string option)
         {
-            string[] options = option.Split('|', ',');
-
-            foreach (string oneOption in options)
+            if (String.IsNullOrEmpty(option))
             {
-                if (oneOption.Contains(">") || oneOption.Contains("<"))
-                {
-                    int level = Game.Other.LevelParse(oneOption);
-
-                    if (oneOption.Contains("НЕДОВОЛЬСТВО >") && (level >= protagonist.Dissatisfaction))
-                        return false;
-                    else if (oneOption.Contains("НЕДОВОЛЬСТВО <=") && (level < protagonist.Dissatisfaction))
-                        return false;
-                    else if (oneOption.Contains("ВЕРБОВКА >") && (level >= protagonist.Recruitment))
-                        return false;
-                    else if (oneOption.Contains("ВЕРБОВКА <=") && (level < protagonist.Recruitment))
-                        return false;
-                }
-                else if (oneOption.Contains("!"))
-                {
-                    if (Game.Option.IsTriggered(oneOption.Replace("!", String.Empty).Trim()))
-                        return false;
-                }
-                else if (!Game.Option.IsTriggered(oneOption.Trim()))
-                    return false;
+                return true;
             }
+            else
+            {
+                string[] options = option.Split('|', ',');
 
-            return true;
+                foreach (string oneOption in options)
+                {
+                    if (oneOption.Contains(">") || oneOption.Contains("<"))
+                    {
+                        int level = Game.Other.LevelParse(oneOption);
+
+                        if (oneOption.Contains("НЕДОВОЛЬСТВО >") && (level >= protagonist.Dissatisfaction))
+                        {
+                            return false;
+                        }
+                        else if (oneOption.Contains("НЕДОВОЛЬСТВО <=") && (level < protagonist.Dissatisfaction))
+                        {
+                            return false;
+                        }
+                        else if (oneOption.Contains("ВЕРБОВКА >") && (level >= protagonist.Recruitment))
+                        {
+                            return false;
+                        }
+                        else if (oneOption.Contains("ВЕРБОВКА <=") && (level < protagonist.Recruitment))
+                        {
+                            return false;
+                        }
+                    }
+                    else if (oneOption.Contains("!"))
+                    {
+                        if (Game.Option.IsTriggered(oneOption.Replace("!", String.Empty).Trim()))
+                            return false;
+                    }
+                    else if (!Game.Option.IsTriggered(oneOption.Trim()))
+                        return false;
+                }
+
+                return true;
+            }
         }
     }
 }
