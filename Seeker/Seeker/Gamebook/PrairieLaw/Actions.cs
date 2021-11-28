27e1f887221e99d6638bdc9309814ca682a225bb
mig1023 <mig1023@yandex.ru>
2021-11-28 17:37:54 +0300

    max strength output fix (prairie)

--- Seeker/Seeker/Gamebook/PrairieLaw/Actions.cs
+++ Seeker/Seeker/Gamebook/PrairieLaw/Actions.cs
@@ -24,7 +24,7 @@ namespace Seeker.Gamebook.PrairieLaw
         public override List<string> Status() => new List<string>
         {
             String.Format("Ловкость: {0}", protagonist.Skill),
-            String.Format("Сила: {0}", protagonist.Strength),
+            String.Format("Сила: {0}/{1}", protagonist.Strength, protagonist.MaxStrength),
             String.Format("Обаяние: {0}", protagonist.Charm),
         };
 
