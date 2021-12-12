6af2febadd37924621c8d63b520f7b2a80b0b370
mig1023 <mig1023@yandex.ru>
2021-12-12 17:50:00 +0300

    representer + another methods

--- Seeker/Seeker/Gamebook/OrcsDay/Actions.cs
+++ Seeker/Seeker/Gamebook/OrcsDay/Actions.cs
@@ -8,6 +8,8 @@ namespace Seeker.Gamebook.OrcsDay
         public static Actions StaticInstance = new Actions();
         private static Character protagonist = Character.Protagonist;
 
+        public string Stat { get; set; }      
+
         public override List<string> Status() => new List<string>
         {
             String.Format("Оркишность: {0}", protagonist.Orcishness),
@@ -22,5 +24,53 @@ namespace Seeker.Gamebook.OrcsDay
             String.Format("Мозги: {0}", protagonist.Wits),
             String.Format("Мышцы: {0}", protagonist.Muscle),
         };
+
+        public override List<string> Representer()
+        {
+            List<string> enemies = new List<string>();
+
+            if ((Name == "Get") && !String.IsNullOrEmpty(Stat) && !Decrement)
+            {
+                return new List<string> { String.Format("{0}\n(текущее значение: {1})",
+                    Text, Game.Other.NegativeMeaning(GetProperty(protagonist, Stat))) };
+            }
+
+            return enemies;
+        }
+
+        public override bool IsButtonEnabled(bool secondButton = false) =>
+            String.IsNullOrEmpty(Stat) || (protagonist.StatBonuses > 0) || secondButton;
+
+        public List<string> Get()
+        {
+            if (!String.IsNullOrEmpty(Stat))
+            {
+                int currentStat = GetProperty(protagonist, Stat);
+
+                currentStat += (Decrement ? -1 : 1);
+
+                protagonist.GetType().GetProperty(Stat).SetValue(protagonist, currentStat);
+
+                protagonist.StatBonuses += (Decrement ? 1 : -1);
+            }
+
+            return new List<string> { "RELOAD" };
+        }
+
+        public List<string> Decrease()
+        {
+            if (!String.IsNullOrEmpty(Stat))
+            {
+                int currentStat = GetProperty(protagonist, Stat);
+
+                currentStat -= 1;
+
+                protagonist.GetType().GetProperty(Stat).SetValue(protagonist, currentStat);
+
+                protagonist.StatBonuses += 1;
+            }
+
+            return new List<string> { "RELOAD" };
+        }
     }
 }
