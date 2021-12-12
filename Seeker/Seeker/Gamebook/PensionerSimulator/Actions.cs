981ea5a64692688aece528b066bb8074a1c55ad8
mig1023 <mig1023@yandex.ru>
2021-12-12 17:50:28 +0300

    micro refactoring (pensioner)

--- Seeker/Seeker/Gamebook/PensionerSimulator/Actions.cs
+++ Seeker/Seeker/Gamebook/PensionerSimulator/Actions.cs
@@ -17,13 +17,10 @@ namespace Seeker.Gamebook.PensionerSimulator
                 foreach (string oneOption in option.Split(','))
                 {
                     if (oneOption.Contains("!") && (Game.Option.IsTriggered(oneOption.Replace("!", String.Empty).Trim())))
-                    {
                         return false;
-                    }
+
                     else if (!oneOption.Contains("!") && (!Game.Option.IsTriggered(oneOption.Trim())))
-                    {
                         return false;
-                    }
                 }
 
                 return true;
