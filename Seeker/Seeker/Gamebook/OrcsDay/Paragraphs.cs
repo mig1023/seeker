fa7f0a79309a9ea95d91d9d4199a75ab1cbd8a9b
mig1023 <mig1023@yandex.ru>
2021-12-18 18:17:06 +0300

    paragraph with add.option

--- Seeker/Seeker/Gamebook/OrcsDay/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/OrcsDay/Paragraphs.cs
@@ -8,7 +8,24 @@ namespace Seeker.Gamebook.OrcsDay
     {
         public static Paragraphs StaticInstance = new Paragraphs();
 
-        public override Paragraph Get(int id, XmlNode xmlParagraph) => base.Get(xmlParagraph);
+        public override Paragraph Get(int id, XmlNode xmlParagraph)
+        {
+            Game.Paragraph paragraph = ParagraphTemplate(xmlParagraph);
+
+            foreach (XmlNode xmlOption in xmlParagraph.SelectNodes("Options/Option"))
+                paragraph.Options.Add(OptionParse(xmlOption));
+
+            if (Game.Option.IsTriggered("Имя") && (Character.Protagonist.Orcishness <= 0))
+                paragraph.Options.Add(GetOption(destination: 33, text: "Получить имя", onlyIf: "selectOnly"));
+
+            foreach (XmlNode xmlAction in xmlParagraph.SelectNodes("Actions/Action"))
+                paragraph.Actions.Add(ActionParse(xmlAction));
+
+            foreach (XmlNode xmlModification in xmlParagraph.SelectNodes("Modifications/Modification"))
+                paragraph.Modification.Add(ModificationParse(xmlModification));
+
+            return paragraph;
+        }
 
         public override Abstract.IActions ActionParse(XmlNode xmlAction)
         {
@@ -40,5 +57,12 @@ namespace Seeker.Gamebook.OrcsDay
 
             return enemy;
         }
+
+        private static Option GetOption(int destination, string text, string onlyIf) => new Option
+        {
+            Destination = destination,
+            Text = text,
+            OnlyIf = onlyIf,
+        };
     }
 }
