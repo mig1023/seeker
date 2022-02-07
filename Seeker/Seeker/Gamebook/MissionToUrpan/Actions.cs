8ff7183b1298a835006909b02170c7ee7e49e746
mig1023 <mig1023@yandex.ru>
2022-02-07 17:36:04 +0300

    character params (mission)

--- Seeker/Seeker/Gamebook/MissionToUrpan/Actions.cs
+++ Seeker/Seeker/Gamebook/MissionToUrpan/Actions.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Seeker.Gamebook.MissionToUrpan
 {
     class Actions : Prototypes.Actions, Abstract.IActions
     {
         public static Actions StaticInstance = new Actions();
+
+        public override List<string> Status() =>
+            new List<string> { String.Format("Репутация: {0}", Character.Protagonist.Reputation) };
     }
 }
--- Seeker/Seeker/Gamebook/MissionToUrpan/Character.cs
+++ Seeker/Seeker/Gamebook/MissionToUrpan/Character.cs
@@ -5,5 +5,29 @@ namespace Seeker.Gamebook.MissionToUrpan
     class Character : Prototypes.Character, Abstract.ICharacter
     {
         public static Character Protagonist = new Character();
+
+        public int Reputation { get; set; }
+
+        public override void Init()
+        {
+            base.Init();
+
+            Reputation = 3;
+        }
+
+        public Character Clone() => new Character()
+        {
+            IsProtagonist = this.IsProtagonist,
+            Name = this.Name,
+            Reputation = this.Reputation,
+        };
+
+        public override string Save() => Reputation.ToString();
+
+        public override void Load(string saveLine)
+        {
+            Reputation = int.Parse(saveLine);
+            IsProtagonist = true;
+        }
     }
 }
