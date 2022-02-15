d819f17523eccf5601ef467fd5f97a5f25255c90
mig1023 <mig1023@yandex.ru>
2022-02-15 18:13:24 +0300

    onlyIf's

--- Seeker/Seeker/Gamebook/Sheriff/Actions.cs
+++ Seeker/Seeker/Gamebook/Sheriff/Actions.cs
@@ -6,5 +6,29 @@ namespace Seeker.Gamebook.Sheriff
     {
         public static Actions StaticInstance = new Actions();
         private static Character protagonist = Character.Protagonist;
+
+        public override bool CheckOnlyIf(string option)
+        {
+            if (String.IsNullOrEmpty(option))
+            {
+                return true;
+            }
+            else if (option.Contains(">") || option.Contains("<"))
+            {
+                int level = Game.Services.LevelParse(option);
+
+                if (option.Contains("ВЖУХ >=") && (level > protagonist.Whoosh))
+                    return false;
+
+                if (option.Contains("ВЖУХ <") && (level <= protagonist.Whoosh))
+                    return false;
+
+                return true;
+            }
+            else
+            {
+                return CheckOnlyIfTrigger(option);
+            }
+        }
     }
 }
