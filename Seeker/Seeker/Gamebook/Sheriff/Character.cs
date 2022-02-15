1f6af752a666b5dd37683d06305cb6e2091fd136
mig1023 <mig1023@yandex.ru>
2022-02-15 18:12:55 +0300

    character params

--- Seeker/Seeker/Gamebook/Sheriff/Character.cs
+++ Seeker/Seeker/Gamebook/Sheriff/Character.cs
@@ -5,5 +5,34 @@ namespace Seeker.Gamebook.Sheriff
     class Character : Prototypes.Character, Abstract.ICharacter
     {
         public static Character Protagonist = new Character();
+
+        private int _whoosh;
+        public int Whoosh
+        {
+            get => _whoosh;
+            set => _whoosh = Game.Param.Setter(value);
+        }
+
+        public override void Init()
+        {
+            base.Init();
+
+            Whoosh = 0;
+        }
+
+        public Character Clone() => new Character()
+        {
+            IsProtagonist = this.IsProtagonist,
+            Name = this.Name,
+            Whoosh = this.Whoosh,
+        };
+
+        public override string Save() => Whoosh.ToString();
+
+        public override void Load(string saveLine)
+        {
+            Whoosh = int.Parse(saveLine);
+            IsProtagonist = true;
+        }
     }
 }
--- Seeker/Seeker/Gamebook/Sheriff/Modification.cs
+++ Seeker/Seeker/Gamebook/Sheriff/Modification.cs
@@ -4,6 +4,9 @@ namespace Seeker.Gamebook.Sheriff
 {
     class Modification : Prototypes.Modification, Abstract.IModification
     {
-        public override void Do() => base.Do(Character.Protagonist);
+        public override void Do()
+        {
+            base.Do(Character.Protagonist);
+        }
     }
 }
