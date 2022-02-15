a2373c32844a99512c0a8a2f2f1983d3196fac40
mig1023 <mig1023@yandex.ru>
2022-02-15 18:27:32 +0300

    onlyIf's fixes + triggers fix

--- Seeker/Seeker/Gamebook/Sheriff/Actions.cs
+++ Seeker/Seeker/Gamebook/Sheriff/Actions.cs
@@ -7,28 +7,71 @@ namespace Seeker.Gamebook.Sheriff
         public static Actions StaticInstance = new Actions();
         private static Character protagonist = Character.Protagonist;
 
+        //public override bool CheckOnlyIf(string option)
+        //{
+        //    if (String.IsNullOrEmpty(option))
+        //    {
+        //        return true;
+        //    }
+        //    else if (option.Contains(">") || option.Contains("<"))
+        //    {
+        //        int level = Game.Services.LevelParse(option);
+
+        //        if (option.Contains("ВЖУХ >=") && (level > protagonist.Whoosh))
+        //            return false;
+
+        //        if (option.Contains("ВЖУХ <") && (level <= protagonist.Whoosh))
+        //            return false;
+
+        //        return true;
+        //    }
+        //    else if (option.Contains(","))
+        //    {
+        //        foreach (string opt in option.Split(','))
+        //        {
+        //            if (!Game.Option.IsTriggered(opt.Trim()))
+        //                return false;
+        //        }
+
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return CheckOnlyIfTrigger(option);
+        //    }
+        //}
+
         public override bool CheckOnlyIf(string option)
         {
             if (String.IsNullOrEmpty(option))
-            {
                 return true;
-            }
-            else if (option.Contains(">") || option.Contains("<"))
+
+            foreach (string oneOption in option.Split(','))
             {
-                int level = Game.Services.LevelParse(option);
+                if (oneOption.Contains(">") || oneOption.Contains("<"))
+                {
+                    int level = Game.Services.LevelParse(oneOption);
 
-                if (option.Contains("ВЖУХ >=") && (level > protagonist.Whoosh))
+                    if (oneOption.Contains("ВЖУХ >=") && (level > protagonist.Whoosh))
+                        return false;
+
+                    if (oneOption.Contains("ВЖУХ <") && (level <= protagonist.Whoosh))
+                        return false;
+
+                    return true;
+                }
+                else if (oneOption.Contains("!"))
+                {
+                    if (Game.Option.IsTriggered(oneOption.Replace("!", String.Empty).Trim()))
+                        return false;
+                }
+                else if (!Game.Option.IsTriggered(oneOption.Trim()))
+                {
                     return false;
-
-                if (option.Contains("ВЖУХ <") && (level <= protagonist.Whoosh))
-                    return false;
-
-                return true;
+                }
             }
-            else
-            {
-                return CheckOnlyIfTrigger(option);
-            }
+
+            return true;
         }
     }
 }
