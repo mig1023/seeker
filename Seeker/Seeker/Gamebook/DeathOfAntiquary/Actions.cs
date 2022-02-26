db303b691f484c809ce4b75cadcff9e4f59b426a
mig1023 <mig1023@yandex.ru>
2022-02-26 18:07:40 +0300

    xml pracing fix (antiquary)

--- Seeker/Seeker/Gamebook/DeathOfAntiquary/Actions.cs
+++ Seeker/Seeker/Gamebook/DeathOfAntiquary/Actions.cs
@@ -5,7 +5,6 @@ namespace Seeker.Gamebook.DeathOfAntiquary
     class Actions : Prototypes.Actions, Abstract.IActions
     {
         public static Actions StaticInstance = new Actions();
-        private static Character protagonist = Character.Protagonist;
 
         public override bool CheckOnlyIf(string option) => CheckOnlyIfTrigger(option);
     }
--- Seeker/Seeker/Gamebook/DeathOfAntiquary/Character.cs
+++ Seeker/Seeker/Gamebook/DeathOfAntiquary/Character.cs
@@ -5,18 +5,5 @@ namespace Seeker.Gamebook.DeathOfAntiquary
     class Character : Prototypes.Character, Abstract.ICharacter
     {
         public static Character Protagonist = new Character();
-
-        public Character Clone() => new Character()
-        {
-            IsProtagonist = this.IsProtagonist,
-            Name = this.Name,
-        };
-
-        public override string Save() => String.Empty;
-
-        public override void Load(string saveLine)
-        {
-            IsProtagonist = true;
-        }
     }
 }
