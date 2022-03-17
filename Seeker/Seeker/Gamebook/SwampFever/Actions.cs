a04e06703222c15c988faa5842127a56e9301d6f
mig1023 <mig1023@yandex.ru>
2022-03-17 18:11:34 +0300

    button text (swamp fever)

--- Seeker/Seeker/Gamebook/SwampFever/Actions.cs
+++ Seeker/Seeker/Gamebook/SwampFever/Actions.cs
@@ -45,6 +45,21 @@ namespace Seeker.Gamebook.SwampFever
             String.Format("Ярость: {0}", Constants.GetFuryLevel()[protagonist.Fury]),
         };
 
+        public override string ButtonText()
+        {
+            switch (Name)
+            {
+                case "Fight":
+                    return "Сражаться";
+
+                case "MentalTest":
+                    return "Проверка";
+
+                default:
+                    return Button;
+            }
+        }
+
         public override bool GameOver(out int toEndParagraph, out string toEndText) =>
             GameOverBy(protagonist.HarversterDestroyed, out toEndParagraph, out toEndText);
 
