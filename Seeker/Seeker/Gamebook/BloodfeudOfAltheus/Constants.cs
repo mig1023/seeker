0c9aa30d4bb01ac6992f57770e20b13f92c6d568
mig1023 <mig1023@yandex.ru>
2022-03-21 18:31:02 +0300

    constants by xml (altheus)

--- Seeker/Seeker/Gamebook/BloodfeudOfAltheus/Constants.cs
+++ Seeker/Seeker/Gamebook/BloodfeudOfAltheus/Constants.cs
@@ -7,14 +7,6 @@ namespace Seeker.Gamebook.BloodfeudOfAltheus
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
         public static Constants StaticInstance = new Constants();
-        public override Dictionary<ButtonTypes, string> ButtonsColors() => new Dictionary<ButtonTypes, string>
-        {
-            [ButtonTypes.Main] = "#ebd5b3",
-            [ButtonTypes.Action] = "#ebd5b3",
-            [ButtonTypes.Option] = "#f9f2e8",
-            [ButtonTypes.Continue] = "#f5ead9",
-            [ButtonTypes.Font] = "#000000",
-        };
 
         public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
         {
