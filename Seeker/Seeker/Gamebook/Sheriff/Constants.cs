2d577e82e0d576fbf27443464522b2248dca81a0
mig1023 <mig1023@yandex.ru>
2022-03-22 21:34:56 +0300

    constants by xml (sheriff)

--- Seeker/Seeker/Gamebook/Sheriff/Constants.cs
+++ Seeker/Seeker/Gamebook/Sheriff/Constants.cs
@@ -8,14 +8,6 @@ namespace Seeker.Gamebook.Sheriff
     {
         public static Constants StaticInstance = new Constants();
 
-        public override Dictionary<ButtonTypes, string> ButtonsColors() => new Dictionary<ButtonTypes, string>
-        {
-            [ButtonTypes.Main] = "#7d2c1c",
-            [ButtonTypes.Option] = "#b45b27",
-            [ButtonTypes.Continue] = "#b45b27",
-            [ButtonTypes.System] = "#f0d1b9",
-        };
-
         public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
         {
             [ColorTypes.BookColor] = "#7d2c1c",
