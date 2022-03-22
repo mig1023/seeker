4fb81805078be5a4d40d29188b3bd3c91bee4c8f
mig1023 <mig1023@yandex.ru>
2022-03-22 21:35:20 +0300

    constants by xml (school)

--- Seeker/Seeker/Gamebook/SilentSchool/Constants.cs
+++ Seeker/Seeker/Gamebook/SilentSchool/Constants.cs
@@ -8,14 +8,6 @@ namespace Seeker.Gamebook.SilentSchool
     {
         public static Constants StaticInstance = new Constants();
 
-        public override Dictionary<ButtonTypes, string> ButtonsColors() => new Dictionary<ButtonTypes, string>
-        {
-            [ButtonTypes.Main] = "#434343",
-            [ButtonTypes.Action] = "#555555",
-            [ButtonTypes.Option] = "#686868",
-            [ButtonTypes.System] = "#eeeeee",
-        };
-
         public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
         {
             [ColorTypes.StatusBar] = "#2a2a2a",
