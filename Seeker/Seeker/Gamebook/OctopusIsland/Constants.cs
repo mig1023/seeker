153de567a88fafda217fe9df8f39e608e81578d8
mig1023 <mig1023@yandex.ru>
2022-03-22 21:33:35 +0300

    constants by xml (octopus)

--- Seeker/Seeker/Gamebook/OctopusIsland/Constants.cs
+++ Seeker/Seeker/Gamebook/OctopusIsland/Constants.cs
@@ -8,14 +8,6 @@ namespace Seeker.Gamebook.OctopusIsland
     {
         public static Constants StaticInstance = new Constants();
 
-        public override Dictionary<ButtonTypes, string> ButtonsColors() => new Dictionary<ButtonTypes, string>
-        {
-            [ButtonTypes.Main] = "#c93c20",
-            [ButtonTypes.Action] = "#c93c20",
-            [ButtonTypes.Option] = "#c93c20",
-            [ButtonTypes.System] = "#eeeeee",
-        };
-
         public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
         {
             [ColorTypes.StatusBar] = "#ce4f36",
