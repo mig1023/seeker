64389d055b2e0ddb6caefbad58bcc68782db7a47
mig1023 <mig1023@yandex.ru>
2022-03-23 18:16:11 +0300

    constants fixes (mission)

--- Seeker/Seeker/Gamebook/MissionToUrpan/Constants.cs
+++ Seeker/Seeker/Gamebook/MissionToUrpan/Constants.cs
@@ -1,17 +1,10 @@
 ﻿using System.Collections.Generic;
-using static Seeker.Output.Buttons;
-using static Seeker.Game.Data;
 
 namespace Seeker.Gamebook.MissionToUrpan
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
         public static Constants StaticInstance = new Constants();
-
-        public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
-        {
-            [ColorTypes.StatusBar] = "#0d1a36",
-        };
 
         public static Dictionary<string, int> ResultCalculation() => new Dictionary<string, int>
         {
