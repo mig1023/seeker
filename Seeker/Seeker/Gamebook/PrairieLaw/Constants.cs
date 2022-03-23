ec110567547c1faa307f4dcb5e85fc4b266bf32d
mig1023 <mig1023@yandex.ru>
2022-03-23 18:17:12 +0300

    constants fixes (prairie)

--- Seeker/Seeker/Gamebook/PrairieLaw/Constants.cs
+++ Seeker/Seeker/Gamebook/PrairieLaw/Constants.cs
@@ -1,18 +1,10 @@
 ﻿using System.Collections.Generic;
-using static Seeker.Output.Buttons;
-using static Seeker.Game.Data;
 
 namespace Seeker.Gamebook.PrairieLaw
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
         public static Constants StaticInstance = new Constants();
-
-        public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
-        {
-            [ColorTypes.StatusBar] = "#b85d43",
-            [ColorTypes.AdditionalStatus] = "#d69b8b",
-        };
 
         public static Dictionary<int, int> Skills() => new Dictionary<int, int>
         {
