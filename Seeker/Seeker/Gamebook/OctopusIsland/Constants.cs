bdcb36db0b7cbb66b53d1d4035f7f2d6cfbb3940
mig1023 <mig1023@yandex.ru>
2022-03-23 18:16:48 +0300

    constants fixes (octopus)

--- Seeker/Seeker/Gamebook/OctopusIsland/Constants.cs
+++ Seeker/Seeker/Gamebook/OctopusIsland/Constants.cs
@@ -1,17 +1,10 @@
 ﻿using System.Collections.Generic;
-using static Seeker.Output.Buttons;
-using static Seeker.Game.Data;
 
 namespace Seeker.Gamebook.OctopusIsland
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
         public static Constants StaticInstance = new Constants();
-
-        public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
-        {
-            [ColorTypes.StatusBar] = "#ce4f36",
-        };
 
         public static Links GetLinks() => new Links
         {
