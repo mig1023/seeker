065325b95f00d93dd40f0817af8a2eaba1bdaee9
mig1023 <mig1023@yandex.ru>
2022-03-23 18:11:29 +0300

    constants fixes (heart)

--- Seeker/Seeker/Gamebook/HeartOfIce/Constants.cs
+++ Seeker/Seeker/Gamebook/HeartOfIce/Constants.cs
@@ -1,21 +1,10 @@
 ﻿using System.Collections.Generic;
-using static Seeker.Output.Buttons;
-using static Seeker.Game.Data;
 
 namespace Seeker.Gamebook.HeartOfIce
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
         public static Constants StaticInstance = new Constants();
-
-        public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
-        {
-            [ColorTypes.Background] = "#0787c4",
-            [ColorTypes.Font] = "#d3f0fd",
-            [ColorTypes.SystemFont] = "#d3f0fd",
-            [ColorTypes.ActionBox] = "#389fcf",
-            [ColorTypes.StatusBar] = "#5c7ca5",
-        };
 
         public override List<int> GetParagraphsWithoutStatuses() => new List<int> { 0, 454, 455, 456, 457 };
 
