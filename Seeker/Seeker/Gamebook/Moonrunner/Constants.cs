c3f0787990413fe40e027d72ba87388d806885b1
mig1023 <mig1023@yandex.ru>
2022-03-23 18:16:37 +0300

    constants fixes (moonrunner)

--- Seeker/Seeker/Gamebook/Moonrunner/Constants.cs
+++ Seeker/Seeker/Gamebook/Moonrunner/Constants.cs
@@ -1,18 +1,10 @@
 ﻿using System.Collections.Generic;
-using static Seeker.Output.Buttons;
-using static Seeker.Game.Data;
 
 namespace Seeker.Gamebook.Moonrunner
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
         public static Constants StaticInstance = new Constants();
-
-        public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
-        {
-            [ColorTypes.StatusBar] = "#44264b",
-            [ColorTypes.ActionBox] = "#e3d9d9",
-        };
 
         public static Dictionary<int, string> SpellsList() => new Dictionary<int, string>
         {
