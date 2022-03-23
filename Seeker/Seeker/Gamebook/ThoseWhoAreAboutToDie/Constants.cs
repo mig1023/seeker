d7d6624b9ceb29e87eb3f4010ea0efb9b98108af
mig1023 <mig1023@yandex.ru>
2022-03-23 18:18:22 +0300

    constants fixes (those who are about)

--- Seeker/Seeker/Gamebook/ThoseWhoAreAboutToDie/Constants.cs
+++ Seeker/Seeker/Gamebook/ThoseWhoAreAboutToDie/Constants.cs
@@ -1,18 +1,10 @@
 ﻿using System.Collections.Generic;
-using static Seeker.Output.Buttons;
-using static Seeker.Game.Data;
 
 namespace Seeker.Gamebook.ThoseWhoAreAboutToDie
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
         public static Constants StaticInstance = new Constants();
-
-        public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
-        {
-            [ColorTypes.StatusBar] = "#fce391",
-            [ColorTypes.StatusFont] = "#000000",
-        };
 
         public override bool ShowDisabledOption() => true;
 
