00d7bcba19b8f88bbf873eb0055ab029771ec7fc
mig1023 <mig1023@yandex.ru>
2022-03-23 18:12:17 +0300

    constants fixes (front)

--- Seeker/Seeker/Gamebook/InvisibleFront/Constants.cs
+++ Seeker/Seeker/Gamebook/InvisibleFront/Constants.cs
@@ -1,20 +1,10 @@
 ﻿using System.Collections.Generic;
-using static Seeker.Output.Buttons;
-using static Seeker.Game.Data;
 
 namespace Seeker.Gamebook.InvisibleFront
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
         public static Constants StaticInstance = new Constants();
-
-        public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
-        {
-            [ColorTypes.Background] = "#ffdadb",
-            [ColorTypes.StatusBar] = "#aa2218",
-            [ColorTypes.StatusFont] = "#eede49",
-            [ColorTypes.SystemFont] = "#f7f1b6",
-        };
 
         public static List<string> GetApartments() => new List<string>
         {
