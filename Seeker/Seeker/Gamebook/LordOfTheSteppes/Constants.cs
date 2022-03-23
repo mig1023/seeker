bae8562ed24994c2e76809c6bf98dfdce9aeef0a
mig1023 <mig1023@yandex.ru>
2022-03-23 18:12:49 +0300

    constants fixes (lord)

--- Seeker/Seeker/Gamebook/LordOfTheSteppes/Constants.cs
+++ Seeker/Seeker/Gamebook/LordOfTheSteppes/Constants.cs
@@ -1,18 +1,10 @@
 ﻿using System.Collections.Generic;
-using static Seeker.Output.Buttons;
-using static Seeker.Game.Data;
 
 namespace Seeker.Gamebook.LordOfTheSteppes
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
         public static Constants StaticInstance = new Constants();
-
-        public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
-        {
-            [ColorTypes.StatusBar] = "#b42806",
-            [ColorTypes.StatusFont] = "#ffffff",
-        };
 
         public override Output.Interface.TextFontSize GetFontSize() => Output.Interface.TextFontSize.little;
 
