17df0ec702943d7dcc19b0c80a92bf2f5046978f
mig1023 <mig1023@yandex.ru>
2022-03-23 18:17:47 +0300

    constants fixes (school)

--- Seeker/Seeker/Gamebook/SilentSchool/Constants.cs
+++ Seeker/Seeker/Gamebook/SilentSchool/Constants.cs
@@ -1,17 +1,10 @@
 ﻿using System.Collections.Generic;
-using static Seeker.Output.Buttons;
-using static Seeker.Game.Data;
 
 namespace Seeker.Gamebook.SilentSchool
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
         public static Constants StaticInstance = new Constants();
-
-        public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
-        {
-            [ColorTypes.StatusBar] = "#2a2a2a",
-        };
 
         public static Links GetLinks() => new Links
         {
