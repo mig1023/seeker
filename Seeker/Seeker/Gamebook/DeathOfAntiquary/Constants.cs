da258e282e2354a6291ca53d42a632316eca6799
mig1023 <mig1023@yandex.ru>
2022-03-23 17:58:40 +0300

    book color fix (antiquary)

--- Seeker/Seeker/Gamebook/DeathOfAntiquary/Constants.cs
+++ Seeker/Seeker/Gamebook/DeathOfAntiquary/Constants.cs
@@ -8,11 +8,6 @@ namespace Seeker.Gamebook.DeathOfAntiquary
     {
         public static Constants StaticInstance = new Constants();
 
-        public override Dictionary<ColorTypes, string> Colors() => new Dictionary<ColorTypes, string>
-        {
-            [ColorTypes.BookColor] = "#2e2e2e",
-        };
-
         public static Links GetLinks() => new Links
         {
             Protagonist = Character.Protagonist.Init,
