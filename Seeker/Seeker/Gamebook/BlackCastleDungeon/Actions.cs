d504a5ebad6f28992ecd9be46200304e714d9df1
mig1023 <mig1023@yandex.ru>
2022-04-01 18:54:58 +0300

    button text action -> constants (black castle)

--- Seeker/Seeker/Gamebook/BlackCastleDungeon/Actions.cs
+++ Seeker/Seeker/Gamebook/BlackCastleDungeon/Actions.cs
@@ -157,21 +157,6 @@ namespace Seeker.Gamebook.BlackCastleDungeon
             return enemies;
         }
 
-        public override string ButtonText()
-        {
-            switch (Name)
-            {
-                case "Fight":
-                    return "Сражаться";
-
-                case "Luck":
-                    return "Проверить удачу";
-
-                default:
-                    return Button;
-            }
-        }
-
         public List<string> Luck()
         {
             Game.Dice.DoubleRoll(out int firstDice, out int secondDice);
