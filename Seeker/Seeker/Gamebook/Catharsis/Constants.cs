72923b51fa0d744c4de63e4bf05c77aff85a814e
mig1023 <mig1023@yandex.ru>
2022-04-08 16:37:15 +0300

    disabled option fix (catharsis)

--- Seeker/Seeker/Gamebook/Catharsis/Constants.cs
+++ Seeker/Seeker/Gamebook/Catharsis/Constants.cs
@@ -13,8 +13,6 @@ namespace Seeker.Gamebook.Catharsis
             ["Stealth"] = 3,
         };
 
-        public override bool ShowDisabledOption() => true;
-
         public static Links GetLinks() => new Links
         {
             Protagonist = Character.Protagonist.Init,
