4cafc95cf49cfb35314a4c91ed3a7eafc40bbbaf
mig1023 <mig1023@yandex.ru>
2022-04-10 15:36:47 +0300

    button text fix (aldar)

--- Seeker/Seeker/Gamebook/AdventuresOfABeardlessDeceiver/Constants.cs
+++ Seeker/Seeker/Gamebook/AdventuresOfABeardlessDeceiver/Constants.cs
@@ -16,11 +16,6 @@ namespace Seeker.Gamebook.AdventuresOfABeardlessDeceiver
             ["Popularity"] = "популярности",
         };
 
-        public override Dictionary<string, string> ButtonText() => new Dictionary<string, string>
-        {
-            ["Test"] = "Проверить",
-        };
-
         public static Links GetLinks() => new Links
         {
             Protagonist = Character.Protagonist.Init,
