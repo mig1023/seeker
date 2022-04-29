1f1e2ac0c48fd74f4dbcf760d5a208825f994e3e
mig1023 <mig1023@yandex.ru>
2022-04-29 17:41:14 +0300

    option nodes fix (pensioner)

--- Seeker/Seeker/Gamebook/PensionerSimulator/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/PensionerSimulator/Paragraphs.cs
@@ -11,7 +11,7 @@ namespace Seeker.Gamebook.PensionerSimulator
         {
             Paragraph paragraph = ParagraphTemplate(xmlParagraph);
 
-            foreach (XmlNode xmlOption in xmlParagraph.SelectNodes("Options/Option"))
+            foreach (XmlNode xmlOption in xmlParagraph.SelectNodes("Options/*"))
                 paragraph.Options.Add(OptionParseWithDo(xmlOption, new Modification()));
 
             return paragraph;
