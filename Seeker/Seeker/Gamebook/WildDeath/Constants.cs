6f1dea6ebde19325a9c611f64b888ff03bf8a0f4
mig1023 <mig1023@yandex.ru>
2022-05-03 18:40:53 +0300

    availability (wild death)

--- Seeker/Seeker/Gamebook/WildDeath/Constants.cs
+++ Seeker/Seeker/Gamebook/WildDeath/Constants.cs
@@ -11,7 +11,7 @@ namespace Seeker.Gamebook.WildDeath
         public static Links GetLinks() => new Links
         {
             Protagonist = Character.Protagonist.Init,
-            CheckOnlyIf = Actions.StaticInstance.CheckOnlyIf,
+            Availability = Actions.StaticInstance.Availability,
             Paragraphs = Paragraphs.StaticInstance,
             Actions = Actions.StaticInstance,
             Constants = StaticInstance,
