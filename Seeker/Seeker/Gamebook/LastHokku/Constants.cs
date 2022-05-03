8f8f1826ead9154cf7556f202962d0cae53e1331
mig1023 <mig1023@yandex.ru>
2022-05-03 18:35:50 +0300

    availability (hokku)

--- Seeker/Seeker/Gamebook/LastHokku/Constants.cs
+++ Seeker/Seeker/Gamebook/LastHokku/Constants.cs
@@ -13,7 +13,7 @@ namespace Seeker.Gamebook.LastHokku
         public static Links GetLinks() => new Links
         {
             Protagonist = Character.Protagonist.Init,
-            CheckOnlyIf = Actions.StaticInstance.CheckOnlyIf,
+            Availability = Actions.StaticInstance.Availability,
             Paragraphs = Paragraphs.StaticInstance,
             Actions = Actions.StaticInstance,
             Constants = StaticInstance,
