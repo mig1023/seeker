b94d89ca45f27bd70dec2b0d238a2ebf5477db83
mig1023 <mig1023@yandex.ru>
2022-05-08 18:28:35 +0300

    random colors for skills (going)

--- Seeker/Seeker/Gamebook/GoingToLaughter/Constants.cs
+++ Seeker/Seeker/Gamebook/GoingToLaughter/Constants.cs
@@ -1,10 +1,34 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using static Seeker.Game.Data;
 
 namespace Seeker.Gamebook.GoingToLaughter
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
         public static Constants StaticInstance = new Constants();
+
+        public override string GetColor(ColorTypes type)
+        {
+            if (Game.Settings.IsEnabled("WithoutStyles"))
+            {
+                return base.GetColor(type);
+            }
+            else if ((type == ColorTypes.ActionBox) && ParagraphWithSkills())
+            {
+                Color myColor = Color.FromArgb(LltRandom(), LltRandom(), LltRandom());
+                return myColor.R.ToString("X2") + myColor.G.ToString("X2") + myColor.B.ToString("X2");
+            }
+            else
+            {
+                return String.Empty;
+            }
+        }
+
+        private static int LltRandom() => 200 + Game.Dice.Roll(size: 40);
+
+        private static bool ParagraphWithSkills() => (CurrentParagraphID == 1393) || (CurrentParagraphID == 1394);
 
         public static Dictionary<string, string> IncompatiblesDisadvantages { get; set; }
 
