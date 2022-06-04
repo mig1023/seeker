597ade8d5dbf049e082e4ee3baf5048aafcf67a0
mig1023 <mig1023@yandex.ru>
2022-06-04 23:03:07 +0300

    ltl refactoring (three paths)

--- Seeker/Seeker/Gamebook/ThreePaths/Actions.cs
+++ Seeker/Seeker/Gamebook/ThreePaths/Actions.cs
@@ -46,40 +46,36 @@ namespace Seeker.Gamebook.ThreePaths
         public override bool Availability(string option)
         {
             if (String.IsNullOrEmpty(option))
-            {
                 return true;
-            }
-            else
+
+            foreach (string oneOption in option.Split(','))
             {
-                foreach (string oneOption in option.Split(','))
+                if (oneOption.Contains(">") || oneOption.Contains("<"))
                 {
-                    if (oneOption.Contains(">") || oneOption.Contains("<"))
-                    {
-                        int level = Game.Services.LevelParse(option);
+                    int level = Game.Services.LevelParse(option);
 
-                        if (oneOption.Contains("ВРЕМЯ <") && (level <= protagonist.Time))
-                            return false;
-
-                        if (oneOption.Contains("ВРЕМЯ >=") && (level > protagonist.Time))
-                            return false;
-                    }
-                    else if (oneOption.Contains("ЗАКЛЯТИЕ"))
-                    {
-                        return protagonist.Spells.Contains(oneOption.Trim());
-                    }
-                    else if (oneOption.Contains("!"))
-                    {
-                        if (Game.Option.IsTriggered(oneOption.Replace("!", String.Empty).Trim()))
-                            return false;
-                    }
-                    else if (!Game.Option.IsTriggered(oneOption.Trim()))
-                    {
+                    if (oneOption.Contains("ВРЕМЯ <") && (level <= protagonist.Time))
                         return false;
-                    }
-                }
 
-                return true;
+                    if (oneOption.Contains("ВРЕМЯ >=") && (level > protagonist.Time))
+                        return false;
+                }
+                else if (oneOption.Contains("ЗАКЛЯТИЕ"))
+                {
+                    return protagonist.Spells.Contains(oneOption.Trim());
+                }
+                else if (oneOption.Contains("!"))
+                {
+                    if (Game.Option.IsTriggered(oneOption.Replace("!", String.Empty).Trim()))
+                        return false;
+                }
+                else if (!Game.Option.IsTriggered(oneOption.Trim()))
+                {
+                    return false;
+                }
             }
+
+            return true;
         }
 
         public override List<string> Representer()
