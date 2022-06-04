35b966c81f8e3f87aa52d566e340b2bb2aa711cb
mig1023 <mig1023@yandex.ru>
2022-06-04 23:02:30 +0300

    action <-> services ltl refactoring (tournament)

--- Seeker/Seeker/Gamebook/YounglingTournament/Actions.cs
+++ Seeker/Seeker/Gamebook/YounglingTournament/Actions.cs
@@ -44,7 +44,7 @@ namespace Seeker.Gamebook.YounglingTournament
             }
             else
             {
-                newStatuses.Add(String.Format("Форма {0}", GetSwordSkillName(GetSwordType())));
+                newStatuses.Add(String.Format("Форма {0}", Services.GetSwordSkillName(Services.GetSwordType())));
                 newStatuses.Add(String.Format("Понимание Силы: {0}", protagonist.ForceTechniques.Values.Sum()));
 
                 if ((protagonist.Thrust > 0) || (protagonist.EnemyThrust > 0))
@@ -130,7 +130,8 @@ namespace Seeker.Gamebook.YounglingTournament
                     if (!anotherTechnique)
                         currectSwordTechniques = SwordTypes.Rivalry;
 
-                    technique = String.Format("\nиспользует Форму {0}", GetSwordSkillName(currectSwordTechniques, rang: enemy.Rang));
+                    technique = String.Format("\nиспользует Форму {0}",
+                        Services.GetSwordSkillName(currectSwordTechniques, rang: enemy.Rang));
                 }
 
                 if (NoStrikeBack)
@@ -366,218 +367,6 @@ namespace Seeker.Gamebook.YounglingTournament
             }
         }
 
-        private static Character.SwordTypes GetSwordType()
-        {
-            int max = protagonist.SwordTechniques[Character.SwordTypes.Decisiveness];
-            Character.SwordTypes swordTechniques = Character.SwordTypes.Decisiveness;
-
-            foreach (Character.SwordTypes swordType in protagonist.SwordTechniques.Keys)
-            {
-                if (protagonist.SwordTechniques[swordType] <= 0)
-                    continue;
-
-                int swordResult = SwordSkills(swordType, out string _);
-
-                if (swordResult > max)
-                {
-                    max = swordResult;
-                    swordTechniques = swordType;
-                }
-            }
-
-            return swordTechniques;
-        }
-
-        private static int SwordSkills(Character.SwordTypes skill, out string detail)
-        {
-            int rang = protagonist.SwordTechniques[skill];
-
-            switch (skill)
-            {
-                case Character.SwordTypes.Elasticity:
-                    detail = String.Format("4 + {0} ранг", rang);
-                    return 4 + rang;
-
-                case Character.SwordTypes.Rivalry:
-                    detail = String.Format("4 + (2 x {0} ранг)", rang);
-                    return 4 + (2 * rang);
-
-                case Character.SwordTypes.Perseverance:
-                    detail = String.Format("8 + {0} ранг", rang);
-                    return 8 + rang;
-
-                case Character.SwordTypes.Aggressiveness:
-                    detail = String.Format("12 + (2 x {0} ранг)", rang);
-                    return 12 + (2 * rang);
-
-                case Character.SwordTypes.Confidence:
-                    detail = String.Format("12 + (3 x {0} ранг)", rang);
-                    return 12 + (3 * rang);
-
-                case Character.SwordTypes.Vaapad:
-                    detail = String.Format("12 + (4 x {0} ранг)", rang);
-                    return 12 + (4 * rang);
-                
-                case Character.SwordTypes.JarKai:
-                    detail = String.Format("12 + (3 x {0} ранг)", rang);
-                    return 12 + (3 * rang);
-
-                default:
-                case Character.SwordTypes.Decisiveness:
-                    detail = String.Format("1 x {0} ранг", rang);
-                    return rang;
-            }
-        }
-
-        private bool AdditionalAttack(ref List<string> fight, Character enemy, string head, string body)
-        {
-            fight.Add(String.Format("BOLD|{0}", head));
-
-            int strikeWound = Game.Dice.Roll();
-
-            fight.Add(String.Format("{0}: {1}", body, Game.Dice.Symbol(strikeWound)));
-
-            enemy.Hitpoints -= strikeWound;
-
-            fight.Add(String.Format("GOOD|Вы ранили {0}, он потерял {1} ед.выносливости (осталось {2})",
-                enemy.Name, strikeWound, enemy.Hitpoints));
-
-            protagonist.Thrust += 1;
-
-            fight.Add("GOOD|BOLD|Вы наносите укол противнику!");
-
-            return true;
-        }
-
-        private void SpeedFightHitpointsLoss(ref List<string> fight, Character character)
-        {
-            bool isProtagonist = (character == protagonist);
-
-            int technique = (isProtagonist ? protagonist.ForceTechniques[ForcesTypes.Speed] : character.Speed);
-            int wound = (5 - technique);
-            character.Hitpoints -= wound;
-
-            if (isProtagonist)
-                fight.Add(String.Format("BAD|Из-за применения Скорости Силы вы теряете {0} ед.выносливости (осталось {1})",
-                    wound, protagonist.Hitpoints));
-            else
-                fight.Add(String.Format("GOOD|Из-за применения Скорости Силы {0} теряет {1} ед.выносливости (осталось {2})",
-                    character.Name, wound, character.Hitpoints));
-        }
-
-        private bool SpeedActivation(ref List<string> fight, ref bool speedActivate, List<Character> EnemiesList)
-        {
-            fight.Add(String.Format("BOLD|Вы активируете Скорость Силы {0} ранга!",
-                protagonist.ForceTechniques[ForcesTypes.Speed]));
-
-            foreach (Character enemy in EnemiesList)
-                fight.Add(String.Format("{0} активирует Скорость Силы {1} ранга.", enemy.Name, enemy.Speed));
-
-            speedActivate = true;
-
-            return false;
-        }
-
-        private bool UseForcesInFight(ref List<string> fight, ref bool speedActivate,
-            List<Character> EnemiesList)
-        {
-            Character target = EnemiesList.Where(x => x.Hitpoints > 0).FirstOrDefault();
-
-            if (SpeedActivate && !speedActivate)
-                return SpeedActivation(ref fight, ref speedActivate, EnemiesList);
-
-            if (WithoutTechnique || speedActivate)
-                return false;
-
-            int forceTechniques = 0;
-
-            for (int i = 0; i < protagonist.ForceTechniquesOrder.Count; i++)
-            {
-                if (protagonist.ForceTechniquesOrder[i] != 0)
-                {
-                    forceTechniques = protagonist.ForceTechniquesOrder[i];
-                    protagonist.ForceTechniquesOrder[i] = 0;
-
-                    break;
-                }
-            }
-
-            if (forceTechniques == 0)
-                return SpeedActivation(ref fight, ref speedActivate, EnemiesList);
-
-            switch (forceTechniques)
-            {
-                case 1:
-                    fight.Add("BOLD|Вы применяете Толчок Силы!");
-
-                    int pushWound = Game.Dice.Roll();
-                    target.Hitpoints -= (pushWound + protagonist.ForceTechniques[ForcesTypes.Push]);
-
-                    fight.Add(String.Format("GOOD|{0} теряет {1} + {2} (за ранг техники) ед.выносливости (осталось {3})",
-                        target.Name, Game.Dice.Symbol(pushWound),
-                        protagonist.ForceTechniques[ForcesTypes.Push], target.Hitpoints));
-
-                    return true;
-
-                case 2:
-                    fight.Add("BOLD|Вы применяете Прыжок Силы!");
-
-                    int jump = Game.Dice.Roll();
-                    int technique = protagonist.ForceTechniques[ForcesTypes.Jump];
-                    bool success = (jump + technique) > 6;
-
-                    fight.Add(String.Format("Прыжок: {0} + {1} {2} 6 - {3}",
-                        Game.Dice.Symbol(jump), technique, Game.Services.Сomparison(jump + technique, 6),
-                        (success ? "прыжок удался!" : "прыжок не получился...")));
-
-                    if (success)
-                    {
-                        target.Hitpoints -= jump;
-
-                        fight.Add(String.Format("GOOD|{0} теряет {1} ед.выносливости (осталось {2})",
-                            target.Name, jump, target.Hitpoints));
-
-                        protagonist.Thrust += 1;
-
-                        fight.Add("GOOD|BOLD|Вы наносите укол противнику!");
-                    }
-
-                    return true;
-
-                case 3:
-                    fight.Add("BOLD|Вы применяете Удушение Силы!");
-
-                    protagonist.DarkSide += 50;
-                    Game.Option.Trigger("Темная сторона");
-                    fight.Add("Вы получаете +50 к очкам Тёмной стороны и ключевое слово 'Тёмная сторона'.");
-
-                    int suffWound = Game.Dice.Roll();
-                    target.Hitpoints -= suffWound;
-
-                    fight.Add(String.Format("GOOD|{0} теряет {1} ед.выносливости (осталось {2})",
-                        target.Name, Game.Dice.Symbol(suffWound), target.Hitpoints));
-
-                    protagonist.Thrust += 1;
-
-                    fight.Add("GOOD|BOLD|Вы наносите укол противнику!");
-
-                    return true;
-
-                default:
-                    return false;
-            }
-        }
-
-        private string GetSwordSkillName(SwordTypes swordTechniques, int? rang = null)
-        {
-            SwordTypes currectSwordTechniques = GetSwordType();
-
-            string skillName = Constants.SwordSkillsNames()[currectSwordTechniques];
-            int skillRang = rang ?? protagonist.SwordTechniques[currectSwordTechniques];
-
-            return String.Format("{0} ({1} ранг)", skillName, skillRang);
-        }
-
         public List<string> SwordFight()
         {
             List<string> fight = new List<string>();
@@ -592,10 +381,10 @@ namespace Seeker.Gamebook.YounglingTournament
                 EnemiesList.Add(newEnemy);
             }
 
-            SwordTypes currectSwordTechniques = GetSwordType();
-            fight.Add(String.Format("Вы выбрали для боя Форму {0}", GetSwordSkillName(currectSwordTechniques)));
+            SwordTypes currectSwordTechniques = Services.GetSwordType();
+            fight.Add(String.Format("Вы выбрали для боя Форму {0}", Services.GetSwordSkillName(currectSwordTechniques)));
 
-            int skill = SwordSkills(currectSwordTechniques, out string detail);
+            int skill = Services.SwordSkills(currectSwordTechniques, out string detail);
             fight.Add(String.Format("Ваша Ловкость в этом бою: {0} (по формуле: {1})", skill, detail));
 
             fight.Add(String.Empty);
@@ -608,7 +397,7 @@ namespace Seeker.Gamebook.YounglingTournament
             {
                 fight.Add(String.Format("HEAD|BOLD|Раунд: {0}", round));
 
-                if (UseForcesInFight(ref fight, ref speedActivate, EnemiesList))
+                if (Services.UseForcesInFight(ref fight, ref speedActivate, EnemiesList, SpeedActivate, WithoutTechnique))
                 {
                     int enemyLimit = (EnemyHitpointsLimith > 0 ? EnemyHitpointsLimith : 0);
 
@@ -622,7 +411,9 @@ namespace Seeker.Gamebook.YounglingTournament
                 if (Game.Option.IsTriggered("Скоростная атака") && (round > 3) && !rapidAttack)
                 {
                     Character target = EnemiesList.Where(x => x.Hitpoints > 0).FirstOrDefault();
-                    irresistibleAttack = AdditionalAttack(ref fight, target, "Вы проводите Скоростную атаку!", "Урон от атаки");
+
+                    irresistibleAttack = Services.AdditionalAttack(ref fight, target,
+                        "Вы проводите Скоростную атаку!", "Урон от атаки");
                 }                  
 
                 int protagonistFirstDice = Game.Dice.Roll();
@@ -667,7 +458,10 @@ namespace Seeker.Gamebook.YounglingTournament
                             enemy.Key.Name, enemy.Key.Hitpoints));
 
                         if ((heroRoundWin >= 3) && !irresistibleAttack && Game.Option.IsTriggered("Неотразимая атака"))
-                            irresistibleAttack = AdditionalAttack(ref fight, enemy.Key, "Вы проводите Неотразимую атаку!", "Урон от атаки");
+                        {
+                            irresistibleAttack = Services.AdditionalAttack(ref fight, enemy.Key,
+                                "Вы проводите Неотразимую атаку!", "Урон от атаки");
+                        }
                     }
 
                     else if (enemy.Value > hitSkill)
@@ -679,19 +473,21 @@ namespace Seeker.Gamebook.YounglingTournament
                             enemy.Key.Name, protagonist.Hitpoints));
 
                         if ((enemyRoundWin >= 3) && !strikeBack && Game.Option.IsTriggered("Встречный удар"))
-                            strikeBack = AdditionalAttack(ref fight, enemy.Key, "Вы проводите Встречный удар!", "Урон от удара");
+                        {
+                            strikeBack = Services.AdditionalAttack(ref fight, enemy.Key,
+                                "Вы проводите Встречный удар!", "Урон от удара");
+                        }
                     }
-
                     else
                         fight.Add("BOLD|Вы парировали удары друг друга");
                 }
 
                 if (speedActivate)
                 {
-                    SpeedFightHitpointsLoss(ref fight, protagonist);
+                    Services.SpeedFightHitpointsLoss(ref fight, protagonist);
 
                     foreach (Character enemy in EnemiesList.Where(x => x.Hitpoints > 0))
-                        SpeedFightHitpointsLoss(ref fight, enemy);
+                        Services.SpeedFightHitpointsLoss(ref fight, enemy);
                 }
 
                 fight.Add(String.Empty);
---
+++ Seeker/Seeker/Gamebook/YounglingTournament/Services.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static Seeker.Gamebook.YounglingTournament.Character;
+
+namespace Seeker.Gamebook.YounglingTournament
+{
+    class Services
+    {
+        private static Character protagonist = Character.Protagonist;
+
+        public static Character.SwordTypes GetSwordType()
+        {
+            int max = protagonist.SwordTechniques[Character.SwordTypes.Decisiveness];
+            Character.SwordTypes swordTechniques = Character.SwordTypes.Decisiveness;
+
+            foreach (Character.SwordTypes swordType in protagonist.SwordTechniques.Keys)
+            {
+                if (protagonist.SwordTechniques[swordType] <= 0)
+                    continue;
+
+                int swordResult = SwordSkills(swordType, out string _);
+
+                if (swordResult > max)
+                {
+                    max = swordResult;
+                    swordTechniques = swordType;
+                }
+            }
+
+            return swordTechniques;
+        }
+
+        public static int SwordSkills(Character.SwordTypes skill, out string detail)
+        {
+            int rang = protagonist.SwordTechniques[skill];
+
+            switch (skill)
+            {
+                case Character.SwordTypes.Elasticity:
+                    detail = String.Format("4 + {0} ранг", rang);
+                    return 4 + rang;
+
+                case Character.SwordTypes.Rivalry:
+                    detail = String.Format("4 + (2 x {0} ранг)", rang);
+                    return 4 + (2 * rang);
+
+                case Character.SwordTypes.Perseverance:
+                    detail = String.Format("8 + {0} ранг", rang);
+                    return 8 + rang;
+
+                case Character.SwordTypes.Aggressiveness:
+                    detail = String.Format("12 + (2 x {0} ранг)", rang);
+                    return 12 + (2 * rang);
+
+                case Character.SwordTypes.Confidence:
+                    detail = String.Format("12 + (3 x {0} ранг)", rang);
+                    return 12 + (3 * rang);
+
+                case Character.SwordTypes.Vaapad:
+                    detail = String.Format("12 + (4 x {0} ранг)", rang);
+                    return 12 + (4 * rang);
+
+                case Character.SwordTypes.JarKai:
+                    detail = String.Format("12 + (3 x {0} ранг)", rang);
+                    return 12 + (3 * rang);
+
+                default:
+                case Character.SwordTypes.Decisiveness:
+                    detail = String.Format("1 x {0} ранг", rang);
+                    return rang;
+            }
+        }
+
+        public static bool AdditionalAttack(ref List<string> fight, Character enemy, string head, string body)
+        {
+            fight.Add(String.Format("BOLD|{0}", head));
+
+            int strikeWound = Game.Dice.Roll();
+
+            fight.Add(String.Format("{0}: {1}", body, Game.Dice.Symbol(strikeWound)));
+
+            enemy.Hitpoints -= strikeWound;
+
+            fight.Add(String.Format("GOOD|Вы ранили {0}, он потерял {1} ед.выносливости (осталось {2})",
+                enemy.Name, strikeWound, enemy.Hitpoints));
+
+            protagonist.Thrust += 1;
+
+            fight.Add("GOOD|BOLD|Вы наносите укол противнику!");
+
+            return true;
+        }
+
+        public static void SpeedFightHitpointsLoss(ref List<string> fight, Character character)
+        {
+            bool isProtagonist = (character == protagonist);
+
+            int technique = (isProtagonist ? protagonist.ForceTechniques[ForcesTypes.Speed] : character.Speed);
+            int wound = (5 - technique);
+            character.Hitpoints -= wound;
+
+            if (isProtagonist)
+                fight.Add(String.Format("BAD|Из-за применения Скорости Силы вы теряете {0} ед.выносливости (осталось {1})",
+                    wound, protagonist.Hitpoints));
+            else
+                fight.Add(String.Format("GOOD|Из-за применения Скорости Силы {0} теряет {1} ед.выносливости (осталось {2})",
+                    character.Name, wound, character.Hitpoints));
+        }
+
+        public static bool SpeedActivation(ref List<string> fight, ref bool speedActivate, List<Character> EnemiesList)
+        {
+            fight.Add(String.Format("BOLD|Вы активируете Скорость Силы {0} ранга!",
+                protagonist.ForceTechniques[ForcesTypes.Speed]));
+
+            foreach (Character enemy in EnemiesList)
+                fight.Add(String.Format("{0} активирует Скорость Силы {1} ранга.", enemy.Name, enemy.Speed));
+
+            speedActivate = true;
+
+            return false;
+        }
+
+        public static bool UseForcesInFight(ref List<string> fight, ref bool speedActivate,
+            List<Character> EnemiesList, bool SpeedActivate, bool WithoutTechnique)
+        {
+            Character target = EnemiesList.Where(x => x.Hitpoints > 0).FirstOrDefault();
+
+            if (SpeedActivate && !speedActivate)
+                return SpeedActivation(ref fight, ref speedActivate, EnemiesList);
+
+            if (WithoutTechnique || speedActivate)
+                return false;
+
+            int forceTechniques = 0;
+
+            for (int i = 0; i < protagonist.ForceTechniquesOrder.Count; i++)
+            {
+                if (protagonist.ForceTechniquesOrder[i] != 0)
+                {
+                    forceTechniques = protagonist.ForceTechniquesOrder[i];
+                    protagonist.ForceTechniquesOrder[i] = 0;
+
+                    break;
+                }
+            }
+
+            if (forceTechniques == 0)
+                return SpeedActivation(ref fight, ref speedActivate, EnemiesList);
+
+            switch (forceTechniques)
+            {
+                case 1:
+                    fight.Add("BOLD|Вы применяете Толчок Силы!");
+
+                    int pushWound = Game.Dice.Roll();
+                    target.Hitpoints -= (pushWound + protagonist.ForceTechniques[ForcesTypes.Push]);
+
+                    fight.Add(String.Format("GOOD|{0} теряет {1} + {2} (за ранг техники) ед.выносливости (осталось {3})",
+                        target.Name, Game.Dice.Symbol(pushWound),
+                        protagonist.ForceTechniques[ForcesTypes.Push], target.Hitpoints));
+
+                    return true;
+
+                case 2:
+                    fight.Add("BOLD|Вы применяете Прыжок Силы!");
+
+                    int jump = Game.Dice.Roll();
+                    int technique = protagonist.ForceTechniques[ForcesTypes.Jump];
+                    bool success = (jump + technique) > 6;
+
+                    fight.Add(String.Format("Прыжок: {0} + {1} {2} 6 - {3}",
+                        Game.Dice.Symbol(jump), technique, Game.Services.Сomparison(jump + technique, 6),
+                        (success ? "прыжок удался!" : "прыжок не получился...")));
+
+                    if (success)
+                    {
+                        target.Hitpoints -= jump;
+
+                        fight.Add(String.Format("GOOD|{0} теряет {1} ед.выносливости (осталось {2})",
+                            target.Name, jump, target.Hitpoints));
+
+                        protagonist.Thrust += 1;
+
+                        fight.Add("GOOD|BOLD|Вы наносите укол противнику!");
+                    }
+
+                    return true;
+
+                case 3:
+                    fight.Add("BOLD|Вы применяете Удушение Силы!");
+
+                    protagonist.DarkSide += 50;
+                    Game.Option.Trigger("Темная сторона");
+                    fight.Add("Вы получаете +50 к очкам Тёмной стороны и ключевое слово 'Тёмная сторона'.");
+
+                    int suffWound = Game.Dice.Roll();
+                    target.Hitpoints -= suffWound;
+
+                    fight.Add(String.Format("GOOD|{0} теряет {1} ед.выносливости (осталось {2})",
+                        target.Name, Game.Dice.Symbol(suffWound), target.Hitpoints));
+
+                    protagonist.Thrust += 1;
+
+                    fight.Add("GOOD|BOLD|Вы наносите укол противнику!");
+
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+
+        public static string GetSwordSkillName(SwordTypes swordTechniques, int? rang = null)
+        {
+            SwordTypes currectSwordTechniques = GetSwordType();
+
+            string skillName = Constants.SwordSkillsNames()[currectSwordTechniques];
+            int skillRang = rang ?? protagonist.SwordTechniques[currectSwordTechniques];
+
+            return String.Format("{0} ({1} ранг)", skillName, skillRang);
+        }
+    }
+}
