034bd1a23950767c4fca1be035374efef382603b
mig1023 <mig1023@yandex.ru>
2022-08-24 16:37:36 +0300

    micro refactoring (antiquary)

--- Seeker/Seeker/Gamebook/DeathOfAntiquary/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/DeathOfAntiquary/Paragraphs.cs
@@ -7,9 +7,11 @@ namespace Seeker.Gamebook.DeathOfAntiquary
     {
         public static Paragraphs StaticInstance = new Paragraphs();
 
-        public override Paragraph Get(int id, XmlNode xmlParagraph) => base.Get(xmlParagraph);
+        public override Paragraph Get(int id, XmlNode xmlParagraph) =>
+            base.Get(xmlParagraph);
 
-        public override Option OptionParse(XmlNode xmlOption) => OptionParseWithDo(xmlOption, new Modification());
+        public override Option OptionParse(XmlNode xmlOption) =>
+            OptionParseWithDo(xmlOption, new Modification());
 
         public override Abstract.IModification ModificationParse(XmlNode xmlModification) =>
             Xml.ModificationParse(xmlModification, new Modification());
