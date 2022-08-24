a52bf765c2bd7c8b35a3a9735510f4235b9f7342
mig1023 <mig1023@yandex.ru>
2022-08-24 16:38:16 +0300

    micro refactoring (heart)

--- Seeker/Seeker/Gamebook/HeartOfIce/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/HeartOfIce/Paragraphs.cs
@@ -8,7 +8,8 @@ namespace Seeker.Gamebook.HeartOfIce
     {
         public static Paragraphs StaticInstance = new Paragraphs();
 
-        public override Paragraph Get(int id, XmlNode xmlParagraph) => base.Get(xmlParagraph);
+        public override Paragraph Get(int id, XmlNode xmlParagraph) =>
+            base.Get(xmlParagraph);
 
         public override Abstract.IActions ActionParse(XmlNode xmlAction)
         {
@@ -28,7 +29,8 @@ namespace Seeker.Gamebook.HeartOfIce
             return action;
         }
 
-        public override Option OptionParse(XmlNode xmlOption) => OptionParseWithDo(xmlOption, new Modification());
+        public override Option OptionParse(XmlNode xmlOption) =>
+            OptionParseWithDo(xmlOption, new Modification());
 
         public override Abstract.IModification ModificationParse(XmlNode xmlModification) =>
             Xml.ModificationParse(xmlModification, new Modification());
