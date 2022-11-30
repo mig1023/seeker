738485bc18808fdecfdd04f7b9bfa2af95c49efa
mig1023 <mig1023@yandex.ru>
2022-11-30 17:32:31 +0300

    availability (silver age)

--- Seeker/Seeker/Gamebook/SilverAgeSilhouette/Actions.cs
+++ Seeker/Seeker/Gamebook/SilverAgeSilhouette/Actions.cs
@@ -1,9 +1,26 @@
 ﻿using System;
+using System.Linq;
 
 namespace Seeker.Gamebook.SilverAgeSilhouette
 {
     class Actions : Prototypes.Actions, Abstract.IActions
     {
         public static Actions StaticInstance = new Actions();
+
+        public override bool Availability(string option)
+        {
+            if (String.IsNullOrEmpty(option))
+            {
+                return true;
+            }
+            else if (option.Contains("|"))
+            {
+                return option.Split('|').Where(x => Game.Option.IsTriggered(x.Trim())).Count() > 0;
+            }
+            else
+            {
+                return AvailabilityTrigger(option);
+            }
+        }
     }
 }
