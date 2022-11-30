2dd8ad13310194fe1b6ad2b9cf37a433fefd1016
mig1023 <mig1023@yandex.ru>
2022-11-30 17:34:31 +0300

    modification logic fix (silver age)

--- Seeker/Seeker/Gamebook/SilverAgeSilhouette/Modification.cs
+++ Seeker/Seeker/Gamebook/SilverAgeSilhouette/Modification.cs
@@ -2,5 +2,8 @@
 
 namespace Seeker.Gamebook.SilverAgeSilhouette
 {
-    class Modification : Prototypes.Modification, Abstract.IModification { }
+    class Modification : Prototypes.Modification, Abstract.IModification
+    {
+        public override void Do() => base.Do(Character.Protagonist);
+    }
 }
