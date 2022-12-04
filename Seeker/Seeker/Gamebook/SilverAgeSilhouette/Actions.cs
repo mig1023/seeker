2b59cb622123e49a921280e4814e09d72f87fc9f
mig1023 <mig1023@yandex.ru>
2022-12-04 16:19:58 +0300

    rating availability (silver age)

--- Seeker/Seeker/Gamebook/SilverAgeSilhouette/Actions.cs
+++ Seeker/Seeker/Gamebook/SilverAgeSilhouette/Actions.cs
@@ -6,6 +6,7 @@ namespace Seeker.Gamebook.SilverAgeSilhouette
     class Actions : Prototypes.Actions, Abstract.IActions
     {
         public static Actions StaticInstance = new Actions();
+        private static Character protagonist = Character.Protagonist;
 
         public override bool Availability(string option)
         {
@@ -16,6 +17,18 @@ namespace Seeker.Gamebook.SilverAgeSilhouette
             else if (option.Contains("|"))
             {
                 return option.Split('|').Where(x => Game.Option.IsTriggered(x.Trim())).Count() > 0;
+            }
+            else if (option.Contains("ОЦЕНКА"))
+            {
+                int level = Game.Services.LevelParse(option);
+
+                if (option.Contains("ОЦЕНКА >=") && (level > protagonist.Rating))
+                    return false;
+
+                if (option.Contains("ОЦЕНКА <") && (level <= protagonist.Rating))
+                    return false;
+
+                return true;
             }
             else
             {
