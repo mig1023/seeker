c3f1f18c4b29b350ba15afd2d06b3ece6754302f
mig1023 <mig1023@yandex.ru>
2022-12-17 16:24:17 +0300

    resource designer changes

--- Seeker/Seeker.Android/Resources/Resource.designer.cs
+++ Seeker/Seeker.Android/Resources/Resource.designer.cs
@@ -8508,76 +8508,82 @@ namespace Seeker.Droid
 			public const int SilentSchool_Chess = 2131165342;
 			
 			// aapt resource value: 0x7F07009F
-			public const int StainlessSteelRat = 2131165343;
+			public const int SilverAgeSilhouette = 2131165343;
 			
 			// aapt resource value: 0x7F0700A0
-			public const int StringOfWorlds = 2131165344;
+			public const int StainlessSteelRat = 2131165344;
 			
 			// aapt resource value: 0x7F0700A1
-			public const int SwampFever = 2131165345;
+			public const int StrikeBack = 2131165345;
 			
 			// aapt resource value: 0x7F0700A2
-			public const int SwampFever_BipolarKochkarnik = 2131165346;
+			public const int StringOfWorlds = 2131165346;
 			
 			// aapt resource value: 0x7F0700A3
-			public const int SwampFever_CrystalPath = 2131165347;
+			public const int SwampFever = 2131165347;
 			
 			// aapt resource value: 0x7F0700A4
-			public const int SwampFever_Hunting = 2131165348;
+			public const int SwampFever_BipolarKochkarnik = 2131165348;
 			
 			// aapt resource value: 0x7F0700A5
-			public const int SwampFever_LarchThorns = 2131165349;
+			public const int SwampFever_CrystalPath = 2131165349;
 			
 			// aapt resource value: 0x7F0700A6
-			public const int SwampFever_QuietPool = 2131165350;
+			public const int SwampFever_Hunting = 2131165350;
 			
 			// aapt resource value: 0x7F0700A7
-			public const int SwampFever_SpectralBushes = 2131165351;
+			public const int SwampFever_LarchThorns = 2131165351;
 			
 			// aapt resource value: 0x7F0700A8
-			public const int SwampFever_SulfurCavity = 2131165352;
+			public const int SwampFever_QuietPool = 2131165352;
 			
 			// aapt resource value: 0x7F0700A9
-			public const int SwampFever_TugOfWar = 2131165353;
+			public const int SwampFever_SpectralBushes = 2131165353;
 			
 			// aapt resource value: 0x7F0700AA
-			public const int ThoseWhoAreAboutToDie = 2131165354;
+			public const int SwampFever_SulfurCavity = 2131165354;
 			
 			// aapt resource value: 0x7F0700AB
-			public const int ThoseWhoAreAboutToDie_essedarius = 2131165355;
+			public const int SwampFever_TugOfWar = 2131165355;
 			
 			// aapt resource value: 0x7F0700AC
-			public const int ThoseWhoAreAboutToDie_retiarius = 2131165356;
+			public const int ThoseWhoAreAboutToDie = 2131165356;
 			
 			// aapt resource value: 0x7F0700AD
-			public const int ThoseWhoAreAboutToDie_secutor = 2131165357;
+			public const int ThoseWhoAreAboutToDie_essedarius = 2131165357;
 			
 			// aapt resource value: 0x7F0700AE
-			public const int ThoseWhoAreAboutToDie_thracians = 2131165358;
+			public const int ThoseWhoAreAboutToDie_retiarius = 2131165358;
 			
 			// aapt resource value: 0x7F0700AF
-			public const int ThreePaths = 2131165359;
+			public const int ThoseWhoAreAboutToDie_secutor = 2131165359;
 			
 			// aapt resource value: 0x7F0700B0
-			public const int tooltip_frame_dark = 2131165360;
+			public const int ThoseWhoAreAboutToDie_thracians = 2131165360;
 			
 			// aapt resource value: 0x7F0700B1
-			public const int tooltip_frame_light = 2131165361;
+			public const int ThreePaths = 2131165361;
 			
 			// aapt resource value: 0x7F0700B2
-			public const int VWeapons = 2131165362;
+			public const int tooltip_frame_dark = 2131165362;
 			
 			// aapt resource value: 0x7F0700B3
-			public const int VWeapons_Plan = 2131165363;
+			public const int tooltip_frame_light = 2131165363;
 			
 			// aapt resource value: 0x7F0700B4
-			public const int WildDeath = 2131165364;
+			public const int VWeapons = 2131165364;
 			
 			// aapt resource value: 0x7F0700B5
-			public const int WildDeath_Choice = 2131165365;
+			public const int VWeapons_Plan = 2131165365;
 			
 			// aapt resource value: 0x7F0700B6
-			public const int YounglingTournament = 2131165366;
+			public const int WildDeath = 2131165366;
+			
+			// aapt resource value: 0x7F0700B7
+			public const int WildDeath_Choice = 2131165367;
+			
+			// aapt resource value: 0x7F0700B8
+			public const int YounglingTournament = 2131165368;
 			
 			static Drawable()
 			{
