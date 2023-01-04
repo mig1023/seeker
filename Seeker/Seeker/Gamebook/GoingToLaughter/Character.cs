0751c8c176798f5b10d26e9f47985be13660b526
mig1023 <mig1023@yandex.ru>
2023-01-04 18:18:12 +0300

    reInit fix (going)

--- Seeker/Seeker/Gamebook/GoingToLaughter/Character.cs
+++ Seeker/Seeker/Gamebook/GoingToLaughter/Character.cs
@@ -43,14 +43,17 @@ namespace Seeker.Gamebook.GoingToLaughter
         {
             base.Init();
 
-            Heroism = 0;
-            Villainy = 0;
-            Buffoonery = 5;
-            Inspiration = 1;
+            if (Advantages == null)
+            {
+                Heroism = 0;
+                Villainy = 0;
+                Buffoonery = 5;
+                Inspiration = 1;
+                Balance = 0;
 
-            Advantages = new List<string>();
-            Disadvantages = new List<string>();
-            Balance = 0;
+                Advantages = new List<string>();
+                Disadvantages = new List<string>();
+            }            
         }
 
         public Character Clone() => new Character()
