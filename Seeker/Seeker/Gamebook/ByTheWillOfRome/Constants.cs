8e2086c4aca073dd34ef65d7a98581dd753c543d
mig1023 <mig1023@yandex.ru>
2023-02-03 13:26:42 +0300

    ParagraphsStatusesLimit (rome)

--- Seeker/Seeker/Gamebook/ByTheWillOfRome/Constants.cs
+++ Seeker/Seeker/Gamebook/ByTheWillOfRome/Constants.cs
@@ -6,6 +6,16 @@ namespace Seeker.Gamebook.ByTheWillOfRome
     {
         public static Constants StaticInstance = new Constants();
 
+        public override bool GetParagraphsStatusesLimit(out int limitStart, out int limitEnd)
+        {
+            limitStart = ParagraphsStatusesLimit[0];
+            limitEnd = ParagraphsStatusesLimit[1];
+
+            return true;
+        }
+
+        public static List<int> ParagraphsStatusesLimit { get; set; }
+
         public static Links GetLinks() => new Links
         {
             Protagonist = Character.Protagonist.Init,
