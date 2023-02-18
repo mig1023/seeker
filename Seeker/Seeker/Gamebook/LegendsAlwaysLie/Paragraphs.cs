c8b35f0d709f0f064d1733320145ddb5dc2f7e90
mig1023 <mig1023@yandex.ru>
2023-02-18 13:59:17 +0300

    Option in Actions parse fix (legends)

--- Seeker/Seeker/Gamebook/LegendsAlwaysLie/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/LegendsAlwaysLie/Paragraphs.cs
@@ -37,7 +37,7 @@ namespace Seeker.Gamebook.LegendsAlwaysLie
             }
 
             if (action.Type == "Option")
-                action.Option = OptionParse(xmlAction["Option"]);
+                action.Option = OptionParse(xmlAction);
 
             return action;
         }
