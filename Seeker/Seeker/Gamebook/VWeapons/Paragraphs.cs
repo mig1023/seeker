fc354e5294f635c292d208733e4c326883869d6a
mig1023 <mig1023@yandex.ru>
2023-03-05 12:45:19 +0300

    Enemy <--> Enemies parsing fix (vweapons)

--- Seeker/Seeker/Gamebook/VWeapons/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/VWeapons/Paragraphs.cs
@@ -18,24 +18,31 @@ namespace Seeker.Gamebook.VWeapons
             foreach (string param in GetProperties(action))
                 SetProperty(action, param, xmlAction);
 
-            if (xmlAction["Enemies"] != null)
+            if (xmlAction["Enemy"] != null)
+            {
+                action.Enemies = new List<Character> { ParseEnemy(xmlAction["Enemy"]) };
+            }
+            else if (xmlAction["Enemies"] != null)
             {
                 action.Enemies = new List<Character>();
 
                 foreach (XmlNode xmlEnemy in xmlAction.SelectNodes("Enemies/Enemy"))
-                {
-                    Character enemy = new Character();
-
-                    foreach (string param in GetProperties(enemy))
-                        SetPropertyByAttr(enemy, param, xmlEnemy);
-
-                    enemy.Cartridges = (enemy.WithoutCartridges || enemy.Animal) ? 0 : 8;
-
-                    action.Enemies.Add(enemy);
-                }
+                    action.Enemies.Add(ParseEnemy(xmlEnemy));
             }
 
             return action;
+        }
+
+        private Character ParseEnemy(XmlNode xmlEnemy)
+        {
+            Character enemy = new Character();
+
+            foreach (string param in GetProperties(enemy))
+                SetPropertyByAttr(enemy, param, xmlEnemy, maxPrefix: true);
+
+            enemy.Cartridges = (enemy.WithoutCartridges || enemy.Animal) ? 0 : 8;
+
+            return enemy;
         }
 
         public override Option OptionParse(XmlNode xmlOption) =>
