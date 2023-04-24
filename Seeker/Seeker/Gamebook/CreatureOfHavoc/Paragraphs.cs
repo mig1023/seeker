c834572fafdec26cdc8dfe8f60099cae8574bb87
mig1023 <mig1023@yandex.ru>
2023-04-24 18:08:02 +0300

    List of Modification fix (creature)

--- Seeker/Seeker/Gamebook/CreatureOfHavoc/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/CreatureOfHavoc/Paragraphs.cs
@@ -34,7 +34,7 @@ namespace Seeker.Gamebook.CreatureOfHavoc
                 XmlNode optionMod = xmlOption.SelectSingleNode("Modification");
 
                 if (optionMod != null)
-                    option.Do = Xml.ModificationParse(optionMod, new Modification());
+                    option.Do.Add(Xml.ModificationParse(optionMod, new Modification()));
 
                 paragraph.Options.Add(option);
             }
