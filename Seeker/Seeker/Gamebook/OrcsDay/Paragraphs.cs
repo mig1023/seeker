4efe5c29d80d88ccfe07c8febc1de6e398533ad6
mig1023 <mig1023@yandex.ru>
2023-04-24 18:08:28 +0300

    List of Modification fix (orc)

--- Seeker/Seeker/Gamebook/OrcsDay/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/OrcsDay/Paragraphs.cs
@@ -24,7 +24,7 @@ namespace Seeker.Gamebook.OrcsDay
                 XmlNode optionMod = xmlOption.SelectSingleNode("Modification");
 
                 if (optionMod != null)
-                    option.Do = Xml.ModificationParse(optionMod, new Modification());
+                    option.Do.Add(Xml.ModificationParse(optionMod, new Modification()));
 
                 paragraph.Options.Add(option);
             }
