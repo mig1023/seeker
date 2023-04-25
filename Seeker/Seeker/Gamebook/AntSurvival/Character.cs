15110c818fa61c5287b9de5e62747f3f024d7dde
mig1023 <mig1023@yandex.ru>
2023-04-25 17:27:44 +0300

    Time/Enemy (ants)

--- Seeker/Seeker/Gamebook/AntSurvival/Character.cs
+++ Seeker/Seeker/Gamebook/AntSurvival/Character.cs
@@ -12,6 +12,8 @@ namespace Seeker.Gamebook.AntSurvival
 
         public int Quantity { get; set; }
         public int Increase { get; set; }
+        public int Time { get; set; }
+        public int Enemy { get; set; }
 
         public override void Init()
         {
@@ -19,6 +21,8 @@ namespace Seeker.Gamebook.AntSurvival
 
             Quantity = 0;
             Increase = 0;
+            Time = 0;
+            Enemy = 0;
             Dice = new List<bool> { false, false, false, false, false, false, false };
         }
 
@@ -27,10 +31,13 @@ namespace Seeker.Gamebook.AntSurvival
             Dice = this.Dice,
             Quantity = this.Quantity,
             Increase = this.Increase,
+            Time = this.Time,
+            Enemy = this.Enemy,
         };
 
         public override string Save() => String.Join("|",
-            String.Join(",", Dice.Select(x => x ? "1" : "0")), Quantity, Increase
+            String.Join(",", Dice.Select(x => x ? "1" : "0")),
+            Quantity, Increase, Time, Enemy
         );
 
         public override void Load(string saveLine)
@@ -40,6 +47,8 @@ namespace Seeker.Gamebook.AntSurvival
             Dice = save[0].Split(',').Select(x => x == "1").ToList();
             Quantity = int.Parse(save[1]);
             Increase = int.Parse(save[2]);
+            Time = int.Parse(save[3]);
+            Enemy = int.Parse(save[4]);
 
             IsProtagonist = true;
         }
