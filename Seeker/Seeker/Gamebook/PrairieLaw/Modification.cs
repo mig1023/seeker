9ed9cab79b6f98119dafa9cc9f7fccf21d83f115
mig1023 <mig1023@yandex.ru>
2023-07-29 14:42:28 +0300

    String formats fixes #193

--- Seeker/Seeker/Gamebook/PrairieLaw/Modification.cs
+++ Seeker/Seeker/Gamebook/PrairieLaw/Modification.cs
@@ -7,22 +7,26 @@ namespace Seeker.Gamebook.PrairieLaw
         public override void Do()
         {
             if (Name == "StrengthByPoison")
+            {
                 DoByTrigger("Противоядие", () => Character.Protagonist.Strength -= 10, not: true);
-
+            }
             else if (Name == "StrengthByBlanket")
+            {
                 Character.Protagonist.Strength += (Game.Option.IsTriggered("Одеяло") ? 1 : -1);
-
+            }
             else if (Name == "StrengthByBlanketOrMatches")
             {
                 bool blanketOrMatches = Game.Option.IsTriggered("Одеяло") || Game.Option.IsTriggered("Спички");
                 Character.Protagonist.Strength += (blanketOrMatches ? 1 : -1);
             }
-
             else if (Name == "Skin")
+            {
                 DoByTrigger("Нож", () => Character.Protagonist.AnimalSkins.Add(ValueString));
-
+            }
             else
+            {
                 base.Do(Character.Protagonist);
+            }
         }
     }
 }
