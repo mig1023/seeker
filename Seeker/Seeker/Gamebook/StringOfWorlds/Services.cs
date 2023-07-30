8eff75f29f7e795d8ceaa18bb82d3836922aeb50
mig1023 <mig1023@yandex.ru>
2023-07-30 15:02:50 +0300

    String formats fixes #221

--- Seeker/Seeker/Gamebook/StringOfWorlds/Services.cs
+++ Seeker/Seeker/Gamebook/StringOfWorlds/Services.cs
@@ -11,7 +11,10 @@ namespace Seeker.Gamebook.StringOfWorlds
             string luckListShow = String.Empty;
 
             for (int i = 1; i < 7; i++)
-                luckListShow += String.Format("{0} ", Constants.LuckList[Character.Protagonist.Luck[i] ? i : i + 10]);
+            {
+                string luck = Constants.LuckList[Character.Protagonist.Luck[i] ? i : i + 10];
+                luckListShow += $"{luck} ";
+            }
 
             return luckListShow;
         }
