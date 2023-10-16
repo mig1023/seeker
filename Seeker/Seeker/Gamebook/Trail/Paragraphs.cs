b9d90a97f61d076ede2ea7fe8c5a6ce50ec13cee
mig1023 <mig1023@yandex.ru>
2023-10-16 16:56:53 +0300

    plan image (trail)

--- Seeker/Seeker/Gamebook/Trail/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/Trail/Paragraphs.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using System.Xml;
+using Seeker.Game;
 
 namespace Seeker.Gamebook.Trail
 {
     class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs
     {
         public static Paragraphs StaticInstance = new Paragraphs();
+
+        public override Paragraph Get(int id, XmlNode xmlParagraph) =>
+            base.Get(xmlParagraph);
     }
 }
