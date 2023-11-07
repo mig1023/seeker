7e2352eb377acc79f5f9acbef260c0e9a7787419
mig1023 <mig1023@yandex.ru>
2023-11-07 16:15:26 +0300

    Character (moria)

--- Seeker/Seeker/Gamebook/Moria/Character.cs
+++ Seeker/Seeker/Gamebook/Moria/Character.cs
@@ -1,9 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Seeker.Gamebook.Moria
 {
     class Character : Prototypes.Character, Abstract.ICharacter
     {
         public static Character Protagonist = new Character();
+
+        public List<string> Fellowship { get; set; }
+
+        public int MagicPause { get; set; }
+
+        public override void Init()
+        {
+            base.Init();
+
+            MagicPause = 0;
+            Fellowship = new List<string>(Constants.Fellowship.Keys);
+        }
+
+        public Character Clone() => new Character()
+        {
+            IsProtagonist = this.IsProtagonist,
+            Name = this.Name,
+            MagicPause = this.MagicPause,
+            Fellowship = new List<string>(this.Fellowship),
+        };
+
+        public override string Save() => String.Join("|",
+            Name, MagicPause, String.Join(",", Fellowship));
+
+        public override void Load(string saveLine)
+        {
+            string[] save = saveLine.Split('|');
+
+            Name = save[0];
+            MagicPause = int.Parse(save[1]);
+            Fellowship = save[2].Split(',').ToList();
+
+            IsProtagonist = true;
+        }
     }
 }
