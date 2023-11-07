2b25108b6e23d388baaa25299e35045a94c4a45d
mig1023 <mig1023@yandex.ru>
2023-11-07 16:17:32 +0300

    Status/AdditionalStatus (moria)

--- Seeker/Seeker/Gamebook/Moria/Actions.cs
+++ Seeker/Seeker/Gamebook/Moria/Actions.cs
@@ -1,9 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Seeker.Gamebook.Moria
 {
     class Actions : Prototypes.Actions, Abstract.IActions
     {
         public static Actions StaticInstance = new Actions();
+        private static Character protagonist = Character.Protagonist;
+
+        public override List<string> Status()
+        {
+            bool canMakeMagic = protagonist.MagicPause == 0;
+
+            string magic = canMakeMagic ? "доступно" :
+                $"устал (ещё {protagonist.MagicPause} параграфа)";
+
+            return new List<string> { $"Волшебство Гэндальфа: {magic}" };
+        }
+
+        public override List<string> AdditionalStatus()
+        {
+            List<string> fellowship = Constants.Fellowship
+                .OrderByDescending(x => x.Value)
+                .Where(x => protagonist.Fellowship.Contains(x.Key))
+                .Select(x => x.Key)
+                .ToList();
+
+            return fellowship;
+        }
     }
 }
