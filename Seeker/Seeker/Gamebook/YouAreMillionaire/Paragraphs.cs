bb7baf763fcc98c52b6e599958781b529617a7e8
mig1023 <mig1023@yandex.ru>
2023-11-12 13:11:40 +0300

    Option output fix (millionaire)

--- Seeker/Seeker/Gamebook/YouAreMillionaire/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/YouAreMillionaire/Paragraphs.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml;
+﻿using System;
+using System.Xml;
 using Seeker.Game;
 
 namespace Seeker.Gamebook.YouAreMillionaire
@@ -6,5 +7,20 @@ namespace Seeker.Gamebook.YouAreMillionaire
     class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs
     {
         public static Paragraphs StaticInstance = new Paragraphs();
+
+        public override Paragraph Get(int id, XmlNode xmlParagraph) =>
+            base.Get(xmlParagraph);
+
+        public override Option OptionParse(XmlNode xmlOption)
+        {
+            Option option = base.OptionParse(xmlOption);
+
+            if (String.IsNullOrEmpty(option.Text))
+                option.Text = (option.Goto == 0 ? "Начать сначала" : "Далее");
+
+            option.Text = $"$$$  {option.Text}  $$$";
+
+            return option;
+        }
     }
 }
