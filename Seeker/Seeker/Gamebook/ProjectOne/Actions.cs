318a34dd93e45f342ae757ff2e89d209c0f7162d
mig1023 <mig1023@yandex.ru>
2023-11-19 14:10:18 +0300

    Fight action + parsing (project)

--- Seeker/Seeker/Gamebook/ProjectOne/Actions.cs
+++ Seeker/Seeker/Gamebook/ProjectOne/Actions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Seeker.Gamebook.ProjectOne
 {
@@ -31,6 +32,97 @@ namespace Seeker.Gamebook.ProjectOne
                 enemies.Add($"{enemy.Name}\nловкость {enemy.Skill}  сила {enemy.Endurance}");
 
             return enemies;
+        }
+
+        public static bool NoMoreEnemies(List<Character> enemies) =>
+           enemies.Where(x => x.Endurance > 0).Count() == 0;
+
+        public List<string> Fight()
+        {
+            List<string> fight = new List<string>();
+
+            List<Character> FightEnemies = new List<Character>();
+
+            foreach (Character enemy in Enemies)
+                FightEnemies.Add(enemy.Clone());
+
+            int round = 1;
+
+            while (true)
+            {
+                fight.Add($"HEAD|BOLD|Раунд: {round}");
+
+                bool attackAlready = false;
+                int protagonistHitStrength = 0;
+
+                foreach (Character enemy in FightEnemies)
+                {
+                    if (enemy.Endurance <= 0)
+                        continue;
+
+                    fight.Add($"{enemy.Name} (сила {enemy.Endurance})");
+
+                    if (!attackAlready)
+                    {
+                        Game.Dice.DoubleRoll(out int protagonistRollFirst, out int protagonistRollSecond);
+                        protagonistHitStrength = protagonistRollFirst + protagonistRollSecond + protagonist.Skill;
+
+                        fight.Add($"Мощность вашей атаки: " +
+                            $"{Game.Dice.Symbol(protagonistRollFirst)} + " +
+                            $"{Game.Dice.Symbol(protagonistRollSecond)} + " +
+                            $"{protagonist.Skill} ловкость = {protagonistHitStrength}");
+                    }
+
+                    Game.Dice.DoubleRoll(out int enemyRollFirst, out int enemyRollSecond);
+                    int enemyHitStrength = enemyRollFirst + enemyRollSecond + enemy.Skill;
+
+                    fight.Add($"Мощность его атаки: " +
+                        $"{Game.Dice.Symbol(enemyRollFirst)} + " +
+                        $"{Game.Dice.Symbol(enemyRollSecond)} + " +
+                        $"{enemy.Skill} ловкость = {enemyHitStrength}");
+
+                    if ((protagonistHitStrength > enemyHitStrength) && !attackAlready)
+                    {
+                        fight.Add($"GOOD|{enemy.Name} ранен");
+
+                        enemy.Endurance -= 2;
+
+                        if (NoMoreEnemies(FightEnemies))
+                        {
+                            fight.Add(String.Empty);
+                            fight.Add("BIG|GOOD|Вы ПОБЕДИЛИ :)");
+                            return fight;
+                        }
+                    }
+                    else if (protagonistHitStrength > enemyHitStrength)
+                    {
+                        fight.Add($"BOLD|{enemy.Name} не смог вас ранить");
+                    }
+                    else if (protagonistHitStrength < enemyHitStrength)
+                    {
+                        fight.Add($"BAD|{enemy.Name} ранил вас");
+
+                        protagonist.Endurance -= 2;
+
+                        if (protagonist.Endurance <= 0)
+                        {
+                            fight.Add(String.Empty);
+                            fight.Add("BIG|BAD|Вы ПРОИГРАЛИ :(");
+                            return fight;
+                        }
+                    }
+                    else
+                    {
+                        fight.Add("BOLD|Ничья в раунде");
+                    }
+
+                    attackAlready = true;
+
+                    fight.Add(String.Empty);
+                }
+
+                round += 1;
+            }
         }
     }
 }
--- Seeker/Seeker/Gamebook/ProjectOne/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/ProjectOne/Paragraphs.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Xml;
 using Seeker.Game;
 
@@ -7,5 +8,46 @@ namespace Seeker.Gamebook.ProjectOne
     class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs
     {
         public static Paragraphs StaticInstance = new Paragraphs();
+
+        public override Paragraph Get(int id, XmlNode xmlParagraph) =>
+           base.Get(xmlParagraph);
+
+        public override Abstract.IActions ActionParse(XmlNode xmlAction)
+        {
+            Actions action = (Actions)ActionTemplate(xmlAction, new Actions());
+
+            foreach (string param in GetProperties(action))
+                SetProperty(action, param, xmlAction);
+
+            if (xmlAction["Enemy"] != null)
+            {
+                action.Enemies = new List<Character> { EnemyParse(xmlAction["Enemy"]) };
+            }
+            else if (xmlAction["Enemies"] != null)
+            {
+                action.Enemies = new List<Character>();
+
+                foreach (XmlNode xmlEnemy in xmlAction.SelectNodes("Enemies/Enemy"))
+                    action.Enemies.Add(EnemyParse(xmlEnemy));
+            }
+
+            return action;
+        }
+
+        public override Abstract.IModification ModificationParse(XmlNode xmlModification) =>
+            Xml.ModificationParse(xmlModification, new Modification());
+
+        private Character EnemyParse(XmlNode xmlEnemy)
+        {
+            Character enemy = new Character();
+
+            foreach (string param in GetProperties(enemy))
+                SetPropertyByAttr(enemy, param, xmlEnemy, maxPrefix: true);
+
+            enemy.Skill = enemy.MaxSkill;
+            enemy.Endurance = enemy.MaxEndurance;
+
+            return enemy;
+        }
     }
 }
