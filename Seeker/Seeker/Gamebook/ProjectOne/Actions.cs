75a451a205b0980fd36338b3f305cee6d8cd337e
mig1023 <mig1023@yandex.ru>
2023-11-19 14:09:52 +0300

    Character params (project)

--- Seeker/Seeker/Gamebook/ProjectOne/Actions.cs
+++ Seeker/Seeker/Gamebook/ProjectOne/Actions.cs
@@ -1,9 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Seeker.Gamebook.ProjectOne
 {
     class Actions : Prototypes.Actions, Abstract.IActions
     {
         public static Actions StaticInstance = new Actions();
+        private static Character protagonist = Character.Protagonist;
+
+        public List<Character> Enemies { get; set; }
+
+        public override List<string> Status() => new List<string>
+        {
+            $"Ловкость: {protagonist.Skill}/{protagonist.MaxSkill}",
+            $"Сила: {protagonist.Endurance}/{protagonist.MaxEndurance}",
+            $"Удача: {protagonist.Luck}/{protagonist.MaxLuck}",
+        };
+
+        public override bool GameOver(out int toEndParagraph, out string toEndText) =>
+            GameOverBy(protagonist.Endurance, out toEndParagraph, out toEndText);
+
+        public override List<string> Representer()
+        {
+            List<string> enemies = new List<string>();
+
+            if (Enemies == null)
+                return enemies;
+
+            foreach (Character enemy in Enemies)
+                enemies.Add($"{enemy.Name}\nловкость {enemy.Skill}  сила {enemy.Endurance}");
+
+            return enemies;
+        }
     }
 }
--- Seeker/Seeker/Gamebook/ProjectOne/Character.cs
+++ Seeker/Seeker/Gamebook/ProjectOne/Character.cs
@@ -5,5 +5,70 @@ namespace Seeker.Gamebook.ProjectOne
     class Character : Prototypes.Character, Abstract.ICharacter
     {
         public static Character Protagonist = new Character();
+
+        private int _skill;
+        public int MaxSkill { get; set; }
+        public int Skill
+        {
+            get => _skill;
+            set => _skill = Game.Param.Setter(value, max: MaxSkill, _skill, this);
+        }
+
+        private int _endurance;
+        public int MaxEndurance { get; set; }
+        public int Endurance
+        {
+            get => _endurance;
+            set => _endurance = Game.Param.Setter(value, max: MaxEndurance, _endurance, this);
+        }
+
+        private int _luck;
+        public int MaxLuck { get; set; }
+        public int Luck
+        {
+            get => _luck;
+            set => _luck = Game.Param.Setter(value, max: MaxLuck, _luck, this);
+        }
+
+        public override void Init()
+        {
+            base.Init();
+
+            MaxSkill = Game.Dice.Roll() + 6;
+            Skill = MaxSkill;
+            MaxEndurance = Game.Dice.Roll() + 12;
+            Endurance = MaxEndurance;
+            MaxLuck = Game.Dice.Roll() + 6;
+            Luck = MaxLuck;
+        }
+
+        public Character Clone() => new Character()
+        {
+            IsProtagonist = this.IsProtagonist,
+            Name = this.Name,
+            MaxSkill = this.MaxSkill,
+            Skill = this.Skill,
+            MaxEndurance = this.MaxEndurance,
+            Endurance = this.Endurance,
+            MaxLuck = this.MaxLuck,
+            Luck = this.Luck,
+        };
+
+        public override string Save() => String.Join("|",
+            MaxSkill, Skill, MaxEndurance, Endurance, MaxLuck, Luck);
+
+        public override void Load(string saveLine)
+        {
+            string[] save = saveLine.Split('|');
+
+            MaxSkill = int.Parse(save[0]);
+            Skill = int.Parse(save[1]);
+            MaxEndurance = int.Parse(save[2]);
+            Endurance = int.Parse(save[3]);
+            MaxLuck = int.Parse(save[4]);
+            Luck = int.Parse(save[5]);
+
+            IsProtagonist = true;
+        }
     }
 }
