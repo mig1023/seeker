dc70d012b7b18191b54bed033853aeeb8d35d581
mig1023 <mig1023@yandex.ru>
2023-12-07 16:37:20 +0300

    Setter with Min Param

--- Seeker/Seeker/Game/Param.cs
+++ Seeker/Seeker/Game/Param.cs
@@ -32,6 +32,22 @@ namespace Seeker.Game
         public static int Setter(int value, int max, int current, Abstract.ICharacter character) =>
             (value > max ? max : Setter(value, current, character));
 
+        public static int Setter(int value, int min, int max, int current, Abstract.ICharacter character)
+        {
+            if (value > max)
+            {
+                return max;
+            }
+            else if (value < min)
+            {
+                return min;
+            }
+            else
+            {
+                return Setter(value, current, character);
+            }
+        }
+
         public static int? Setter(int? value, int max, int? current, Abstract.ICharacter character) =>
             (value.HasValue ? Setter((int)value, max, (current ?? 0), character) : (int?)null);
     }
