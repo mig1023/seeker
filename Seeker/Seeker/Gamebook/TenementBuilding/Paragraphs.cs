d03a960723f31981feeb96df37df2b4ef48223c1
mig1023 <mig1023@yandex.ru>
2023-12-13 14:06:51 +0300

    Paragraph/Option parsing fix (tenement)

--- Seeker/Seeker/Gamebook/TenementBuilding/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/TenementBuilding/Paragraphs.cs
@@ -6,5 +6,11 @@ namespace Seeker.Gamebook.TenementBuilding
     class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs
     {
         public static Paragraphs StaticInstance = new Paragraphs();
+
+        public override Paragraph Get(int id, XmlNode xmlParagraph) =>
+            base.Get(xmlParagraph, ParagraphTemplate(xmlParagraph));
+
+        public override Option OptionParse(XmlNode xmlOption) =>
+            OptionParseWithDo(xmlOption, new Modification());
     }
 }
