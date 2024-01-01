d84eb0ed9c8991e1a251902e31789237bb887504
mig1023 <mig1023@yandex.ru>
2024-01-01 10:59:06 +0300

    ModsByArmyForces mods (president)

--- Seeker/Seeker/Gamebook/PresidentSimulator/Modification.cs
+++ Seeker/Seeker/Gamebook/PresidentSimulator/Modification.cs
@@ -4,7 +4,18 @@ namespace Seeker.Gamebook.PresidentSimulator
 {
     class Modification : Prototypes.Modification, Abstract.IModification
     {
-        public override void Do() =>
-            base.Do(Character.Protagonist);
+        private static Character protagonist = Character.Protagonist;
+
+        public override void Do()
+        {
+            if (Name == "ModsByArmyForces")
+            {
+                protagonist.Rating += Game.Dice.Roll(protagonist.Army) + 1;
+            }
+            else
+            {
+                base.Do(protagonist);
+            }
+        }
     }
 }
