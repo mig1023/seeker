f15579f30dcd480463b14b5f5cee26ba65833ba1
mig1023 <mig1023@yandex.ru>
2024-02-15 16:50:05 +0300

    GetInstance fixes (snow wall)

--- Seeker/Seeker/Gamebook/DangerFromBehindTheSnowWall/Actions.cs
+++ Seeker/Seeker/Gamebook/DangerFromBehindTheSnowWall/Actions.cs
@@ -5,5 +5,6 @@ namespace Seeker.Gamebook.DangerFromBehindTheSnowWall
     class Actions : Prototypes.Actions, Abstract.IActions
     {
         public static Actions StaticInstance = new Actions();
+        public static Actions GetInstance() => StaticInstance;
     }
 }
