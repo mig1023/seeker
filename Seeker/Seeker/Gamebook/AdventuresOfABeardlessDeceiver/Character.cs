f5194e85e086d43943bc117038111d984bd9ca97
mig1023 <mig1023@yandex.ru>
2024-02-15 16:48:04 +0300

    GetInstance fixes (aldar)

--- Seeker/Seeker/Gamebook/AdventuresOfABeardlessDeceiver/Character.cs
+++ Seeker/Seeker/Gamebook/AdventuresOfABeardlessDeceiver/Character.cs
@@ -5,6 +5,7 @@ namespace Seeker.Gamebook.AdventuresOfABeardlessDeceiver
     class Character : Prototypes.Character, Abstract.ICharacter
     {
         public static Character Protagonist = new Character();
+        public static Character GetInstance() => Protagonist;
 
         private int _strength;
         public int Strength
