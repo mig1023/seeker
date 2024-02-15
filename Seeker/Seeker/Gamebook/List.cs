067675b16ee6ea91b7a826e89085536836ccb965
mig1023 <mig1023@yandex.ru>
2024-02-15 16:43:19 +0300

    List links with Reflection

--- Seeker/Seeker/Gamebook/List.cs
+++ Seeker/Seeker/Gamebook/List.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text.RegularExpressions;
 using static Seeker.Output.Constants;
 
@@ -8,72 +9,72 @@ namespace Seeker.Gamebook
 {
     class List
     {
-        private static Dictionary<string, Links> Books = new Dictionary<string, Links>
+        private static List<string> Books = new List<string>
         {
-            ["BlackCastleDungeon"] = BlackCastleDungeon.Constants.GetLinks(),
-            ["CaptainSheltonsSecret"] = CaptainSheltonsSecret.Constants.GetLinks(),
-            ["FaithfulSwordOfTheKing"] = FaithfulSwordOfTheKing.Constants.GetLinks(),
-            ["AdventuresOfABeardlessDeceiver"] = AdventuresOfABeardlessDeceiver.Constants.GetLinks(),
-            ["DzungarWar"] = DzungarWar.Constants.GetLinks(),
-            ["RockOfTerror"] = RockOfTerror.Constants.GetLinks(),
-            ["RendezVous"] = RendezVous.Constants.GetLinks(),
-            ["SwampFever"] = SwampFever.Constants.GetLinks(),
-            ["MentorsAlwaysRight"] = MentorsAlwaysRight.Constants.GetLinks(),
-            ["LegendsAlwaysLie"] = LegendsAlwaysLie.Constants.GetLinks(),
-            ["StringOfWorlds"] = StringOfWorlds.Constants.GetLinks(),
-            ["ThreePaths"] = ThreePaths.Constants.GetLinks(),
-            ["InvisibleFront"] = InvisibleFront.Constants.GetLinks(),
-            ["SilentSchool"] = SilentSchool.Constants.GetLinks(),
-            ["ThoseWhoAreAboutToDie"] = ThoseWhoAreAboutToDie.Constants.GetLinks(),
-            ["OctopusIsland"] = OctopusIsland.Constants.GetLinks(),
-            ["CreatureOfHavoc"] = CreatureOfHavoc.Constants.GetLinks(),
-            ["BloodfeudOfAltheus"] = BloodfeudOfAltheus.Constants.GetLinks(),
-            ["PensionerSimulator"] = PensionerSimulator.Constants.GetLinks(),
-            ["HowlOfTheWerewolf"] = HowlOfTheWerewolf.Constants.GetLinks(),
-            ["LordOfTheSteppes"] = LordOfTheSteppes.Constants.GetLinks(),
-            ["StainlessSteelRat"] = StainlessSteelRat.Constants.GetLinks(),
-            ["LastHokku"] = LastHokku.Constants.GetLinks(),
-            ["Genesis"] = Genesis.Constants.GetLinks(),
-            ["Catharsis"] = Catharsis.Constants.GetLinks(),           
-            ["PrairieLaw"] = PrairieLaw.Constants.GetLinks(),
-            ["HeartOfIce"] = HeartOfIce.Constants.GetLinks(),
-            ["MasterOfTaiga"] = MasterOfTaiga.Constants.GetLinks(),
-            ["VWeapons"] = VWeapons.Constants.GetLinks(),
-            ["LandOfUnwaryBears"] = LandOfUnwaryBears.Constants.GetLinks(),
-            ["YounglingTournament"] = YounglingTournament.Constants.GetLinks(),
-            ["GoingToLaughter"] = GoingToLaughter.Constants.GetLinks(),
-            ["WildDeath"] = WildDeath.Constants.GetLinks(),
-            ["ChooseCthulhu"] = ChooseCthulhu.Constants.GetLinks(),
-            ["Damanskiy"] = Damanskiy.Constants.GetLinks(),
-            ["ByTheWillOfRome"] = ByTheWillOfRome.Constants.GetLinks(),
-            ["OrcsDay"] = OrcsDay.Constants.GetLinks(),
-            ["Sheriff"] = Sheriff.Constants.GetLinks(),
-            ["Moonrunner"] = Moonrunner.Constants.GetLinks(),
-            ["DeathOfAntiquary"] = DeathOfAntiquary.Constants.GetLinks(),
-            ["Cyberpunk"] = Cyberpunk.Constants.GetLinks(),
-            ["SilverAgeSilhouette"] = SilverAgeSilhouette.Constants.GetLinks(),
-            ["StrikeBack"] = StrikeBack.Constants.GetLinks(),
-            ["CommunityOfWorms"] = CommunityOfWorms.Constants.GetLinks(),
-            ["Ants"] = Ants.Constants.GetLinks(),
-            ["ConquistadorDiary"] = ConquistadorDiary.Constants.GetLinks(),
-            ["DinosaurIsland"] = DinosaurIsland.Constants.GetLinks(),
-            ["Trail"] = Trail.Constants.GetLinks(),
-            ["UnexpectedPassenger"] = UnexpectedPassenger.Constants.GetLinks(),
-            ["OutlawsOfSherwoodForest"] = OutlawsOfSherwoodForest.Constants.GetLinks(),
-            ["AlamutFortress"] = AlamutFortress.Constants.GetLinks(),
-            ["Moria"] = Moria.Constants.GetLinks(),
-            ["YouAreMillionaire"] = YouAreMillionaire.Constants.GetLinks(),
-            ["ProjectOne"] = ProjectOne.Constants.GetLinks(),
-            ["MadameGuillotine"] = MadameGuillotine.Constants.GetLinks(),
-            ["TenementBuilding"] = TenementBuilding.Constants.GetLinks(),
-            ["PresidentSimulator"] = PresidentSimulator.Constants.GetLinks(),
-            ["Quakers"] = Quakers.Constants.GetLinks(),
-            ["ScorpionSwamp"] = ScorpionSwamp.Constants.GetLinks(),
-            ["DangerFromBehindTheSnowWall"] = DangerFromBehindTheSnowWall.Constants.GetLinks(),
+            "BlackCastleDungeon",
+            "CaptainSheltonsSecret",
+            "FaithfulSwordOfTheKing",
+            "AdventuresOfABeardlessDeceiver",
+            "DzungarWar",
+            "RockOfTerror",
+            "RendezVous",
+            "SwampFever",
+            "MentorsAlwaysRight",
+            "LegendsAlwaysLie",
+            "StringOfWorlds",
+            "ThreePaths",
+            "InvisibleFront",
+            "SilentSchool",
+            "ThoseWhoAreAboutToDie",
+            "OctopusIsland",
+            "CreatureOfHavoc",
+            "BloodfeudOfAltheus",
+            "PensionerSimulator",
+            "HowlOfTheWerewolf",
+            "LordOfTheSteppes",
+            "StainlessSteelRat",
+            "LastHokku",
+            "Genesis",
+            "Catharsis",
+            "PrairieLaw",
+            "HeartOfIce",
+            "MasterOfTaiga",
+            "VWeapons",
+            "LandOfUnwaryBears",
+            "YounglingTournament",
+            "GoingToLaughter",
+            "WildDeath",
+            "ChooseCthulhu",
+            "Damanskiy",
+            "ByTheWillOfRome",
+            "OrcsDay",
+            "Sheriff",
+            "Moonrunner",
+            "DeathOfAntiquary",
+            "Cyberpunk",
+            "SilverAgeSilhouette",
+            "StrikeBack",
+            "CommunityOfWorms",
+            "Ants",
+            "ConquistadorDiary",
+            "DinosaurIsland",
+            "Trail",
+            "UnexpectedPassenger",
+            "OutlawsOfSherwoodForest",
+            "AlamutFortress",
+            "Moria",
+            "YouAreMillionaire",
+            "ProjectOne",
+            "MadameGuillotine",
+            "TenementBuilding",
+            "PresidentSimulator",
+            "Quakers",
+            "ScorpionSwamp",
+            "DangerFromBehindTheSnowWall",
         };
 
         public static List<string> GetBooks() =>
-            Books.Keys.ToList();
+            Books;
 
         public static SortBy Sort()
         {
@@ -170,11 +171,19 @@ namespace Seeker.Gamebook
             }
         }
 
+        private static Links GetLinksFromBook(string name)
+        {
+            Type gamebookClass = Type.GetType($"Seeker.Gamebook.{name}.Constants");
+            MethodInfo gamebookGetLinks = gamebookClass.GetMethod("GetLinks");
+
+            return gamebookGetLinks.Invoke(null, parameters: null) as Links;
+        }
+
         public static Description GetDescription(string name)
         {
             Description book = new Description
             {
-                Links = Books[name],
+                Links = GetLinksFromBook(name),
                 Book = name,
                 Illustration = string.Format("{0}.jpg", name),
                 XmlBook = string.Format("Gamebooks/{0}.xml", name),
