c814b431ad22137b933bbf95287ad654f0b8a72b
mig1023 <mig1023@yandex.ru>
2024-02-15 16:49:03 +0300

    GetInstance fixes (catharsis)

--- Seeker/Seeker/Gamebook/Catharsis/Character.cs
+++ Seeker/Seeker/Gamebook/Catharsis/Character.cs
@@ -5,6 +5,7 @@ namespace Seeker.Gamebook.Catharsis
     class Character : Prototypes.Character, Abstract.ICharacter
     {
         public static Character Protagonist = new Character();
+        public static Character GetInstance() => Protagonist;
 
         private int _life;
         public int MaxLife { get; set; }
