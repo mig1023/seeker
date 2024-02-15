b6821c07799b78d49a97461de1db2a0745f0d372
mig1023 <mig1023@yandex.ru>
2024-02-15 16:49:54 +0300

    GetInstance fixes (damanskiy)

--- Seeker/Seeker/Gamebook/Damanskiy/Character.cs
+++ Seeker/Seeker/Gamebook/Damanskiy/Character.cs
@@ -5,5 +5,6 @@ namespace Seeker.Gamebook.Damanskiy
     class Character : Prototypes.Character, Abstract.ICharacter
     {
         public static Character Protagonist = new Character();
+        public static Character GetInstance() => Protagonist;
     }
 }
