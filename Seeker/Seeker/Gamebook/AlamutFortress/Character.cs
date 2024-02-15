6437472fb7936e4c0c161bf2675f0edeef67c133
mig1023 <mig1023@yandex.ru>
2024-02-15 16:48:12 +0300

    GetInstance fixes (alamut)

--- Seeker/Seeker/Gamebook/AlamutFortress/Character.cs
+++ Seeker/Seeker/Gamebook/AlamutFortress/Character.cs
@@ -5,6 +5,7 @@ namespace Seeker.Gamebook.AlamutFortress
     class Character : Prototypes.Character, Abstract.ICharacter
     {
         public static Character Protagonist = new Character();
+        public static Character GetInstance() => Protagonist;
 
         private int _strength;
         public int MaxStrength { get; set; }
