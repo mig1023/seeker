f9b1718814bfafb84cdd847150963d98e7552a6b
mig1023 <mig1023@yandex.ru>
2024-02-15 16:56:21 +0300

    GetInstance fixes (passenger)

--- Seeker/Seeker/Gamebook/UnexpectedPassenger/Character.cs
+++ Seeker/Seeker/Gamebook/UnexpectedPassenger/Character.cs
@@ -5,5 +5,6 @@ namespace Seeker.Gamebook.UnexpectedPassenger
     class Character : Prototypes.Character, Abstract.ICharacter
     {
         public static Character Protagonist = new Character();
+        public static Character GetInstance() => Protagonist;
     }
 }
--- Seeker/Seeker/Gamebook/UnexpectedPassenger/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/UnexpectedPassenger/Paragraphs.cs
@@ -5,5 +5,6 @@ namespace Seeker.Gamebook.UnexpectedPassenger
     class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs
     {
         public static Paragraphs StaticInstance = new Paragraphs();
+        public static Paragraphs GetInstance() => StaticInstance;
     }
 }
