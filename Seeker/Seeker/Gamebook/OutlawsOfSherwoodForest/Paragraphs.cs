5fb08d5b4d3b26b7bab539f3216bd97336f996ec
mig1023 <mig1023@yandex.ru>
2024-02-15 16:52:50 +0300

    GetInstance fixes (outlaws)

--- Seeker/Seeker/Gamebook/OutlawsOfSherwoodForest/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/OutlawsOfSherwoodForest/Paragraphs.cs
@@ -6,5 +6,6 @@ namespace Seeker.Gamebook.OutlawsOfSherwoodForest
     class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs
     {
         public static Paragraphs StaticInstance = new Paragraphs();
+        public static Paragraphs GetInstance() => StaticInstance;
     }
 }
