8c6bbc538742825c5fce530cb5501d1d8438ae88
mig1023 <mig1023@yandex.ru>
2024-02-15 16:52:17 +0300

    GetInstance fixes (moonrunner)

--- Seeker/Seeker/Gamebook/Moonrunner/Constants.cs
+++ Seeker/Seeker/Gamebook/Moonrunner/Constants.cs
@@ -5,6 +5,7 @@ namespace Seeker.Gamebook.Moonrunner
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
         public static Constants StaticInstance = new Constants();
+        public static Constants GetInstance() => StaticInstance;
 
         public static Dictionary<int, string> SpellsList { get; set; }
 
@@ -12,14 +13,9 @@ namespace Seeker.Gamebook.Moonrunner
 
         public static Links GetLinks() => new Links
         {
-            Protagonist = Character.Protagonist.Init,
-            Availability = Actions.StaticInstance.Availability,
             Paragraphs = Paragraphs.StaticInstance,
             Actions = Actions.StaticInstance,
             Constants = StaticInstance,
-            Save = Character.Protagonist.Save,
-            Load = Character.Protagonist.Load,
-            Debug = Character.Protagonist.Debug,
         };
     }
 }
