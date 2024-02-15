df55b2ac81202a21fd55f80cde0026e4246c4239
mig1023 <mig1023@yandex.ru>
2024-02-15 16:57:01 +0300

    GetInstance fixes (millionaire)

--- Seeker/Seeker/Gamebook/YouAreMillionaire/Actions.cs
+++ Seeker/Seeker/Gamebook/YouAreMillionaire/Actions.cs
@@ -5,5 +5,6 @@ namespace Seeker.Gamebook.YouAreMillionaire
     class Actions : Prototypes.Actions, Abstract.IActions
     {
         public static Actions StaticInstance = new Actions();
+        public static Actions GetInstance() => StaticInstance;
     }
 }
--- Seeker/Seeker/Gamebook/YouAreMillionaire/Character.cs
+++ Seeker/Seeker/Gamebook/YouAreMillionaire/Character.cs
@@ -5,5 +5,6 @@ namespace Seeker.Gamebook.YouAreMillionaire
     class Character : Prototypes.Character, Abstract.ICharacter
     {
         public static Character Protagonist = new Character();
+        public static Character GetInstance() => Protagonist;
     }
 }
