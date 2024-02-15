ab08d337949f67a1cf9670678feff54becb02cea
mig1023 <mig1023@yandex.ru>
2024-02-15 16:50:13 +0300

    GetInstance fixes (antiquary)

--- Seeker/Seeker/Gamebook/DeathOfAntiquary/Actions.cs
+++ Seeker/Seeker/Gamebook/DeathOfAntiquary/Actions.cs
@@ -5,7 +5,9 @@ namespace Seeker.Gamebook.DeathOfAntiquary
     class Actions : Prototypes.Actions, Abstract.IActions
     {
         public static Actions StaticInstance = new Actions();
+        public static Actions GetInstance() => StaticInstance;
 
-        public override bool Availability(string option) => AvailabilityTrigger(option);
+        public override bool Availability(string option) =>
+            AvailabilityTrigger(option);
     }
 }
