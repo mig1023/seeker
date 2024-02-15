21e2bf278e396b6d3ba5a610f6b97d8d9165fbac
mig1023 <mig1023@yandex.ru>
2024-02-15 16:50:22 +0300

    GetInstance fixes (dinosaur)

--- Seeker/Seeker/Gamebook/DinosaurIsland/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/DinosaurIsland/Paragraphs.cs
@@ -5,5 +5,6 @@ namespace Seeker.Gamebook.DinosaurIsland
     class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs
     {
         public static Paragraphs StaticInstance = new Paragraphs();
+        public static Paragraphs GetInstance() => StaticInstance;
     }
 }
