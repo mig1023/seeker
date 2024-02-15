59227721d2fc4777f5fa0b538d74dd952a118a71
mig1023 <mig1023@yandex.ru>
2024-02-15 16:50:44 +0300

    GetInstance fixes (genesis)

--- Seeker/Seeker/Gamebook/Genesis/Constants.cs
+++ Seeker/Seeker/Gamebook/Genesis/Constants.cs
@@ -5,19 +5,15 @@ namespace Seeker.Gamebook.Genesis
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
         public static Constants StaticInstance = new Constants();
+        public static Constants GetInstance() => StaticInstance;
 
         public static Dictionary<string, int> GetStartValues { get; set; }
 
         public static Links GetLinks() => new Links
         {
-            Protagonist = Character.Protagonist.Init,
-            Availability = Actions.StaticInstance.Availability,
             Paragraphs = Paragraphs.StaticInstance,
             Actions = Actions.StaticInstance,
             Constants = StaticInstance,
-            Save = Character.Protagonist.Save,
-            Load = Character.Protagonist.Load,
-            Debug = Character.Protagonist.Debug,
         };
     }
 }
