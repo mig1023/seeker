6fe8f747ec158107b7f864a16689ca923e5e1399
mig1023 <mig1023@yandex.ru>
2024-02-15 16:49:36 +0300

    GetInstance fixes (creature)

--- Seeker/Seeker/Gamebook/CreatureOfHavoc/Constants.cs
+++ Seeker/Seeker/Gamebook/CreatureOfHavoc/Constants.cs
@@ -5,6 +5,7 @@ namespace Seeker.Gamebook.CreatureOfHavoc
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
         public static Constants StaticInstance = new Constants();
+        public static Constants GetInstance() => StaticInstance;
 
         public static Dictionary<char, char> TranslateReplaces = new Dictionary<char, char>
         {
@@ -23,14 +24,9 @@ namespace Seeker.Gamebook.CreatureOfHavoc
 
         public static Links GetLinks() => new Links
         {
-            Protagonist = Character.Protagonist.Init,
-            Availability = Actions.StaticInstance.Availability,
             Paragraphs = Paragraphs.StaticInstance,
             Actions = Actions.StaticInstance,
             Constants = StaticInstance,
-            Save = Character.Protagonist.Save,
-            Load = Character.Protagonist.Load,
-            Debug = Character.Protagonist.Debug,
         };
     }
 }
