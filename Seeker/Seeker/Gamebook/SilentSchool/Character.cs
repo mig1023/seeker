230263934aa6d362ccf27a1365a6702f82fae51d
mig1023 <mig1023@yandex.ru>
2024-02-15 16:54:30 +0300

    GetInstance fixes (school)

--- Seeker/Seeker/Gamebook/SilentSchool/Character.cs
+++ Seeker/Seeker/Gamebook/SilentSchool/Character.cs
@@ -5,6 +5,7 @@ namespace Seeker.Gamebook.SilentSchool
     class Character : Prototypes.Character, Abstract.ICharacter
     {
         public static Character Protagonist = new Character();
+        public static Character GetInstance() => Protagonist;
 
         private int _life;
         public int Life
