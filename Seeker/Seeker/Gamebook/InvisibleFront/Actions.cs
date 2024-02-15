cd46a36e6475d8c9e7fdd74a9f0889416fb9d10e
mig1023 <mig1023@yandex.ru>
2024-02-15 16:51:15 +0300

    GetInstance fixes (front)

--- Seeker/Seeker/Gamebook/InvisibleFront/Actions.cs
+++ Seeker/Seeker/Gamebook/InvisibleFront/Actions.cs
@@ -6,6 +6,7 @@ namespace Seeker.Gamebook.InvisibleFront
     class Actions : Prototypes.Actions, Abstract.IActions
     {
         public static Actions StaticInstance = new Actions();
+        public static Actions GetInstance() => StaticInstance;
         private static Character protagonist = Character.Protagonist;
 
         public override List<string> Status() => new List<string> {
--- Seeker/Seeker/Gamebook/InvisibleFront/Character.cs
+++ Seeker/Seeker/Gamebook/InvisibleFront/Character.cs
@@ -5,6 +5,7 @@ namespace Seeker.Gamebook.InvisibleFront
     class Character : Prototypes.Character, Abstract.ICharacter
     {
         public static Character Protagonist = new Character();
+        public static Character GetInstance() => Protagonist;
 
         public int Dissatisfaction { get; set; }
         public int Recruitment { get; set; }
