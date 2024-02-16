b91c780148d183cee5c8c0926873659372d9adcb
mig1023 <mig1023@yandex.ru>
2024-02-16 16:52:31 +0300

    GetLinks rm (lord)

--- Seeker/Seeker/Gamebook/LordOfTheSteppes/Constants.cs
+++ Seeker/Seeker/Gamebook/LordOfTheSteppes/Constants.cs
@@ -32,12 +32,5 @@ namespace Seeker.Gamebook.LordOfTheSteppes
             [Character.FightStyles.Defensive] = "оборонительный",
             [Character.FightStyles.Fullback] = "глухую защиту",
         };
-
-        public static Links GetLinks() => new Links
-        {
-            Paragraphs = Paragraphs.StaticInstance,
-            Actions = Actions.StaticInstance,
-            Constants = StaticInstance,
-        };
     }
 }
