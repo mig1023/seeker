e203bf5082b34b32668029b9827614c788c7ed9b
mig1023 <mig1023@yandex.ru>
2024-02-16 21:40:48 +0300

    GetLinks Gamebook <--> Prototype fix (rome)

--- Seeker/Seeker/Gamebook/ByTheWillOfRome/Character.cs
+++ Seeker/Seeker/Gamebook/ByTheWillOfRome/Character.cs
@@ -4,8 +4,8 @@ namespace Seeker.Gamebook.ByTheWillOfRome
 {
     class Character : Prototypes.Character, Abstract.ICharacter
     {
-        public static Character Protagonist = new Character();
-        public static Character GetInstance() => Protagonist;
+        public new static Character Protagonist = new Character();
+        public new static Character GetInstance() => Protagonist;
 
         private int _sestertius;
         public int Sestertius
