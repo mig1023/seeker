48cd2306b3294d65bd6b62732982129f178636bd
mig1023 <mig1023@yandex.ru>
2024-02-16 21:40:31 +0300

    GetLinks Gamebook <--> Prototype fix (altheus)

--- Seeker/Seeker/Gamebook/BloodfeudOfAltheus/Actions.cs
+++ Seeker/Seeker/Gamebook/BloodfeudOfAltheus/Actions.cs
@@ -5,8 +5,8 @@ namespace Seeker.Gamebook.BloodfeudOfAltheus
 {
     class Actions : Prototypes.Actions, Abstract.IActions
     {
-        public static Actions StaticInstance = new Actions();
-        public static Actions GetInstance() => StaticInstance;
+        public new static Actions StaticInstance = new Actions();
+        public new static Actions GetInstance() => StaticInstance;
         private static Character protagonist = Character.Protagonist;
 
         public int Dices { get; set; }
