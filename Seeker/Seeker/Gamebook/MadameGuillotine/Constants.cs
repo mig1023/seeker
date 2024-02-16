bd68286353103ed66023decb4d5e68ce7979e502
mig1023 <mig1023@yandex.ru>
2024-02-16 16:52:49 +0300

    GetLinks rm (guillotine)

--- Seeker/Seeker/Gamebook/MadameGuillotine/Constants.cs
+++ Seeker/Seeker/Gamebook/MadameGuillotine/Constants.cs
@@ -8,12 +8,5 @@ namespace Seeker.Gamebook.MadameGuillotine
         public static Constants GetInstance() => StaticInstance;
 
         public static Dictionary<string, string> StatNames { get; set; }
-
-        public static Links GetLinks() => new Links
-        {
-            Paragraphs = Paragraphs.StaticInstance,
-            Actions = Actions.StaticInstance,
-            Constants = StaticInstance,
-        };
     }
 }
