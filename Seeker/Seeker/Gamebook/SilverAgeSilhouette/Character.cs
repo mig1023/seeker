ea12f88eb23c09696513fcc371d0d2fc70ea3941
mig1023 <mig1023@yandex.ru>
2024-02-16 21:47:42 +0300

    GetLinks Gamebook <--> Prototype fix (silver age)

--- Seeker/Seeker/Gamebook/SilverAgeSilhouette/Character.cs
+++ Seeker/Seeker/Gamebook/SilverAgeSilhouette/Character.cs
@@ -6,8 +6,8 @@ namespace Seeker.Gamebook.SilverAgeSilhouette
 {
     class Character : Prototypes.Character, Abstract.ICharacter
     {
-        public static Character Protagonist = new Character();
-        public static Character GetInstance() => Protagonist;
+        public new static Character Protagonist = new Character();
+        public new static Character GetInstance() => Protagonist;
 
         public int Rating { get; set; }
         public List<string> Verse { get; set; }
