0c2b8af460f42ca028e9dafafa03106ec7abda2a
mig1023 <mig1023@yandex.ru>
2024-02-16 21:42:46 +0300

    GetLinks Gamebook <--> Prototype fix (snow wall)

--- Seeker/Seeker/Gamebook/DangerFromBehindTheSnowWall/Actions.cs
+++ Seeker/Seeker/Gamebook/DangerFromBehindTheSnowWall/Actions.cs
@@ -2,9 +2,5 @@
 
 namespace Seeker.Gamebook.DangerFromBehindTheSnowWall
 {
-    class Actions : Prototypes.Actions, Abstract.IActions
-    {
-        public static Actions StaticInstance = new Actions();
-        public static Actions GetInstance() => StaticInstance;
-    }
+    class Actions : Prototypes.Actions, Abstract.IActions { }
 }
