1832346ce65602f8665f12e0ee782caba43d9643
mig1023 <mig1023@yandex.ru>
2024-02-16 21:43:08 +0300

    GetLinks Gamebook <--> Prototype fix (dinosaur)

--- Seeker/Seeker/Gamebook/DinosaurIsland/Constants.cs
+++ Seeker/Seeker/Gamebook/DinosaurIsland/Constants.cs
@@ -2,9 +2,5 @@
 
 namespace Seeker.Gamebook.DinosaurIsland
 {
-    class Constants : Prototypes.Constants, Abstract.IConstants
-    {
-        public static Constants StaticInstance = new Constants();
-        public static Constants GetInstance() => StaticInstance;
-    }
+    class Constants : Prototypes.Constants, Abstract.IConstants { }
 }
