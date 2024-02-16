3cabb4ea4f90dd45c4cc4a067865b2d36c749e0c
mig1023 <mig1023@yandex.ru>
2024-02-16 21:42:15 +0300

    GetLinks Gamebook <--> Prototype fix (creature)

--- Seeker/Seeker/Gamebook/CreatureOfHavoc/Constants.cs
+++ Seeker/Seeker/Gamebook/CreatureOfHavoc/Constants.cs
@@ -4,8 +4,8 @@ namespace Seeker.Gamebook.CreatureOfHavoc
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
-        public static Constants StaticInstance = new Constants();
-        public static Constants GetInstance() => StaticInstance;
+        public new static Constants StaticInstance = new Constants();
+        public new static Constants GetInstance() => StaticInstance;
 
         public static Dictionary<char, char> TranslateReplaces = new Dictionary<char, char>
         {
