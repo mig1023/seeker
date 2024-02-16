d594c85589da2c824e4207d69ec8c919b1c7c9a2
mig1023 <mig1023@yandex.ru>
2024-02-16 21:45:15 +0300

    GetLinks Gamebook <--> Prototype fix (moria)

--- Seeker/Seeker/Gamebook/Moria/Actions.cs
+++ Seeker/Seeker/Gamebook/Moria/Actions.cs
@@ -6,8 +6,8 @@ namespace Seeker.Gamebook.Moria
 {
     class Actions : Prototypes.Actions, Abstract.IActions
     {
-        public static Actions StaticInstance = new Actions();
-        public static Actions GetInstance() => StaticInstance;
+        public new static Actions StaticInstance = new Actions();
+        public new static Actions GetInstance() => StaticInstance;
         private static Character protagonist = Character.Protagonist;
 
         public List<string> Enemies { get; set; }
