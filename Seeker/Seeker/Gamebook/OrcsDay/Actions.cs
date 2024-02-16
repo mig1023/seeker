5e35861d0d20775abe605dae0a317b05c1f81c04
mig1023 <mig1023@yandex.ru>
2024-02-16 21:45:31 +0300

    GetLinks Gamebook <--> Prototype fix (orcs)

--- Seeker/Seeker/Gamebook/OrcsDay/Actions.cs
+++ Seeker/Seeker/Gamebook/OrcsDay/Actions.cs
@@ -5,8 +5,8 @@ namespace Seeker.Gamebook.OrcsDay
 {
     class Actions : Prototypes.Actions, Abstract.IActions
     {
-        public static Actions StaticInstance = new Actions();
-        public static Actions GetInstance() => StaticInstance;
+        public new static Actions StaticInstance = new Actions();
+        public new static Actions GetInstance() => StaticInstance;
         private static Character protagonist = Character.Protagonist;
 
         public List<Character> Enemies { get; set; }
