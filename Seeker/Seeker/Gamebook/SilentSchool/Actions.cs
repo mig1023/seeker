6ecfbb69854d2893fdaff9d558164b5831682917
mig1023 <mig1023@yandex.ru>
2024-02-16 21:47:33 +0300

    GetLinks Gamebook <--> Prototype fix (school)

--- Seeker/Seeker/Gamebook/SilentSchool/Actions.cs
+++ Seeker/Seeker/Gamebook/SilentSchool/Actions.cs
@@ -6,8 +6,8 @@ namespace Seeker.Gamebook.SilentSchool
 {
     class Actions : Prototypes.Actions, Abstract.IActions
     {
-        public static Actions StaticInstance = new Actions();
-        public static Actions GetInstance() => StaticInstance;
+        public new static Actions StaticInstance = new Actions();
+        public new static Actions GetInstance() => StaticInstance;
         private static Character protagonist = Character.Protagonist;
 
         public int HarmedMyself { get; set; }
