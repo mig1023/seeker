ea4af97dd7bc927427c74eb53afcd2b027deab10
mig1023 <mig1023@yandex.ru>
2024-02-16 16:46:00 +0300

    GetLinks rm (cyberpunk)

--- Seeker/Seeker/Gamebook/Cyberpunk/Constants.cs
+++ Seeker/Seeker/Gamebook/Cyberpunk/Constants.cs
@@ -10,12 +10,5 @@ namespace Seeker.Gamebook.Cyberpunk
         public static Dictionary<string, string> CharactersParams { get; set; }
 
         public static List<string> NormalizationParams { get; set; }
-
-        public static Links GetLinks() => new Links
-        {
-            Paragraphs = Paragraphs.StaticInstance,
-            Actions = Actions.StaticInstance,
-            Constants = StaticInstance,
-        };
     }
 }
