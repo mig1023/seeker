a3f03ced1d8b5f21f0e955b53b3ac5a4ef3de472
mig1023 <mig1023@yandex.ru>
2024-02-16 21:47:13 +0300

    GetLinks Gamebook <--> Prototype fix (scorpion)

--- Seeker/Seeker/Gamebook/ScorpionSwamp/Actions.cs
+++ Seeker/Seeker/Gamebook/ScorpionSwamp/Actions.cs
@@ -6,8 +6,8 @@ namespace Seeker.Gamebook.ScorpionSwamp
 {
     class Actions : Prototypes.Actions, Abstract.IActions
     {
-        public static Actions StaticInstance = new Actions();
-        public static Actions GetInstance() => StaticInstance;
+        public new static Actions StaticInstance = new Actions();
+        public new static Actions GetInstance() => StaticInstance;
         private static Character protagonist = Character.Protagonist;
 
         public List<Character> Enemies { get; set; }
