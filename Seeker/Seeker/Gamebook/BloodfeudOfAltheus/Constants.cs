025c63fa6948164e0dbb8f706cb1769b13177f29
mig1023 <mig1023@yandex.ru>
2024-02-16 16:44:48 +0300

    GetLinks rm (altheus)

--- Seeker/Seeker/Gamebook/BloodfeudOfAltheus/Constants.cs
+++ Seeker/Seeker/Gamebook/BloodfeudOfAltheus/Constants.cs
@@ -8,12 +8,5 @@ namespace Seeker.Gamebook.BloodfeudOfAltheus
         public static Constants GetInstance() => StaticInstance;
 
         public static Dictionary<int, string> HealthLine { get; set; }
-
-        public static Links GetLinks() => new Links
-        {
-            Paragraphs = Paragraphs.StaticInstance,
-            Actions = Actions.StaticInstance,
-            Constants = StaticInstance,
-        };
     }
 }
