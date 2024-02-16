29e3a89af4518e78997fed7e848bec6293dc0730
mig1023 <mig1023@yandex.ru>
2024-02-16 21:40:02 +0300

    GetLinks Gamebook <--> Prototype fix (alamut)

--- Seeker/Seeker/Gamebook/AlamutFortress/Character.cs
+++ Seeker/Seeker/Gamebook/AlamutFortress/Character.cs
@@ -4,8 +4,8 @@ namespace Seeker.Gamebook.AlamutFortress
 {
     class Character : Prototypes.Character, Abstract.ICharacter
     {
-        public static Character Protagonist = new Character();
-        public static Character GetInstance() => Protagonist;
+        public new static Character Protagonist = new Character();
+        public new static Character GetInstance() => Protagonist;
 
         private int _strength;
         public int MaxStrength { get; set; }
