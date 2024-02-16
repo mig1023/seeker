83072bb8df0ae177c36ea129a1934869c7836979
mig1023 <mig1023@yandex.ru>
2024-02-16 21:40:20 +0300

    GetLinks Gamebook <--> Prototype fix (black castle)

--- Seeker/Seeker/Gamebook/BlackCastleDungeon/Constants.cs
+++ Seeker/Seeker/Gamebook/BlackCastleDungeon/Constants.cs
@@ -4,8 +4,8 @@ namespace Seeker.Gamebook.BlackCastleDungeon
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
-        public static Constants StaticInstance = new Constants();
-        public static Constants GetInstance() => StaticInstance;
+        public new static Constants StaticInstance = new Constants();
+        public new static Constants GetInstance() => StaticInstance;
 
         public override bool GetParagraphsStatusesLimit(out int limitStart, out int limitEnd)
         {
