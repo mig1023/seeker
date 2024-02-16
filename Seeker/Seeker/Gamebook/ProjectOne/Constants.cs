e585e1eff50849e27b6a94a1fa5adff9e4f1e91f
mig1023 <mig1023@yandex.ru>
2024-02-16 21:46:19 +0300

    GetLinks Gamebook <--> Prototype fix (project)

--- Seeker/Seeker/Gamebook/ProjectOne/Constants.cs
+++ Seeker/Seeker/Gamebook/ProjectOne/Constants.cs
@@ -4,7 +4,7 @@ namespace Seeker.Gamebook.ProjectOne
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
-        public static Constants StaticInstance = new Constants();
-        public static Constants GetInstance() => StaticInstance;
+        public new static Constants StaticInstance = new Constants();
+        public new static Constants GetInstance() => StaticInstance;
     }
 }
