72ed16280d528bd963b99f7250ad8b4761f4800e
mig1023 <mig1023@yandex.ru>
2024-02-16 16:44:55 +0300

    GetLinks rm (rome)

--- Seeker/Seeker/Gamebook/ByTheWillOfRome/Constants.cs
+++ Seeker/Seeker/Gamebook/ByTheWillOfRome/Constants.cs
@@ -18,12 +18,5 @@ namespace Seeker.Gamebook.ByTheWillOfRome
         public static List<int> ParagraphsStatusesLimit { get; set; }
 
         public static int AddonStartParagraph = 375;
-
-        public static Links GetLinks() => new Links
-        {
-            Paragraphs = Paragraphs.StaticInstance,
-            Actions = Actions.StaticInstance,
-            Constants = StaticInstance,
-        };
     }
 }
