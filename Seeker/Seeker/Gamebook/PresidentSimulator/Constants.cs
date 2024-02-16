7ee6bf8de963670bf3c25b4155b83b0f4494c2c7
mig1023 <mig1023@yandex.ru>
2024-02-16 21:46:10 +0300

    GetLinks Gamebook <--> Prototype fix (president)

--- Seeker/Seeker/Gamebook/PresidentSimulator/Constants.cs
+++ Seeker/Seeker/Gamebook/PresidentSimulator/Constants.cs
@@ -4,8 +4,8 @@ namespace Seeker.Gamebook.PresidentSimulator
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
-        public static Constants StaticInstance = new Constants();
-        public static Constants GetInstance() => StaticInstance;
+        public static new Constants StaticInstance = new Constants();
+        public static new Constants GetInstance() => StaticInstance;
 
         public static Dictionary<string, string> TextByYears { get; set; }
     }
