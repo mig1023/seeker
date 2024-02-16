0a2ed912c46a7086fcbd5182a91bb67db232f35c
mig1023 <mig1023@yandex.ru>
2024-02-16 16:45:18 +0300

    GetLinks rm (cthulhu)

--- Seeker/Seeker/Gamebook/ChooseCthulhu/Constants.cs
+++ Seeker/Seeker/Gamebook/ChooseCthulhu/Constants.cs
@@ -57,12 +57,5 @@ namespace Seeker.Gamebook.ChooseCthulhu
                 return base.GetColor(type);
             }
         }
-
-        public static Links GetLinks() => new Links
-        {
-            Paragraphs = Paragraphs.StaticInstance,
-            Actions = Actions.StaticInstance,
-            Constants = StaticInstance,
-        };
     }
 }
