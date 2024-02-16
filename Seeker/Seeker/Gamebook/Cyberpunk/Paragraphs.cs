45820184b4fa6788705ab884f77a324beda3e2cf
mig1023 <mig1023@yandex.ru>
2024-02-16 21:42:25 +0300

    GetLinks Gamebook <--> Prototype fix (cyberpunk)

--- Seeker/Seeker/Gamebook/Cyberpunk/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/Cyberpunk/Paragraphs.cs
@@ -5,8 +5,8 @@ namespace Seeker.Gamebook.Cyberpunk
 {
     class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs
     {
-        public static Paragraphs StaticInstance = new Paragraphs();
-        public static Paragraphs GetInstance() => StaticInstance;
+        public new static Paragraphs StaticInstance = new Paragraphs();
+        public new static Paragraphs GetInstance() => StaticInstance;
 
         public override Paragraph Get(int id, XmlNode xmlParagraph) =>
             base.Get(xmlParagraph);
