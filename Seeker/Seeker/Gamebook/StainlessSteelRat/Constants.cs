bcb041268357b9af4596a9c98b49dcb2ac477dc4
mig1023 <mig1023@yandex.ru>
2024-02-16 21:47:50 +0300

    GetLinks Gamebook <--> Prototype fix (rat)

--- Seeker/Seeker/Gamebook/StainlessSteelRat/Constants.cs
+++ Seeker/Seeker/Gamebook/StainlessSteelRat/Constants.cs
@@ -2,9 +2,5 @@
 
 namespace Seeker.Gamebook.StainlessSteelRat
 {
-    class Constants : Prototypes.Constants, Abstract.IConstants
-    {
-        public static Constants StaticInstance = new Constants();
-        public static Constants GetInstance() => StaticInstance;
-    }
+    class Constants : Prototypes.Constants, Abstract.IConstants { }
 }
