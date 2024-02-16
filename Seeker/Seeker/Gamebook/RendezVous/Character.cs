dfea7a2f21ddeb1fd50ef812c8b7ec1aba435593
mig1023 <mig1023@yandex.ru>
2024-02-16 21:46:47 +0300

    GetLinks Gamebook <--> Prototype fix (rendezvous)

--- Seeker/Seeker/Gamebook/RendezVous/Character.cs
+++ Seeker/Seeker/Gamebook/RendezVous/Character.cs
@@ -4,8 +4,8 @@ namespace Seeker.Gamebook.RendezVous
 {
     class Character : Prototypes.Character, Abstract.ICharacter
     {
-        public static Character Protagonist = new Character();
-        public static Character GetInstance() => Protagonist;
+        public new static Character Protagonist = new Character();
+        public new static Character GetInstance() => Protagonist;
 
         public int Awareness { get; set; }
 
