f9722751bdead8b7568b1ed3c07ad74ecf4c4694
mig1023 <mig1023@yandex.ru>
2024-02-16 21:43:51 +0300

    GetLinks Gamebook <--> Prototype fix (heart)

--- Seeker/Seeker/Gamebook/HeartOfIce/Character.cs
+++ Seeker/Seeker/Gamebook/HeartOfIce/Character.cs
@@ -6,8 +6,8 @@ namespace Seeker.Gamebook.HeartOfIce
 {
     class Character : Prototypes.Character, Abstract.ICharacter
     {
-        public static Character Protagonist = new Character();
-        public static Character GetInstance() => Protagonist;
+        public new static Character Protagonist = new Character();
+        public new static Character GetInstance() => Protagonist;
 
         private int _life;
         public int MaxLife { get; set; }
