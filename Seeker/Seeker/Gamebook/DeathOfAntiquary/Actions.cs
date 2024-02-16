2a18a7d1f82257012b7631467edfe0231140523d
mig1023 <mig1023@yandex.ru>
2024-02-16 21:42:57 +0300

    GetLinks Gamebook <--> Prototype fix (antiquary)

--- Seeker/Seeker/Gamebook/DeathOfAntiquary/Actions.cs
+++ Seeker/Seeker/Gamebook/DeathOfAntiquary/Actions.cs
@@ -4,8 +4,8 @@ namespace Seeker.Gamebook.DeathOfAntiquary
 {
     class Actions : Prototypes.Actions, Abstract.IActions
     {
-        public static Actions StaticInstance = new Actions();
-        public static Actions GetInstance() => StaticInstance;
+        public new static Actions StaticInstance = new Actions();
+        public new static Actions GetInstance() => StaticInstance;
 
         public override bool Availability(string option) =>
             AvailabilityTrigger(option);
