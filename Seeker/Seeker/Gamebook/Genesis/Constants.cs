aedc3c23327ae28f452f2c0a886388eb81992c82
mig1023 <mig1023@yandex.ru>
2024-02-16 16:51:26 +0300

    GetLinks rm (genesis)

--- Seeker/Seeker/Gamebook/Genesis/Constants.cs
+++ Seeker/Seeker/Gamebook/Genesis/Constants.cs
@@ -8,12 +8,5 @@ namespace Seeker.Gamebook.Genesis
         public static Constants GetInstance() => StaticInstance;
 
         public static Dictionary<string, int> GetStartValues { get; set; }
-
-        public static Links GetLinks() => new Links
-        {
-            Paragraphs = Paragraphs.StaticInstance,
-            Actions = Actions.StaticInstance,
-            Constants = StaticInstance,
-        };
     }
 }
