e5c0dff6bb3867a9f4382590586740cad1d6320c
mig1023 <mig1023@yandex.ru>
2024-02-17 17:22:56 +0300

    Text parsing new format (hokku)

--- Seeker/Seeker/Gamebook/LastHokku/Actions.cs
+++ Seeker/Seeker/Gamebook/LastHokku/Actions.cs
@@ -1,41 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Seeker.Gamebook.LastHokku
 {
-    class Actions : Prototypes.Actions, Abstract.IActions
-    {
-        public new static Actions StaticInstance = new Actions();
-        public new static Actions GetInstance() => StaticInstance;
-        private static Character protagonist = Character.Protagonist;
-
-        public override string TextByOptions(string option)
-        {
-            if (protagonist.Hokku == null)
-                return String.Empty;
-
-            bool wihoutHokku = Constants.GetParagraphsWithoutHokkuCreation.Contains(Game.Data.CurrentParagraphID);
-
-            if (!wihoutHokku && !String.IsNullOrEmpty(option))
-                protagonist.Hokku.Add(option);
-
-            if (protagonist.Hokku.Count >= 7)
-            {
-                List<string> oldHokku = protagonist.Hokku;
-
-                oldHokku[2] = new System.Globalization.CultureInfo("ru-RU", false).TextInfo.ToTitleCase(oldHokku[2]);
-
-                List<string> newHokku = new List<string>
-                {
-                    $"{oldHokku[0]} {oldHokku[1]}",
-                    $"{oldHokku[2]} {oldHokku[3]}.",
-                    $"{oldHokku[4]} - {oldHokku[5]} {oldHokku[6]}...",
-                };
-
-                protagonist.Hokku = newHokku;
-            }
-
-            return String.Join("\n", protagonist.Hokku);
-        }
-    }
+    class Actions : Prototypes.Actions, Abstract.IActions { }
 }
--- Seeker/Seeker/Gamebook/LastHokku/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/LastHokku/Paragraphs.cs
@@ -1,6 +1,74 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using Seeker.Game;
+using Seeker.Output;
 
 namespace Seeker.Gamebook.LastHokku
 {
-    class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs { }
+    class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs
+    {
+        public new static Paragraphs StaticInstance = new Paragraphs();
+        public new static Paragraphs GetInstance() => StaticInstance;
+        private Character protagonist = Character.Protagonist;
+
+        public override Paragraph Get(int id, XmlNode xmlParagraph) =>
+            base.Get(xmlParagraph);
+
+        private string TextByOptions(string option)
+        {
+            if (Character.Protagonist.Hokku == null)
+                return String.Empty;
+
+            bool wihoutHokku = Constants.GetParagraphsWithoutHokkuCreation.Contains(Game.Data.CurrentParagraphID);
+
+            bool needToAdd = (protagonist.Hokku.Count == 0) || (protagonist.Hokku.Last() != option);
+            bool fullHokku = (protagonist.Hokku.Count > 0) && protagonist.Hokku.Last().Contains('.');
+
+            if (!wihoutHokku && !String.IsNullOrEmpty(option) && needToAdd && !fullHokku)
+                protagonist.Hokku.Add(option);
+
+            if (protagonist.Hokku.Count >= 7)
+            {
+                List<string> oldHokku = protagonist.Hokku;
+
+                oldHokku[2] = new System.Globalization.CultureInfo("ru-RU", false).TextInfo.ToTitleCase(oldHokku[2]);
+
+                List<string> newHokku = new List<string>
+                {
+                    $"{oldHokku[0]} {oldHokku[1]}",
+                    $"{oldHokku[2]} {oldHokku[3]}.",
+                    $"{oldHokku[4]} - {oldHokku[5]} {oldHokku[6]}...",
+                };
+
+                protagonist.Hokku = newHokku;
+            }
+
+            return String.Join("\n", protagonist.Hokku);
+        }
+
+        public override List<Text> TextsParse(XmlNode xmlNode, bool main = false)
+        {
+            string textByOption = TextByOptions(Game.Data.CurrentSelectedOption);
+
+            if (main && !String.IsNullOrEmpty(textByOption))
+            {
+                return new List<Text> { Xml.TextLine(textByOption) };
+            }
+            else if (xmlNode["Text"] != null)
+            {
+                return new List<Text> { Xml.TextLineParse(xmlNode["Text"]) };
+            }
+            else
+            {
+                List<Text> texts = new List<Text>();
+
+                foreach (XmlNode text in xmlNode.SelectNodes("Texts/Text"))
+                    texts.Add(Xml.TextLineParse(text));
+
+                return texts;
+            }
+        }
+    }
 }
