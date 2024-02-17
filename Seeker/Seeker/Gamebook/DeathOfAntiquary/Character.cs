2ba7137ec719f6eccd92aa72e0647a3f422359f7
mig1023 <mig1023@yandex.ru>
2024-02-17 17:20:37 +0300

    Gamebooks instances fix (antiquary)

--- Seeker/Seeker/Gamebook/DeathOfAntiquary/Character.cs
+++ Seeker/Seeker/Gamebook/DeathOfAntiquary/Character.cs
@@ -2,5 +2,9 @@
 
 namespace Seeker.Gamebook.DeathOfAntiquary
 {
-    class Character : Prototypes.Character, Abstract.ICharacter { }
+    class Character : Prototypes.Character, Abstract.ICharacter
+    {
+        public new static Character Protagonist = new Character();
+        public new static Character GetInstance() => Protagonist;
+    }
 }
