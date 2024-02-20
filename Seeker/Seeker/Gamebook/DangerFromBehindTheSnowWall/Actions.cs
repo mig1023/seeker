a8e6b076e5f79a498628f99026f30174732c1174
mig1023 <mig1023@yandex.ru>
2024-02-20 12:41:45 +0300

    Fight action (snow wall)

--- Seeker/Seeker/Gamebook/DangerFromBehindTheSnowWall/Actions.cs
+++ Seeker/Seeker/Gamebook/DangerFromBehindTheSnowWall/Actions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Seeker.Gamebook.DangerFromBehindTheSnowWall
 {
@@ -27,5 +28,111 @@ namespace Seeker.Gamebook.DangerFromBehindTheSnowWall
 
         private static string MoneyFormat(int ecu) =>
             String.Format("{0:f1}", (double)ecu / 10).TrimEnd('0').TrimEnd(',').Replace(',', '.');
+
+        public override List<string> Representer()
+        {
+            List<string> enemies = new List<string>();
+
+            if (Enemies == null)
+                return enemies;
+
+            foreach (Character enemy in Enemies)
+                enemies.Add($"{enemy.Name}\nловкость {enemy.Skill}  сила {enemy.Strength}  удар {enemy.Damage}");
+
+            return enemies;
+        }
+
+        public static bool NoMoreEnemies(List<Character> enemies) =>
+            enemies.Where(x => x.Strength > 0).Count() == 0;
+
+        public List<string> Fight()
+        {
+            List<string> fight = new List<string>();
+
+            List<Character> FightEnemies = new List<Character>();
+
+            foreach (Character enemy in Enemies)
+                FightEnemies.Add(enemy.Clone());
+
+            int round = 1;
+
+            while (true)
+            {
+                fight.Add($"HEAD|BOLD|Раунд: {round}");
+
+                bool attackAlready = false;
+                int protagonistHitStrength = 0;
+
+                foreach (Character enemy in FightEnemies)
+                {
+                    if (enemy.Strength <= 0)
+                        continue;
+
+                    fight.Add($"{enemy.Name} (сила {enemy.Strength})");
+
+                    if (!attackAlready)
+                    {
+                        int protagonistRoll = Game.Dice.Roll();
+                        protagonistHitStrength = (protagonistRoll * 2) + protagonist.Skill;
+
+                        fight.Add($"Сила вашей атаки: " +
+                            $"{Game.Dice.Symbol(protagonistRoll)} x 2 + " +
+                            $"{protagonist.Skill} = {protagonistHitStrength}");
+                    }
+
+                    int enemyRoll = Game.Dice.Roll();
+                    int enemyHitStrength = (enemyRoll * 2) + enemy.Skill;
+
+                    fight.Add($"Сила его атаки: " +
+                        $"{Game.Dice.Symbol(enemyRoll)} x 2 + " +
+                        $"{enemy.Skill} = {enemyHitStrength}");
+
+                    if ((protagonistHitStrength > enemyHitStrength) && !attackAlready)
+                    {
+                        string points = Game.Services.CoinsNoun(protagonist.Damage, "очко", "очка", "очков");
+                        fight.Add($"GOOD|{enemy.Name} ранен");
+                        fight.Add($"Он теряет {protagonist.Damage} {points} Силы");
+
+                        enemy.Strength -= protagonist.Damage;
+
+                        if (NoMoreEnemies(FightEnemies))
+                        {
+                            fight.Add(String.Empty);
+                            fight.Add("BIG|GOOD|Вы ПОБЕДИЛИ :)");
+                            return fight;
+                        }
+                    }
+                    else if (protagonistHitStrength > enemyHitStrength)
+                    {
+                        fight.Add($"BOLD|{enemy.Name} не смог вас ранить");
+                    }
+                    else if (protagonistHitStrength < enemyHitStrength)
+                    {
+                        string points = Game.Services.CoinsNoun(enemy.Damage, "очко", "очка", "очков");
+                        fight.Add($"BAD|{enemy.Name} ранил вас");
+                        fight.Add($"Вы теряете {enemy.Damage} {points} Силы");
+
+                        protagonist.Strength -= enemy.Damage;
+
+                        if (protagonist.Strength <= 0)
+                        {
+                            fight.Add(String.Empty);
+                            fight.Add("BIG|BAD|Вы ПРОИГРАЛИ :(");
+                            return fight;
+                        }
+                    }
+                    else
+                    {
+                        fight.Add("BOLD|Ничья в раунде");
+                    }
+
+                    attackAlready = true;
+
+                    fight.Add(String.Empty);
+                }
+
+                round += 1;
+            }
+        }
     }
 }
--- Seeker/Seeker/Gamebook/DangerFromBehindTheSnowWall/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/DangerFromBehindTheSnowWall/Paragraphs.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml;
+﻿using System.Collections.Generic;
+using System.Xml;
 using Seeker.Game;
 
 namespace Seeker.Gamebook.DangerFromBehindTheSnowWall
@@ -10,5 +11,43 @@ namespace Seeker.Gamebook.DangerFromBehindTheSnowWall
 
         public override Paragraph Get(int id, XmlNode xmlParagraph) =>
            base.Get(xmlParagraph, ParagraphTemplate(xmlParagraph));
+
+        public override Abstract.IActions ActionParse(XmlNode xmlAction)
+        {
+            Actions action = (Actions)ActionTemplate(xmlAction, new Actions());
+
+            foreach (string param in GetProperties(action))
+                SetProperty(action, param, xmlAction);
+
+            if (xmlAction["Enemy"] != null)
+            {
+                action.Enemies = new List<Character> { EnemyParse(xmlAction["Enemy"]) };
+            }
+            else if (xmlAction["Enemies"] != null)
+            {
+                action.Enemies = new List<Character>();
+
+                foreach (XmlNode xmlEnemy in xmlAction.SelectNodes("Enemies/Enemy"))
+                    action.Enemies.Add(EnemyParse(xmlEnemy));
+            }
+
+            return action;
+        }
+
+        public override Abstract.IModification ModificationParse(XmlNode xmlModification) =>
+            Xml.ModificationParse(xmlModification, new Modification());
+
+        private Character EnemyParse(XmlNode xmlEnemy)
+        {
+            Character enemy = new Character();
+
+            foreach (string param in GetProperties(enemy))
+                SetPropertyByAttr(enemy, param, xmlEnemy, maxPrefix: true);
+
+            enemy.Skill = enemy.MaxSkill;
+            enemy.Strength = enemy.MaxStrength;
+
+            return enemy;
+        }
     }
 }
