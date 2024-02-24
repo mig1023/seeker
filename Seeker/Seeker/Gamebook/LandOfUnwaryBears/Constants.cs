479c788bbf396257bc720e6f99f6c36d2beecafb
mig1023 <mig1023@yandex.ru>
2024-02-24 13:26:11 +0300

    Create/Link refactoring (bears)

--- Seeker/Seeker/Gamebook/LandOfUnwaryBears/Constants.cs
+++ Seeker/Seeker/Gamebook/LandOfUnwaryBears/Constants.cs
@@ -4,9 +4,6 @@ namespace Seeker.Gamebook.LandOfUnwaryBears
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
-        public new static Constants StaticInstance = new Constants();
-        public new static Constants GetInstance() => StaticInstance;
-
         public override string GetFont() => "RobotoFont";
     }
 }
