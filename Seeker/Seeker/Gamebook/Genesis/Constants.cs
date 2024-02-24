088bf8d92ac7057dfe6571e975e5a5adda8d7b21
mig1023 <mig1023@yandex.ru>
2024-02-24 13:25:33 +0300

    Create/Link refactoring (genesis)

--- Seeker/Seeker/Gamebook/Genesis/Constants.cs
+++ Seeker/Seeker/Gamebook/Genesis/Constants.cs
@@ -4,9 +4,6 @@ namespace Seeker.Gamebook.Genesis
 {
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
-        public new static Constants StaticInstance = new Constants();
-        public new static Constants GetInstance() => StaticInstance;
-
         public static Dictionary<string, int> GetStartValues { get; set; }
     }
 }
