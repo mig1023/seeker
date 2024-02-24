26414b2b404d68d54d3e17244611008ab162d63b
mig1023 <mig1023@yandex.ru>
2024-02-24 13:24:58 +0300

    Create/Link refactoring (aniquary)

--- Seeker/Seeker/Gamebook/DeathOfAntiquary/Actions.cs
+++ Seeker/Seeker/Gamebook/DeathOfAntiquary/Actions.cs
@@ -4,9 +4,6 @@ namespace Seeker.Gamebook.DeathOfAntiquary
 {
     class Actions : Prototypes.Actions, Abstract.IActions
     {
-        public new static Actions StaticInstance = new Actions();
-        public new static Actions GetInstance() => StaticInstance;
-
         public override bool Availability(string option) =>
             AvailabilityTrigger(option);
     }
--- Seeker/Seeker/Gamebook/DeathOfAntiquary/Character.cs
+++ Seeker/Seeker/Gamebook/DeathOfAntiquary/Character.cs
@@ -4,7 +4,8 @@ namespace Seeker.Gamebook.DeathOfAntiquary
 {
     class Character : Prototypes.Character, Abstract.ICharacter
     {
-        public new static Character Protagonist = new Character();
-        public new static Character GetInstance() => Protagonist;
+        public static Character Protagonist { get; set; }
+        public override void Set(object character) =>
+            Protagonist = (Character)character;
     }
 }
