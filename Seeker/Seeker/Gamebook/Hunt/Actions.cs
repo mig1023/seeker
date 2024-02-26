811a543661dfb88676d206abcaa50da0b73aa5cd
mig1023 <mig1023@yandex.ru>
2024-02-26 14:34:44 +0300

    Characters properties + Statuses output (hunt)

--- Seeker/Seeker/Gamebook/Hunt/Actions.cs
+++ Seeker/Seeker/Gamebook/Hunt/Actions.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Seeker.Gamebook.Hunt
 {
     class Actions : Prototypes.Actions, Abstract.IActions
     {
+        public override List<string> Status() => new List<string>
+        {
+            $"Укушенные: {Character.Protagonist.Bitten}",
+        };
+
         public override bool Availability(string option)
         {
             if (String.IsNullOrEmpty(option))
--- Seeker/Seeker/Gamebook/Hunt/Character.cs
+++ Seeker/Seeker/Gamebook/Hunt/Character.cs
@@ -7,5 +7,47 @@ namespace Seeker.Gamebook.Hunt
         public static Character Protagonist { get; set; }
         public override void Set(object character) =>
             Protagonist = (Character)character;
+
+        private int _bitten;
+        public int Bitten
+        {
+            get => _bitten;
+            set => _bitten = Game.Param.Setter(value, _bitten, this);
+        }
+
+        private int _cycle;
+        public int Cycle
+        {
+            get => _cycle;
+            set => _cycle = Game.Param.Setter(value, _cycle, this);
+        }
+
+        public override void Init()
+        {
+            base.Init();
+
+            Bitten = 0;
+            Cycle = 0;
+        }
+
+        public Character Clone() => new Character()
+        {
+            IsProtagonist = this.IsProtagonist,
+            Bitten = this.Bitten,
+            Cycle = this.Cycle,
+        };
+
+        public override string Save() =>
+            String.Join("|", Bitten, Cycle);
+
+        public override void Load(string saveLine)
+        {
+            string[] save = saveLine.Split('|');
+
+            Bitten = int.Parse(save[0]);
+            Cycle = int.Parse(save[1]);
+
+            IsProtagonist = true;
+        }
     }
 }
