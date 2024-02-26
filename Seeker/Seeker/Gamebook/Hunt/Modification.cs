fb701d5f2fb3ae57a1097279b78d29c16d82d143
mig1023 <mig1023@yandex.ru>
2024-02-26 14:33:28 +0300

    RandomAction+RandomResponse (hunt)

--- Seeker/Seeker/Gamebook/Hunt/Modification.cs
+++ Seeker/Seeker/Gamebook/Hunt/Modification.cs
@@ -4,7 +4,22 @@ namespace Seeker.Gamebook.Hunt
 {
     class Modification : Prototypes.Modification, Abstract.IModification
     {
-        public override void Do() =>
-            base.Do(Character.Protagonist);
+        public override void Do()
+        {
+            if (Name == "RandomAction")
+            {
+                int action = Game.Dice.Roll(size: 10);
+                Game.Option.Trigger($"{action}действие");
+            }
+            else if (Name == "RandomResponse")
+            {
+                int response = Game.Dice.Roll(size: 10);
+                Game.Option.Trigger($"{response}ответ");
+            }
+            else
+            {
+                base.Do(Character.Protagonist);
+            }
+        }
     }
 }
