202e86b83ebbcb9404334c0b03c62b6001ad11f7
mig1023 <mig1023@yandex.ru>
2024-02-26 14:32:26 +0300

    Availability + TextsParse (hunt)

--- Seeker/Seeker/Gamebook/Hunt/Actions.cs
+++ Seeker/Seeker/Gamebook/Hunt/Actions.cs
@@ -2,5 +2,18 @@
 
 namespace Seeker.Gamebook.Hunt
 {
-    class Actions : Prototypes.Actions, Abstract.IActions { }
+    class Actions : Prototypes.Actions, Abstract.IActions
+    {
+        public override bool Availability(string option)
+        {
+            if (String.IsNullOrEmpty(option))
+            {
+                return true;
+            }
+            else
+            {
+                return AvailabilityTrigger(option);
+            }
+        }
+    }
 }
--- Seeker/Seeker/Gamebook/Hunt/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/Hunt/Paragraphs.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Xml;
 using Seeker.Game;
+using Seeker.Output;
 
 namespace Seeker.Gamebook.Hunt
 {
@@ -8,6 +10,38 @@ namespace Seeker.Gamebook.Hunt
     {
         public override Paragraph Get(int id, XmlNode xmlParagraph) =>
            base.Get(xmlParagraph, ParagraphTemplate(xmlParagraph));
+
+        public override List<Text> TextsParse(XmlNode xmlNode, bool main = false)
+        {
+            if (xmlNode["Text"] != null)
+            {
+                return new List<Text> { Xml.TextLineParse(xmlNode["Text"]) };
+            }
+            else
+            {
+                List<Text> texts = new List<Text>();
+
+                foreach (XmlNode text in xmlNode.SelectNodes("Texts/Text"))
+                {
+                    string option = text.Attributes["Availability"].InnerText;
+                    bool isOptioned = !String.IsNullOrEmpty(option);
+
+                    if (isOptioned && option.Contains("!"))
+                    {
+                        if (Option.IsTriggered(option.Replace("!", String.Empty).Trim()))
+                            continue;
+                    }
+                    else if (isOptioned && !Option.IsTriggered(option))
+                    {
+                        continue;
+                    }
+
+                    texts.Add(Xml.TextLineParse(text));
+                }
+
+                return texts;
+            }
+        }
 
         public override Option OptionParse(XmlNode xmlOption) =>
             OptionParseWithDo(xmlOption, new Modification());
