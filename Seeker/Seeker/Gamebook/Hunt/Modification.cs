982c2faadd52635a370da0a65fda08acf77cc382
mig1023 <mig1023@yandex.ru>
2024-02-29 14:34:34 +0300

    Modification ltl refactoring (hunt)

--- Seeker/Seeker/Gamebook/Hunt/Modification.cs
+++ Seeker/Seeker/Gamebook/Hunt/Modification.cs
@@ -4,41 +4,49 @@ namespace Seeker.Gamebook.Hunt
 {
     class Modification : Prototypes.Modification, Abstract.IModification
     {
+        private void RandomTrigger(string trigger)
+        {
+            int random = Game.Dice.Roll(size: 10);
+            Game.Option.Trigger($"{random}{trigger}");
+        }
+
+        private void ProbabilityTrigger(int probability, string trigger)
+        {
+            if (Game.Dice.Roll(size: probability) == 1)
+                Game.Option.Trigger(trigger);
+        }
+
+        private void Clean(string trigger)
+        {
+            for (int i = 0; i < 10; i++)
+                Game.Option.Trigger($"{i}{trigger}", remove: true);
+        }
+
         public override void Do()
         {
             if (Name == "RandomAction")
             {
-                int action = Game.Dice.Roll(size: 10);
-                Game.Option.Trigger($"{action}действие");
+                RandomTrigger("действие");
             }
             else if (Name == "CleanAction")
             {
-                for (int i = 0; i < 10; i++)
-                    Game.Option.Trigger($"{i}действие", remove: true);
+                Clean("действие");
             }
             else if (Name == "RandomResponse")
             {
-                int response = Game.Dice.Roll(size: 10);
-                Game.Option.Trigger($"{response}ответ");
+                RandomTrigger("ответ");
             }
             else if (Name == "CleanResponse")
             {
-                for (int i = 0; i < 10; i++)
-                    Game.Option.Trigger($"{i}ответ", remove: true);
+                Clean("ответ");
             }
             else if (Name == "RandomBit")
             {
-                int bit = Game.Dice.Roll(size: 2);
-
-                if (bit == 1)
-                    Game.Option.Trigger("Укусил");
+                ProbabilityTrigger(probability: 2, "Укусил");
             }
             else if (Name == "RandomMiss")
             {
-                int miss = Game.Dice.Roll(size: 4);
-
-                if (miss == 1)
-                    Game.Option.Trigger("Мимо");
+                ProbabilityTrigger(probability: 4, "Мимо");
             }
             else
             {
