9a0048495326961b4ebe4468caa35a37f8ee2642
mig1023 <mig1023@yandex.ru>
2024-03-12 12:31:48 +0300

    NewTankCrew modification (tank)

--- Seeker/Seeker/Gamebook/Tank/Modification.cs
+++ Seeker/Seeker/Gamebook/Tank/Modification.cs
@@ -4,7 +4,18 @@ namespace Seeker.Gamebook.Tank
 {
     class Modification : Prototypes.Modification, Abstract.IModification
     {
-        public override void Do() =>
-            base.Do(Character.Protagonist);
+        public override void Do()
+        {
+            if (Name == "NewTankCrew")
+            {
+                Character.Protagonist.Driver = 4;
+                Character.Protagonist.Shooter = 2;
+                Character.Protagonist.Gunner = 3;
+            }
+            else
+            {
+                base.Do(Character.Protagonist);
+            }
+        }
     }
 }
