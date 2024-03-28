c0e932f4300b3cb35c79884b8d100c8cf9071417
mig1023 <mig1023@yandex.ru>
2024-03-28 16:46:34 +0300

    Evidence Search (insight)

--- Seeker/Seeker/Gamebook/Insight/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/Insight/Paragraphs.cs
@@ -5,8 +5,37 @@ namespace Seeker.Gamebook.Insight
 {
     class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs
     {
-        public override Paragraph Get(int id, XmlNode xmlParagraph) =>
-            base.Get(xmlParagraph);
+        public override Paragraph Get(int id, XmlNode xmlParagraph)
+        {
+            Paragraph paragraph = ParagraphTemplate(xmlParagraph);
+
+            foreach (XmlNode xmlOption in xmlParagraph.SelectNodes("Options/*"))
+            {
+                Option option = OptionsTemplateWithoutGoto(xmlOption);
+
+                if (ThisIsBack(xmlOption))
+                {
+                    option.Goto = GetGoto(xmlOption, wayBack: Character.Protagonist.WayBack);
+                }
+                else
+                {
+                    option.Goto = Xml.IntParse(xmlOption.Attributes["Goto"]);
+                }
+
+                paragraph.Options.Add(option);
+            }
+
+            if (!WhithoutEvidenceSearch(id))
+                paragraph.Options.Add(GetEvidenceSearch(id, xmlParagraph));
+
+            foreach (XmlNode xmlAction in xmlParagraph.SelectNodes("Actions/*"))
+                paragraph.Actions.Add(ActionParse(xmlAction));
+
+            foreach (XmlNode xmlModification in xmlParagraph.SelectNodes("Modifications/*"))
+                paragraph.Modification.Add(Xml.ModificationParse(xmlModification, new Modification()));
+
+            return  paragraph;
+        }
 
         public override Abstract.IActions ActionParse(XmlNode xmlAction)
         {
@@ -21,5 +50,36 @@ namespace Seeker.Gamebook.Insight
 
         public override Abstract.IModification ModificationParse(XmlNode xmlModification) =>
             Game.Xml.ModificationParse(xmlModification, new Modification());
+
+        private static Option GetEvidenceSearch(int id, XmlNode xmlParagraph)
+        {
+            bool evidence = Xml.BoolParse(xmlParagraph["EvidenceSearch"]);
+            int link = evidence ? id + 10 : 336;
+            Character.Protagonist.WayBack = id;
+
+            return GetOption(link, text: "Искать улики");
+        }
+
+        private bool WhithoutEvidenceSearch(int id)
+        {
+            if (Data.Constants.GetParagraphsWithoutStatuses().Contains(id))
+            {
+                return true;
+            }
+            else if (id == 336)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static Option GetOption(int link, string text) => new Option
+        {
+            Goto = link,
+            Text = text,
+        };
     }
 }
