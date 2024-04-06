2424284bafdfe0d42c1929172ccd6968ddb91949
mig1023 <mig1023@yandex.ru>
2024-04-06 15:15:42 +0300

    Availability action (alcatraz)

--- Seeker/Seeker/Gamebook/Alcatraz/Actions.cs
+++ Seeker/Seeker/Gamebook/Alcatraz/Actions.cs
@@ -2,5 +2,31 @@
 
 namespace Seeker.Gamebook.Alcatraz
 {
-    class Actions : Prototypes.Actions, Abstract.IActions { }
+    class Actions : Prototypes.Actions, Abstract.IActions
+    {
+        public override bool Availability(string option)
+        {
+            if (String.IsNullOrEmpty(option))
+            {
+                return true;
+            }
+            else
+            {
+                foreach (string oneOption in option.Split(','))
+                {
+                    if (oneOption.Contains("!"))
+                    {
+                        if (Game.Option.IsTriggered(oneOption.Replace("!", String.Empty).Trim()))
+                            return false;
+                    }
+                    else if (!Game.Option.IsTriggered(oneOption.Trim()))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+    }
 }
--- Seeker/Seeker/Gamebook/Alcatraz/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/Alcatraz/Paragraphs.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using System.Xml;
+using Seeker.Game;
 
 namespace Seeker.Gamebook.Alcatraz
 {
-    class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs { }
+    class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs
+    {
+        public override Paragraph Get(int id, XmlNode xmlParagraph) =>
+            base.Get(xmlParagraph);
+    }
 }
