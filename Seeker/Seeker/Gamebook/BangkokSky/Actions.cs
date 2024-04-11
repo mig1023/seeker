e868fc1ee501580b0002ac83bba87fbf1ade21d8
mig1023 <mig1023@yandex.ru>
2024-04-11 15:56:32 +0300

    Action + Paragraph parsing (bangkok)

--- Seeker/Seeker/Gamebook/BangkokSky/Actions.cs
+++ Seeker/Seeker/Gamebook/BangkokSky/Actions.cs
@@ -1,6 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Seeker.Gamebook.BangkokSky
 {
-    class Actions : Prototypes.Actions, Abstract.IActions { }
+    class Actions : Prototypes.Actions, Abstract.IActions
+    {
+        public List<string> Get()
+        {
+            return new List<string> { "RELOAD" };
+        }
+
+        public override bool IsButtonEnabled(bool secondButton = false)
+        {
+            bool triggeredAlready = (Type == "Get") && Game.Option.IsTriggered(Trigger);
+            bool advantagesCount = (Type == "Get") && (Game.Data.Triggers.Count >= 4);
+
+            return !(triggeredAlready || advantagesCount);
+        }
+    }
 }
--- Seeker/Seeker/Gamebook/BangkokSky/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/BangkokSky/Paragraphs.cs
@@ -3,5 +3,25 @@ using Seeker.Game;
 
 namespace Seeker.Gamebook.BangkokSky
 {
-    class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs { }
+    class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs
+    {
+        public override Paragraph Get(int id, XmlNode xmlParagraph) =>
+            base.Get(xmlParagraph);
+
+        public override Abstract.IActions ActionParse(XmlNode xmlAction)
+        {
+            Actions action = (Actions)ActionTemplate(xmlAction, new Actions());
+
+            foreach (string param in GetProperties(action))
+                SetProperty(action, param, xmlAction);
+
+            if (action.Type == "Get")
+                action.Trigger = action.Button;
+
+            return action;
+        }
+
+        public override Abstract.IModification ModificationParse(XmlNode xmlModification) =>
+            (Abstract.IModification)base.ModificationParse(xmlModification, new Modification());
+    }
 }
