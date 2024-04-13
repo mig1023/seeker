bb0acf29533b763ce7cf0c106c7524ccb87cbe2e
mig1023 <mig1023@yandex.ru>
2024-04-13 15:05:58 +0300

    Signal mod (moritai)

--- Seeker/Seeker/Gamebook/PrisonerOfMoritaiCastle/Modification.cs
+++ Seeker/Seeker/Gamebook/PrisonerOfMoritaiCastle/Modification.cs
@@ -4,7 +4,17 @@ namespace Seeker.Gamebook.PrisonerOfMoritaiCastle
 {
     class Modification : Prototypes.Modification, Abstract.IModification
     {
-        public override void Do() =>
-            base.Do(Character.Protagonist);
+        public override void Do()
+        {
+            if (Name == "Signal")
+            {
+                if (Game.Option.IsTriggered("сигнал"))
+                    Game.Option.Trigger("пожар");
+            }
+            else
+            {
+                base.Do(Character.Protagonist);
+            }
+        }
     }
 }
