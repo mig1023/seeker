2363e1380cc28c774f85671876e2b8def4b856bc
mig1023 <mig1023@yandex.ru>
2024-04-13 14:49:03 +0300

    Modifications fix (bangkok)

--- Seeker/Seeker/Gamebook/BangkokSky/Modification.cs
+++ Seeker/Seeker/Gamebook/BangkokSky/Modification.cs
@@ -2,5 +2,9 @@
 
 namespace Seeker.Gamebook.BangkokSky
 {
-    class Modification : Prototypes.Modification, Abstract.IModification { }
+    class Modification : Prototypes.Modification, Abstract.IModification
+    {
+        public override void Do() =>
+            base.Do(Character.Protagonist);
+    }
 }
