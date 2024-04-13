1e029b58e6e5b45900a8e79458327293aa571f53
mig1023 <mig1023@yandex.ru>
2024-04-13 15:06:51 +0300

    CasuisticWound mod (moritai)

--- Seeker/Seeker/Gamebook/PrisonerOfMoritaiCastle/Modification.cs
+++ Seeker/Seeker/Gamebook/PrisonerOfMoritaiCastle/Modification.cs
@@ -11,6 +11,11 @@ namespace Seeker.Gamebook.PrisonerOfMoritaiCastle
                 if (Game.Option.IsTriggered("сигнал"))
                     Game.Option.Trigger("пожар");
             }
+            else if (Name == "CasuisticWound")
+            {
+                if (Character.Protagonist.Hitpoints > 3)
+                    Character.Protagonist.Hitpoints -= 1;
+            }
             else
             {
                 base.Do(Character.Protagonist);
