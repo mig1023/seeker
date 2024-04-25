4c5889c06794dd2b7889c18794c125f64e8b5691
mig1023 <mig1023@yandex.ru>
2024-04-25 12:46:51 +0300

    Roll Coin action (fate)

--- Seeker/Seeker/Gamebook/SwordAndFate/Actions.cs
+++ Seeker/Seeker/Gamebook/SwordAndFate/Actions.cs
@@ -1,6 +1,15 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace Seeker.Gamebook.SwordAndFate
 {
-    class Actions : Prototypes.Actions, Abstract.IActions { }
+    class Actions : Prototypes.Actions, Abstract.IActions
+    {
+        public List<string> RollCoin()
+        {
+            bool coin = Game.Dice.Roll() % 2 == 0;
+            string line = coin ? "GOOD|На монетке выпал ОРЁЛ" : "BAD|На монетке выпала РЕШКА";
+
+            return new List<string> { $"BIG|{line}" };
+        }
+    }
 }
--- Seeker/Seeker/Gamebook/SwordAndFate/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/SwordAndFate/Paragraphs.cs
@@ -1,6 +1,14 @@
-﻿using System;
+﻿using System.Xml;
+using Seeker.Game;
 
 namespace Seeker.Gamebook.SwordAndFate
 {
-    class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs { }
+    class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs
+    {
+        public override Paragraph Get(int id, XmlNode xmlParagraph) =>
+            base.Get(xmlParagraph);
+
+        public override Abstract.IActions ActionParse(XmlNode xmlAction) =>
+            base.ActionParse(xmlAction, new Actions(), GetProperties(new Actions()), new Modification());
+    }
 }
