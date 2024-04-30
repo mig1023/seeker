3c051f7483c2fe44b8d4d5550bab0ef5f116d95d
mig1023 <mig1023@yandex.ru>
2024-04-30 13:30:48 +0300

    HitpointsBySkills mods (fear)

--- Seeker/Seeker/Gamebook/PowerOfFear/Modification.cs
+++ Seeker/Seeker/Gamebook/PowerOfFear/Modification.cs
@@ -23,6 +23,10 @@ namespace Seeker.Gamebook.PowerOfFear
             {
                 Character.Protagonist.Hitpoints = 10;
             }
+            else if (Name == "HitpointsBySkills")
+            {
+                Character.Protagonist.Hitpoints += (Character.Protagonist.Strength > 0 ? 2 : 0);
+            }
             else
             {
                 base.Do();
