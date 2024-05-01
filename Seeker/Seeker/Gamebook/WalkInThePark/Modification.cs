bfecb9d70b609ca4c22864d646e3de6c93688994
mig1023 <mig1023@yandex.ru>
2024-05-01 15:35:22 +0300

    Modifcations (park)

--- Seeker/Seeker/Gamebook/WalkInThePark/Modification.cs
+++ Seeker/Seeker/Gamebook/WalkInThePark/Modification.cs
@@ -2,5 +2,18 @@
 
 namespace Seeker.Gamebook.WalkInThePark
 {
-    class Modification : Prototypes.Modification, Abstract.IModification { }
+    class Modification : Prototypes.Modification, Abstract.IModification
+    {
+        public override void Do()
+        {
+            if (Name == "HalfMoney")
+            {
+                Character.Protagonist.Money /= 2;
+            }
+            else
+            {
+                base.Do(Character.Protagonist);
+            }
+        }
+    }
 }
