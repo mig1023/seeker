807c233987da9be68eecae038499a6eb4c3f44b7
mig1023 <mig1023@yandex.ru>
2024-05-17 13:14:53 +0300

    RandomEndurance action (scorpion)

--- Seeker/Seeker/Gamebook/ScorpionSwamp/Actions.cs
+++ Seeker/Seeker/Gamebook/ScorpionSwamp/Actions.cs
@@ -90,6 +90,25 @@ namespace Seeker.Gamebook.ScorpionSwamp
             return enduranceCheck;
         }
 
+        public List<string> RandomEndurance()
+        {
+            Game.Dice.DoubleRoll(out int firstDice, out int secondDice);
+
+            List<string> endurance = new List<string> {
+                $"Бросаем кубики: {Game.Dice.Symbol(firstDice)} и " +
+                $"{Game.Dice.Symbol(secondDice)}" };
+
+            endurance.Add($"Вычитаем из выносливости наименьший");
+
+            int lost = firstDice > secondDice ? firstDice : secondDice;
+
+            Character.Protagonist.Endurance -= lost;
+
+            string count = Game.Services.CoinsNoun(lost, "единицу", "единицы", "единиц");
+            endurance.Add($"BIG|BAD|Выносливость снижена на {lost} {count}");
+            return endurance;
+        }
+
         public List<string> Mastery()
         {
             Game.Dice.DoubleRoll(out int firstDice, out int secondDice);
