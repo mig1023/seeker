9183c36e0e7a3d48f31d857051ccdb2dc3f2370a
mig1023 <mig1023@yandex.ru>
2024-05-19 13:30:44 +0300

    RollCoin action (help)

--- Seeker/Seeker/Gamebook/HelpJane/Actions.cs
+++ Seeker/Seeker/Gamebook/HelpJane/Actions.cs
@@ -1,6 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Seeker.Gamebook.HelpJane
 {
-    class Actions : Prototypes.Actions, Abstract.IActions { }
+    class Actions : Prototypes.Actions, Abstract.IActions
+    {
+        public List<string> RollCoin()
+        {
+            bool coin = Game.Dice.Roll() % 2 == 0;
+            string line = coin ? "На монетке выпал ОРЁЛ" : "На монетке выпала РЕШКА";
+
+            Game.Buttons.Disable(coin, "Выпал «орёл»", "Выпала «решка»");
+
+            return new List<string> { $"BIG|BOLD|{line}" };
+        }
+    }
 }
--- Seeker/Seeker/Gamebook/HelpJane/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/HelpJane/Paragraphs.cs
@@ -3,5 +3,19 @@ using Seeker.Game;
 
 namespace Seeker.Gamebook.HelpJane
 {
-    class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs { }
+    class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs
+    {
+        public override Paragraph Get(int id, XmlNode xmlParagraph) =>
+            base.Get(xmlParagraph);
+
+        public override Abstract.IActions ActionParse(XmlNode xmlAction)
+        {
+            Actions action = (Actions)ActionTemplate(xmlAction, new Actions());
+
+            foreach (string param in GetProperties(action))
+                SetProperty(action, param, xmlAction);
+
+            return action;
+        }
+    }
 }
