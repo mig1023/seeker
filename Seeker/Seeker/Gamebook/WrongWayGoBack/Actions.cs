cd6bd5563a10d538c6900ea0edd2ac3bc8360699
mig1023 <mig1023@yandex.ru>
2024-05-21 14:24:05 +0300

    Time property (wrong way)

--- Seeker/Seeker/Gamebook/WrongWayGoBack/Actions.cs
+++ Seeker/Seeker/Gamebook/WrongWayGoBack/Actions.cs
@@ -3,5 +3,12 @@ using System.Collections.Generic;
 
 namespace Seeker.Gamebook.WrongWayGoBack
 {
-    class Actions : Prototypes.Actions, Abstract.IActions { }
+    class Actions : Prototypes.Actions, Abstract.IActions
+    {
+        public override List<string> Status()
+        {
+            TimeSpan time = TimeSpan.FromSeconds(Character.Protagonist.Time);
+            return new List<string> { "Оставшееся время:" + time.ToString(@"hh\:mm\:ss") };
+        }
+    }
 }
--- Seeker/Seeker/Gamebook/WrongWayGoBack/Character.cs
+++ Seeker/Seeker/Gamebook/WrongWayGoBack/Character.cs
@@ -2,5 +2,34 @@
 
 namespace Seeker.Gamebook.WrongWayGoBack
 {
-    class Character : Prototypes.Character, Abstract.ICharacter { }
+    class Character : Prototypes.Character, Abstract.ICharacter
+    {
+        public static Character Protagonist { get; set; }
+        public override void Set(object character) =>
+            Protagonist = (Character)character;
+
+        public int Time { get; set; }
+
+        public override void Init()
+        {
+            base.Init();
+            Time = 3661;
+        }
+
+        public Character Clone() => new Character()
+        {
+            IsProtagonist = this.IsProtagonist,
+            Name = this.Name,
+            Time = this.Time,
+        };
+
+        public override string Save() =>
+            Time.ToString();
+
+        public override void Load(string saveLine)
+        {
+            Time = Game.Xml.IntParse(saveLine);
+            IsProtagonist = true;
+        }
+    }
 }
