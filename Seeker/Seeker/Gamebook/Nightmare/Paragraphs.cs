0a04e4e5a27682b030164dc0dabb2d52fb423d58
mig1023 <mig1023@yandex.ru>
2024-06-15 11:16:29 +0300

    option button color by constants (nightmare)

--- Seeker/Seeker/Gamebook/Nightmare/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/Nightmare/Paragraphs.cs
@@ -10,5 +10,16 @@ namespace Seeker.Gamebook.Nightmare
 
         public override Abstract.IActions ActionParse(XmlNode xmlAction) =>
             (Actions)ActionTemplate(xmlAction, new Actions());
+
+        public override Option OptionParse(XmlNode xmlOption)
+        {
+            Option option = OptionsTemplate(xmlOption);
+
+            if (Constants.Buttons.ContainsKey(option.Goto))
+                option.Style = Constants.Buttons[option.Goto]; 
+
+            return option;
+        }
+            
     }
 }
