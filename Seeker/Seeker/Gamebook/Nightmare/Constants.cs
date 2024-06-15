a955b4e12de06d7e414eabc6939e8e3d1bc46ee3
mig1023 <mig1023@yandex.ru>
2024-06-15 11:03:36 +0300

    buttons color by parts (nightmare)

--- Seeker/Seeker/Gamebook/Nightmare/Constants.cs
+++ Seeker/Seeker/Gamebook/Nightmare/Constants.cs
@@ -1,6 +1,33 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using static Seeker.Output.Buttons;
+using static Seeker.Game.Data;
+using System.Linq;
 
 namespace Seeker.Gamebook.Nightmare
 {
-    class Constants : Prototypes.Constants, Abstract.IConstants { }
+    class Constants : Prototypes.Constants, Abstract.IConstants
+    {
+        public static Dictionary<int, string> Buttons { get; set; }
+
+        public override string GetColor(ButtonTypes type)
+        {
+            if (type == ButtonTypes.Main)
+            {
+                string currentColor = String.Empty;
+
+                foreach (int startParagraph in Constants.Buttons.Keys.OrderBy(x => x))
+                {
+                    if (Game.Data.CurrentParagraphID >= startParagraph)
+                        currentColor = Constants.Buttons[startParagraph];
+                }
+
+                return currentColor;
+            }
+            else
+            {
+                return base.GetColor(type);
+            }
+        }
+    }
 }
