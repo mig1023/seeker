364ff0a88a5cdd6864d8586861597828134a265b
mig1023 <mig1023@yandex.ru>
2024-06-19 15:39:41 +0300

    images of parts (adventures)

--- Seeker/Seeker/Gamebook/ChooseYourOwnAdventure/Constants.cs
+++ Seeker/Seeker/Gamebook/ChooseYourOwnAdventure/Constants.cs
@@ -1,6 +1,32 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using static Seeker.Output.Buttons;
+using System.Linq;
 
 namespace Seeker.Gamebook.ChooseYourOwnAdventure
 {
-    class Constants : Prototypes.Constants, Abstract.IConstants { }
+    class Constants : Prototypes.Constants, Abstract.IConstants
+    {
+        public static Dictionary<int, string> Buttons { get; set; }
+
+        public override string GetColor(ButtonTypes type)
+        {
+            if ((type == ButtonTypes.Main) || (type == ButtonTypes.Action))
+            {
+                string currentColor = String.Empty;
+
+                foreach (int startParagraph in Constants.Buttons.Keys.OrderBy(x => x))
+                {
+                    if (Game.Data.CurrentParagraphID >= startParagraph)
+                        currentColor = Constants.Buttons[startParagraph];
+                }
+
+                return currentColor;
+            }
+            else
+            {
+                return base.GetColor(type);
+            }
+        }
+    }
 }
--- Seeker/Seeker/Gamebook/ChooseYourOwnAdventure/Paragraphs.cs
+++ Seeker/Seeker/Gamebook/ChooseYourOwnAdventure/Paragraphs.cs
@@ -1,6 +1,22 @@
-﻿using System;
+﻿using Seeker.Game;
+using System;
+using System.Xml;
 
 namespace Seeker.Gamebook.ChooseYourOwnAdventure
 {
-    class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs { }
+    class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs
+    {
+        public override Paragraph Get(int id, XmlNode xmlParagraph) =>
+           base.Get(xmlParagraph);
+
+        public override Option OptionParse(XmlNode xmlOption)
+        {
+            Option option = OptionsTemplate(xmlOption);
+
+            if (Constants.Buttons.ContainsKey(option.Goto))
+                option.Style = Constants.Buttons[option.Goto];
+
+            return option;
+        }
+    }
 }
