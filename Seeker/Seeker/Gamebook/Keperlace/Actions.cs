a0ed47c40ea0810198563f21bd12fd34da0a31a4
mig1023 <mig1023@yandex.ru>
2024-07-28 12:57:00 +0300

    Triggers Availability (keperlace)

--- Seeker/Seeker/Gamebook/Keperlace/Actions.cs
+++ Seeker/Seeker/Gamebook/Keperlace/Actions.cs
@@ -2,5 +2,9 @@
 
 namespace Seeker.Gamebook.Keperlace
 {
-    class Actions : Prototypes.Actions, Abstract.IActions { }
+    class Actions : Prototypes.Actions, Abstract.IActions
+    {
+        public override bool Availability(string option) =>
+            AvailabilityTrigger(option);
+    }
 }
