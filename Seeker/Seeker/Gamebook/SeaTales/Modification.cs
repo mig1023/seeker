095a6cecad9d000aadd49cfb8184029491099147
mig1023 <mig1023@yandex.ru>
2024-08-24 17:33:50 +0300

    GameOver action (sea tales)

--- Seeker/Seeker/Gamebook/SeaTales/Modification.cs
+++ Seeker/Seeker/Gamebook/SeaTales/Modification.cs
@@ -4,7 +4,16 @@ namespace Seeker.Gamebook.SeaTales
 {
     class Modification : Prototypes.Modification, Abstract.IModification
     {
-        public override void Do() =>
-            base.Do(Character.Protagonist);
+        public override void Do()
+        {
+            if (Name == "ReInit")
+            {
+                Character.Protagonist.Init();
+            }
+            else
+            {
+                base.Do(Character.Protagonist);
+            }
+        }
     }
 }
--- Seeker/Seeker/Gamebook/SeaTales/Parts/Third.cs
+++ Seeker/Seeker/Gamebook/SeaTales/Parts/Third.cs
@@ -24,8 +24,49 @@ namespace Seeker.Gamebook.SeaTales.Parts
         public bool GameOver(out int toEndParagraph, out string toEndText)
         {
             toEndParagraph = 0;
-            toEndText = String.Empty;
-            return false;
+
+            if (Character.Protagonist.Alcoholism >= 10)
+            {
+                toEndParagraph = 720;
+                toEndText = "Алкоголизм зашкалил...";
+            }
+            else if (Character.Protagonist.Heroism >= 110)
+            {
+                toEndParagraph = 940;
+                toEndText = "Героизм зашкалил!!";
+            }
+            else if (Character.Protagonist.Adventurism <= 0)
+            {
+                toEndParagraph = 1050;
+                toEndText = "Авантюризм зашкалил...";
+            }
+            else if (Character.Protagonist.Adventurism >= 10)
+            {
+                toEndParagraph = 830;
+                toEndText = "Авантюризм зашкалил!!";
+            }
+            else if (Character.Protagonist.Travel >= 100)
+            {
+                toEndParagraph = 755;
+                toEndText = "Плавание достигло своей цели!";
+            }
+            else if (Character.Protagonist.Travel <= -100)
+            {
+                toEndParagraph = 888;
+                toEndText = "Плавание занесло куда-то не туда...";
+            }
+            else if (Character.Protagonist.Reputation <= -100)
+            {
+                toEndParagraph = 1160;
+                toEndText = "Репутация зашкалила...";
+            }
+            else
+            {
+                toEndText = String.Empty;
+                return false;
+            }
+
+            return true;
         }
 
         public List<string> Test(Actions action)
