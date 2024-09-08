2722be1adafb2c1bbbfde47f28da7cc9b5bba45c
mig1023 <mig1023@yandex.ru>
2024-09-08 12:38:54 +0300

    Availability (road)

--- Seeker/Seeker/Gamebook/UndergroundRoad/Actions.cs
+++ Seeker/Seeker/Gamebook/UndergroundRoad/Actions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Seeker.Gamebook.UndergroundRoad
 {
@@ -7,5 +8,33 @@ namespace Seeker.Gamebook.UndergroundRoad
     {
         public override List<string> Status() =>
             new List<string> { $"Ранений: {Character.Protagonist.Wounds}" };
+
+        public override bool Availability(string option)
+        {
+            if (String.IsNullOrEmpty(option))
+            {
+                return true;
+            }
+            else if (option.Contains("НЕ РАНЕН ДВАЖДЫ"))
+            {
+                return Character.Protagonist.Wounds < 2;
+            }
+            else if (option.Contains("НЕ РАНЕН"))
+            {
+                return Character.Protagonist.Wounds < 1;
+            }
+            else if (option.Contains("РАНЕН ДВАЖДЫ"))
+            {
+                return Character.Protagonist.Wounds == 2;
+            }
+            else if (option.Contains("РАНЕН"))
+            {
+                return Character.Protagonist.Wounds > 0;
+            }
+            else
+            {
+                return base.Availability(option);
+            }
+        }
     }
 }
