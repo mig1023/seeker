a3cb8cef06fed7d35cc9ca41affc25fd8b674305
mig1023 <mig1023@yandex.ru>
2024-09-08 12:38:38 +0300

    Character property (road)

--- Seeker/Seeker/Gamebook/UndergroundRoad/Actions.cs
+++ Seeker/Seeker/Gamebook/UndergroundRoad/Actions.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Seeker.Gamebook.UndergroundRoad
 {
-    class Actions : Prototypes.Actions, Abstract.IActions { }
+    class Actions : Prototypes.Actions, Abstract.IActions
+    {
+        public override List<string> Status() =>
+            new List<string> { $"Ранений: {Character.Protagonist.Wounds}" };
+    }
 }
--- Seeker/Seeker/Gamebook/UndergroundRoad/Character.cs
+++ Seeker/Seeker/Gamebook/UndergroundRoad/Character.cs
@@ -2,5 +2,36 @@
 
 namespace Seeker.Gamebook.UndergroundRoad
 {
-    class Character : Prototypes.Character, Abstract.ICharacter { }
+    class Character : Prototypes.Character, Abstract.ICharacter
+    {
+        public static Character Protagonist { get; set; }
+        public override void Set(object character) =>
+            Protagonist = (Character)character;
+
+        public int Wounds { get; set; }
+
+        public override void Init()
+        {
+            base.Init();
+
+            Wounds = 0;
+        }
+
+        public Character Clone() => new Character()
+        {
+            IsProtagonist = this.IsProtagonist,
+            Wounds = this.Wounds,
+        };
+
+        public override string Save() => String.Join("|", Wounds);
+
+        public override void Load(string saveLine)
+        {
+            string[] save = saveLine.Split('|');
+
+            Wounds = int.Parse(save[0]);
+
+            IsProtagonist = true;
+        }
+    }
 }
