9b73e8a7d5ec05de7009f60c420ca261022a78de
mig1023 <mig1023@yandex.ru>
2024-09-16 14:37:41 +0300

    Song of Jaguar rock gamebook (start)

---
+++ Seeker/Seeker/Gamebook/SongOfJaguarRock/Actions.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace Seeker.Gamebook.SongOfJaguarRock
+{
+    class Actions : Prototypes.Actions, Abstract.IActions { }
+}
---
+++ Seeker/Seeker/Gamebook/SongOfJaguarRock/Paragraphs.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace Seeker.Gamebook.SongOfJaguarRock
+{
+    class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs { }
+}
