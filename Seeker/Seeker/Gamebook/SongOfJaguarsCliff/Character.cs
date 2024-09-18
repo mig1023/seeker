36d3143aba2ffd722235e7b7a50cdb6cfdcac94b
mig1023 <mig1023@yandex.ru>
2024-09-18 16:53:47 +0300

    Character + Weapon (song)

--- Seeker/Seeker/Gamebook/SongOfJaguarsCliff/Character.cs
+++ Seeker/Seeker/Gamebook/SongOfJaguarsCliff/Character.cs
@@ -1,6 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Seeker.Gamebook.SongOfJaguarsCliff
 {
-    class Character : Prototypes.Character, Abstract.ICharacter { }
+    class Character : Prototypes.Character, Abstract.ICharacter
+    {
+        public static Character Protagonist { get; set; }
+        public override void Set(object character) =>
+            Protagonist = (Character)character;
+
+        private int _wounds;
+        public int Wounds
+        {
+            get => _wounds;
+            set => _wounds = Game.Param.Setter(value, _wounds, this);
+        }
+
+        public int Hitpoints { get; set; }
+
+        public List<Weapon> Weapons { get; set; } 
+
+        public override void Init()
+        {
+            base.Init();
+
+            Wounds = 0;
+            Hitpoints = 6;
+            Weapons = new List<Weapon> { new Weapon("Дерринджер,4,1,0-50,1") };
+        }
+
+        public Character Clone() => new Character()
+        {
+            IsProtagonist = this.IsProtagonist,
+            Wounds = this.Wounds,
+            Hitpoints = this.Hitpoints,
+            Weapons = new List<Weapon>(this.Weapons),
+        };
+
+        public override string Save()
+        {
+            string weapons = String.Join(";", Weapons.Select(x => x.Save()));
+            return String.Join("|", Wounds, Hitpoints, weapons);
+        }
+
+        public override void Load(string saveLine)
+        {
+            string[] save = saveLine.Split('|');
+
+            Wounds = int.Parse(save[0]);
+            Hitpoints = int.Parse(save[1]);
+
+            Weapons = new List<Weapon>();
+            
+            foreach (string weaponLine in save[1].Split(';'))
+                Weapons.Add(new Weapon(weaponLine));
+
+            IsProtagonist = true;
+        }
+    }
 }
---
+++ Seeker/Seeker/Gamebook/SongOfJaguarsCliff/Weapon.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Seeker.Gamebook.SongOfJaguarsCliff
+{
+    class Weapon
+    {
+        public string Name { get; set; }
+
+        public int Cartridges { get; set; }
+
+        public int Magazine { get; set; }
+
+        public int Damage { get; set; }
+
+        public int DistanceMin { get; set; }
+
+        public int DistanceMax { get; set; }
+
+        public int Recharge { get; set; }
+
+        public Weapon(string line)
+        {
+            var lines = line.Split(',');
+            var distance = lines[3].Split('-');
+
+            this.Name = lines[0];
+            this.Magazine = int.Parse(lines[1]);
+            this.Cartridges = int.Parse(lines[1]);
+            this.Damage = int.Parse(lines[2]);
+            this.DistanceMin = int.Parse(distance[0]);
+            this.DistanceMax = int.Parse(distance[1]);
+            this.Recharge = int.Parse(lines[4]);
+        }
+
+        public string Save()
+        {
+            string distances = $"{this.DistanceMin}-{this.DistanceMax}";
+            string weapon = string.Join(",", Name, Magazine, Damage, distances, Recharge);
+
+            return weapon;
+        }
+
+        public static Weapon ChooseWeapon(List<Weapon> weapons, int distance)
+        {
+            var weapon = weapons
+                .Where(x => x.DistanceMax >= distance)
+                .Where(x => x.DistanceMin <= distance)
+                .Where(x => x.Cartridges > 0)
+                .FirstOrDefault();
+
+            return weapon;
+        }
+    }
+}
