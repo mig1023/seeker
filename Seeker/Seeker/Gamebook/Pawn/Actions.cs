653cac9b272eacebd6dfc21075492c5c570d8221
mig1023 <mig1023@yandex.ru>
2024-09-22 18:55:14 +0300

    Pawn gamebook (start)

---
+++ Seeker/Seeker/Gamebook/Pawn/Actions.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace Seeker.Gamebook.Pawn
+{
+    class Actions : Prototypes.Actions, Abstract.IActions { }
+}
---
+++ Seeker/Seeker/Gamebook/Pawn/Character.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace Seeker.Gamebook.Pawn
+{
+    class Character : Prototypes.Character, Abstract.ICharacter { }
+}
