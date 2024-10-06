db1065076bd5a377cee8d5c0a5537a1148979dfb
mig1023 <mig1023@yandex.ru>
2024-10-06 15:21:47 +0300

    Luck action (blood)

--- Seeker/Seeker/Gamebook/SeasOfBlood/Actions.cs
+++ Seeker/Seeker/Gamebook/SeasOfBlood/Actions.cs
@@ -274,5 +274,29 @@ namespace Seeker.Gamebook.SeasOfBlood
 
             return test;
         }
+
+        public List<string> Luck()
+        {
+            Game.Dice.DoubleRoll(out int firstDice, out int secondDice);
+
+            bool goodLuck = (firstDice + secondDice) <= Character.Protagonist.Luck;
+            string luckLine = goodLuck ? "<=" : ">";
+
+            List<string> luckCheck = new List<string> {
+                $"Проверка удачи: {Game.Dice.Symbol(firstDice)} + " +
+                $"{Game.Dice.Symbol(secondDice)} {luckLine} {Character.Protagonist.Luck}" };
+
+            luckCheck.Add(goodLuck ? "BIG|GOOD|УСПЕХ :)" : "BIG|BAD|НЕУДАЧА :(");
+
+            if (Character.Protagonist.Luck > 2)
+            {
+                Character.Protagonist.Luck -= 1;
+                luckCheck.Add("Уровень удачи снижен на единицу");
+            }
+
+            Game.Buttons.Disable(goodLuck, "Повезло", $"Не повезло");
+
+            return luckCheck;
+        }
     }
 }
